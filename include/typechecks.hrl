70856d809f73bfb0bf0617f9912b5eb7cebae3ef
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-02-19 18:18:03 +0000

    * New: typechecks.hrl, macros for error and type checking in stdfuns. * Splitting out the lists in builtins.hrl.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@77 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ include/typechecks.hrl
@@ -0,0 +1,31 @@
+%%% Type and error checking macros used by stdfuns modules.
+%%% Hasan Veldstra <hasan@hypernumbers.com>
+
+-define(ERR_VAL, stdfuns:error('#VALUE!')).
+-define(ERR_DIV, stdfuns:error('#DIV/0!')).
+-define(ERR_NUM, stdfuns:error('#NUM!')).
+
+-define(ensure_number(Num_x),
+        ?IF(not(is_number(Num_x)), ?ERR_VAL)).
+
+-define(ensure_numbers(L_x),
+        AllNumbers_x = lists:all(fun(X_x) ->
+                                       is_number(X_x)
+                               end,
+                               L_x),
+        ?IF(not(AllNumbers_x), ?ERR_VAL)).
+
+-define(ensure_nonzero(Num_x),
+        ?IF(Num_x == 0, ?ERR_DIV)).
+
+-define(ensure_positive_ex(Num_x, Action_x),
+        ?IF(not(is_number(Num_x) andalso Num_x > 0), Action_x)).
+
+%% Looks at a list of values, and if an error value is found, that error
+%% is returned.
+-define(maybe_throw_up(Values_x),
+        lists:foreach(fun(X_x) ->
+                              ?IF(is_error(X_x), stdfuns:error(X_x))
+                      end,
+                      Values_x)).
+ 
