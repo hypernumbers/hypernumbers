09d25f35d41aa9e267709802be8881029b5cae01
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-02-24 14:54:00 +0000

    NEW: * Frontend generation script. * Full Russian frontend for Muin.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@87 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ priv/muin/language_frontends/generate_lexer.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby -wKU
+
+# Generates Leex lexers for language frontends for Muin.
+# Run as:
+# ./generate_lexer russian
+# This will look for a UTF-8 encoded file russian.txt under ./maps/
+# The result will be written to russian_lexer.xrl. The lexer will expect
+# input to be encoded in UTF-8.
+
+# Hasan Veldstra <hasan@hypernumbers.com>
+
+require "erb"
+
+if ARGV[0].nil?
+  puts "You need to specify a map."
+  Kernel.exit
+end
+
+mapfile = "./maps/#{ARGV[0]}.txt"
+
+if !File.exist?(mapfile)
+  puts "#{mapfile} does not exist."
+  Kernel.exit
+end
+
+# Read the map file, create English => Other Language map.
+map = IO.readlines(mapfile).inject({}) { |acc, line|
+  en, tr = line.split("=").map { |s| s.strip }
+  acc[en] = tr # ic.iconv(tr)
+  acc
+}
+
+@lang = ARGV[0].capitalize
+@function_defs = ""
+@function_rules = ""
+map.each do |k, v|
+  bytes = []
+  v.each_byte { |b| bytes << b }
+  octal_seq =
+    "\\" + bytes.inject([]) { |acc, b| acc << b.to_s(8); acc }.join("\\")
+  @function_defs << "#{k} = (#{octal_seq})\n"
+
+  @function_rules << "{#{k}} : {token, {func, \"#{k.downcase}\"}}.\n"
+end
+
+template = ERB.new(IO.readlines("lexer_template.erb").flatten.join, 0, "%<>")
+lexer = template.result
+File.open("#{@lang.downcase}_lexer.xrl", "w") { |f| f << lexer }
