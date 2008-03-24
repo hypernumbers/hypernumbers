e2e87fa1e80a1162b449613fd30d2d368d786b50
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-03-24 16:47:39 +0000

    Minor changes. Fixed ?ERR_* code to call new functions, renamed a macro.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@144 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/typechecks.hrl
+++ include/typechecks.hrl
@@ -1,10 +1,11 @@
 %%% Type and error checking macros used by stdfuns modules.
 %%% Hasan Veldstra <hasan@hypernumbers.com>
 
--define(ERR_VAL, stdfuns:error('#VALUE!')).
--define(ERR_DIV, stdfuns:error('#DIV/0!')).
--define(ERR_NUM, stdfuns:error('#NUM!')).
-
+-define(ERR_VAL, muin_util:error('#VALUE!')).
+-define(ERR_DIV, muin_util:error('#DIV/0!')).
+-define(ERR_NUM, muin_util:error('#NUM!')).
+-define(ERR_NAM, muin_util:error('#NAME?')).
+        
 -define(ensure_number(Num_x),
         ?IF(not(is_number(Num_x)), ?ERR_VAL)).
 
@@ -18,6 +19,7 @@
 -define(ensure_nonzero(Num_x),
         ?IF(Num_x == 0, ?ERR_DIV)).
 
+%% Ensures number is not less than 0.
 -define(ensure_non_negative_ex(Num_x, Action_x),
         ?IF(not(is_number(Num_x) andalso Num_x >= 0), Action_x)).
 
@@ -26,7 +28,7 @@
 
 %% Looks at a list of values, and if an error value is found, that error
 %% is returned.
--define(maybe_throw_up(Values_x),
+-define(ensure_no_errvals(Values_x),
         lists:foreach(fun(X_x) ->
                               ?IF(stdfuns_info:iserror([X_x]),
                                   muin_util:error(X_x))
