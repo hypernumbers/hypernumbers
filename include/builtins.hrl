bffb073cd2c3c1b55b9408dbaadf2b62ab6810a6
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-04-14 16:41:12 +0000

    Updated builtins.hrl
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@182 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/builtins.hrl
+++ include/builtins.hrl
@@ -1,143 +1,35 @@
 
 -define(STDFUNS,
         [
-         {stdfuns_logical,
-         [
-          eq, neq, lt, gt, lte, gte,
-          'if'
-         ]},
-
          {stdfuns_math,
           [
-           %% Basic arithmetics
-           plus,
-           minus,
-           times,
-           divide,
-           negate,
-         
-           %% Arithmetic
-           sum,
-           product,
-           average,
-           quotient,
-           abs,
-           sqrt,
-           power,
-           sign,
-           exp,
-           fact,
-           %%gcd,
-           %%lcm,
-           mod,
-
-           %% Arrays and matrices
-           mdeterm,
-           minverse,
-           mmult,
-           %%multinom,
-
-           %% Logarithms
-           ln,
-           log,
-           log10,
-
-           %% Random numbers
-           rand,
-           randbetween,
-
-           %% Rounding numbers
-           round,
-           rounddown,
-           roundup,
-           ceiling,
-           combin,
-           even,
-           floor,
-           int,
-           mround,
-           odd,
-           trunc,
-
-           %% Special numbers
-           pi,
-           sqrtpi,
-           roman,
-
-           %% Summation
-           %%seriessum,
-           %%subtotal,
-           %%sumif,
-           %%sumproduct,
-           %%sumsq,
-           %%sumx2my2,
-           %%sumx2py2,
-           %%sumxmy2,
-
-           %% Trigonometry
-           sin,
-           cos,
-           tan,
-           asin,
-           acos,
-           atan,
-           atan2,
-           sinh,
-           cosh,
-           tanh,
-           asinh,
-           acosh,
-           atanh,
-           degrees,
-           radian
-          ]},
+           plus, minus, times, divide, negate, sum, product, average,
+           quotient, abs, sqrt, power, sign, exp, fact, mod, mdeterm,
+           minverse, mmult, ln, log, log10, rand, randbetween, round,
+           rounddown, roundup, ceiling, combin, even, floor, int, mround,
+           odd, trunc, pi, sqrtpi, roman, sin, cos, tan, asin, acos, atan,
+           atan2, sinh, cosh, tanh, asinh, acosh, atanh, degrees, radian
+          ]}, 
 
          {stdfuns_text,
           [
-           %% Comparison functions.
-           exact,
-           len,
-           mid,
-           
-           %% Conversion functions.
-           clean,
-           fixed,
-           lower,
-           proper,
-           upper,
-           
-           %% Locate text.
-           char,
-           find,
-           left,
-           right,
-           concatenate,
-           rept           
+           exact, len, mid, clean, fixed, lower, proper, upper, char, find,
+           left, right, concatenate, rept
           ]},
 
          {stdfuns_logical,
           [
-           eq,
-           neq,
-           lt,
-           gt,
-           lte,
-           gte,
-           'if',
-           'and',
-           %%iferror,
-           'not',
-           'or'
+           eq, neq, lt, gt, lte, gte, 'if', 'and', 'not', 'or'
           ]},
 
          {stdfuns_stats,
           [
-           avedev,
-           average,
-           averagea,
-           binomdist,
-           chidist,
-           exp,
-           expondist
+           avedev, average, averagea, binomdist, chidist, exp, expondist
+          ]},
+
+         {stdfuns_info,
+          [
+           error_type, iserr, iserror, iseven, islogical, isna, isnontext,
+           isnumber, isodd, istext, n, na, type
           ]}
         ]).
