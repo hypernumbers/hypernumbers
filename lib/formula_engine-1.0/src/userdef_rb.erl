695805fe55e1d8887178b7d44933f444cb482b11
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-04-20 20:50:34 +0000

    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@187 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ lib/formula_engine-1.0/src/userdef_rb.erl
@@ -0,0 +1,51 @@
+-module(userdef_rb).
+-compile(export_all).
+
+start() ->
+    spawn(fun() ->
+                  register(userdef_rb, self()),
+                  process_flag(trap_exit, true),
+                  {ok, CurrDir} = file:get_cwd(),
+                  Root = muin_util:init(string:tokens(CurrDir, "/")),
+                  DrvPath = filename:join(["/"] ++ Root ++
+                                          ["lib",
+                                           "formula_engine-1.0",
+                                           "src",
+                                           "userdef_drv.rb"]),
+                  Cmd = "ruby " ++ DrvPath,
+                  Port = open_port({spawn, Cmd}, [{packet, 4}, use_stdio,
+                                                  exit_status, binary]),
+                  loop(Port)
+          end).
+
+stop() ->
+    userdef_rb ! stop.
+
+test(Fname, Args) ->
+    userdef_rb ! {test, self(), Fname, Args},
+    io:format(""),
+    receive
+        Res ->
+            Res
+    end.
+
+loop(Port) ->
+    receive
+        {test, Caller, Fname, Args} ->
+            Port ! {self(), {command, term_to_binary({test, Fname, Args})}},
+            receive
+                {Port, {data, Data}} ->
+                    {result, Res} = binary_to_term(Data),
+                    Caller ! Res;
+                Any ->
+                    erlang:display(Any)
+            end,
+            loop(Port);
+        
+        stop ->
+            Port ! {self(), close},
+            receive
+                {Port, closed} ->
+                    exit(normal)
+            end
+    end.
