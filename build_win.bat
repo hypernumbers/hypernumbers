f7259b1e90203c095c99ea9952ee43a061fe57d8
Gordon Guthrie <gordon@hypernumbers.com>
2008-04-28 23:41:16 +0000

    New docos, presentations and stuff...
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@200 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- build_win.bat
+++ build_win.bat
@@ -1,18 +1,27 @@
 @echo off 
 set erldir=C:\Program Files\erl5.6
 
+echo "COMPILING STARLING
+cd ./lib/starling/
+rake
+cd ../../
+echo "OK"
+
+
 echo "COMPILING LEXER AND PARSER"
 cd ./priv/muin/
 "%erldir%\bin\erlc" generate.erl
 "%erldir%\bin\erl" -noshell -s generate gen -s init stop
 cd ../../
+echo "OK"
 
 echo "COMPILING COMPILERS"
 "%erldir%\bin\erlc" -o ebin src/compile_code.erl
+echo "OK"
 
 echo "COMPILING BUILD"
-
 cd ebin
 "%erldir%\bin\erl" -pa ebin -noshell -s compile_code -s init stop
+echo "OK"
 
 cd ..
