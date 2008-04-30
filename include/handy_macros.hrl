70a4e062937d1994de2e2ca5cb34c1d97f8d23ef
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-04-30 01:18:25 +0000

    Added pattern-matching cond. Other minor changes.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@211 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/handy_macros.hrl
+++ include/handy_macros.hrl
@@ -1,6 +1,6 @@
 %%% Hasan Veldstra <hasan@hypernumbers.com>
 
-%% Import most useful functions from lists.
+%% Import useful functions from lists.
 -import(lists, [all/2,
                 any/2,
                 append/1,
@@ -27,6 +27,11 @@
 -define(COND(Test, TrueVal, FalseVal),
         case (Test) of true -> TrueVal; false -> FalseVal end).
 
+%% Like cond, but lets you specify a pattern to match against (instead
+%% of matching against true).
+-define(ifmatch(X, Pat, Trueexpr, Falseexpr),
+        case (X) of (Pat) -> Trueexpr; _ -> Falseexpr end).
+
 %% Shortcut for if.
 -define(IF(Test, TrueVal),
         ?COND(Test, TrueVal, nothing)).
@@ -37,23 +42,23 @@
         element(2, element(2, keysearch(Key, 1, List)))).
 
 %% Less typing. Handy for debugging.
--define(PUTS(Str),
-        io:format(Str ++ "~n")).
+-define(p(Str),
+        io:format("~p~n", [Str])).
 
 %% A fun that takes no arguments.
--define(fun0(BODY),
+-define(L(BODY),
         fun() -> BODY end). 
 
 %% A fun that takes one argument.
--define(funX(BODY),
+-define(Lx(BODY),
         fun(X) -> BODY end).
 
 %% A fun that takes two arguments.
--define(funXY(BODY),
+-define(Lxy(BODY),
         fun(X, Y) -> BODY end).
 
 %% Just like funXY, nicer in folds.
--define(funXAcc(BODY),
+-define(Lxacc(BODY),
         fun(X, Acc) -> BODY end).
 
 %% Shorthand for ustring:pr(Ustr).
