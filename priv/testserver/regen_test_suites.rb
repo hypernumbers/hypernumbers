9e41ba6e78c66653a0f9946a9c1964796662a3e2
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-04-30 01:01:20 +0000

    Utility script to regenerate full test suites + data file for it.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@207 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ priv/testserver/regen_test_suites.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby -wKU
+
+PATH_TO_DATA_FILES = "../../tests/excel_files/Win_Excel07_As_97/DATA/"
+#PATH_TO_DATA_FILES = "C:\\\\cygwin\\\\home\\\\hasse\\\\proj\\\\hn\\\\tests\\\\excel_files\\\\Win_Excel07_As_97\\\\DATA\\\\"
+
+PATH_TO_TESTS = "../../tests/"
+#PATH_TO_TESTS = "C:\\\\cygwin\\\\home\\\\hasse\\\\proj\\\\hn\\\\tests\\\\"
+
+IO.readlines("load_ranges_list.txt").map { |l| l.strip }.each { |l|
+  a = l.split(/\s+/)
+  `ruby gen_full_test.rb #{PATH_TO_DATA_FILES + a[0]}.dat #{a.slice(1..-1).join(" ")}`
+  puts "OK: #{l.split(/\s+/)[0]}"
+}
+
+puts "DONE GENERATING."
+
+["a", "b", "c", "d", "e", "x"].each do |l|
+  `mv #{l}_*erl #{PATH_TO_TESTS + "excel_import_2" + l + "_test"}`
+end
+
+puts "ALL DONE."
+
+
