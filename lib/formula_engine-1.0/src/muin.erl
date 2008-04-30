a48b66bfb049a2d3f970a084ff166e61c35cf5b8
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-04-30 01:25:49 +0000

    Lots of cleanup/simplification, mainly to do with new uniform internal ref format.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@216 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/muin.erl
+++ lib/formula_engine-1.0/src/muin.erl
@@ -1,18 +1,17 @@
-%%% @doc        Interface to the formula engine. Also, the interpreter.
-%%% @author     Hasan Veldstra <hasan@hypernumbers.com>
-%%% Formulas are currently compiled into simple sexps.
+%%%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%%% @doc Interface to the formula engine. Also, the interpreter.
+%%% @author Hasan Veldstra <hasan@hypernumbers.com>
+%%%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+%%% Formulas are currently compiled into simple s-exps.
 
 -module(muin).
--export([compile/1, run/2, update/2]).
+-export([compile/2, run/2]).
 
 -include("spriki.hrl").
 -include("builtins.hrl").
 -include("handy_macros.hrl").
 -include("typechecks.hrl").
-
--import(muin_util, [expand_cellrange/2, fdbg/1, fdbg/2, get_frontend/0,
-                    getxy/1, init/1, join/1, join/2, just_path/1,
-                    just_ref/1, split_ssref/1, walk_path/2]).
 
 -import(tconv, [to_b26/1, to_i/1, to_i/2, to_s/1]).
 
@@ -23,7 +22,7 @@
 -define(msite, get(site)).
 
 %% Guard for eval() and plain_eval().
--define(IS_FUNCALL(X),
+-define(isfuncall(X),
         is_atom(X) andalso X =/= true andalso X =/= false).
 
 %%%--------------------%%%
@@ -32,20 +31,18 @@
 
 %%% @doc Parses formula, and returns AST as an s-expression.
 %%% @spec parse(Formula :: string()) -> {ok, {Ast :: list()}}
-compile(Fla) ->
-    case (catch try_parse(Fla)) of
+compile(Fla, {X, Y}) ->
+    case (catch try_parse(Fla, {X, Y})) of
         {ok, Pcode} ->
             {ok, Pcode};
         _Else ->
             {error, error_in_formula}
     end.
 
-try_parse(Fla) ->
-    FlaTr = translator:do(Fla),
-    {LexMod, ParseMod} = get_frontend(),
-    {ok, Tokens, _} = LexMod:string(FlaTr),
-    {ok, _Ast} = ParseMod:parse(Tokens). % Match to enforce the contract.
-    
+try_parse(Fla, {X, Y}) ->
+    Trans = translator:do(Fla),
+    {ok, Toks} = muin_lexer:lex(Trans, {X, Y}),
+    {ok, _Ast} = muin_parser:parse(Toks). % Match to enforce the contract.
 
 %% @doc Runs compiled formula.
 run(Pcode, Bindings) ->
@@ -61,22 +58,12 @@ run(Pcode, Bindings) ->
             {ok, {Value, RefTree, Errors, References}}
     end.
 
-
-%%% @doc Updates formula for structural change.
-%%% @spec update(Formula::string(), UpdateMsg::tuple()) ->
-%%%            {ok, {NewFormula :: string(), Ast :: list()}}
-update(Formula, UpdateMsg) ->
-    {ok, NewFormula} = muin_supd:do(Formula, UpdateMsg),
-    {ok, Pcode} = compile(NewFormula),
-    {ok, {NewFormula, Pcode}}.
-
-
 %%%---------------------%%%
 %%%  Private functions  %%%
 %%%---------------------%%%
 
 %% @doc Evaluates an s-expression, pre-processing subexps as needed.
-eval([Fun__ | Args__]) when ?IS_FUNCALL(Fun__) ->
+eval([Fun__ | Args__]) when ?isfuncall(Fun__) ->
     %% Transform s-exp if needed.
     [Fun | Args] = preproc([Fun__ | Args__]),
     %% Evaluate args.
@@ -91,18 +78,20 @@ eval(Value) ->
 
 %% Arguments to ':' calls are either tuples (literals) or lists (funcalls).
 %% Not checking if funcalls are to INDIRECT as it'll fail later if they aren't.
+%% FIXME: This doesn't work anymore with the new ref format.
 preproc([':', StartExpr, EndExpr]) ->
-    Eval = fun(Arg) when is_list(Arg) ->
-                   Cellref = hd(plain_eval(tl(Arg))),
+    Eval = fun(Expr) when is_list(Expr) ->
+                   Cellref = hd(plain_eval(tl(Expr))),
                    {sscellref, "./" ++ Cellref};
-              (Arg) when is_tuple(Arg) ->
-                   Arg
+              (Expr) when is_tuple(Expr) ->
+                   Expr
            end,
 
     [':', Eval(StartExpr), Eval(EndExpr)];
 
 %% This clause is for standalone calls to INDIRECT. Ones that are part of
 %% a range construction expression will be caught by the clause above.
+%% FIXME: As the clause above.
 preproc([indirect, Arg]) ->
     RefStr = plain_eval(Arg),
     {ok, Tokens, _} = muin_lexer:string(RefStr),% Yeh, screw the frontends.
@@ -135,161 +124,71 @@ preproc(Sexp) ->
 
 
 %% @doc Same as eval() but doesn't pre-process.
-plain_eval([Fun | Args]) when ?IS_FUNCALL(Fun) ->
+plain_eval([Fun | Args]) when ?isfuncall(Fun) ->
     CallArgs = [plain_eval(X) || X <- Args],
     funcall(Fun, CallArgs);
 plain_eval(Value) ->
     Value.
 
+%% Refs
+funcall(ref, [Col, Row, Path]) ->
+    Rowidx = toidx(Row),
+    Colidx = toidx(Col),
+    do_cell(Path, Rowidx, Colidx);
 
-%%% ----- Reference functions.
+%% Cell ranges (A1:A5, R1C2:R2C10 etc).
+%% In a range, the path of second ref **must** be ./
+funcall(':', [{ref, Col1, Row1, Path1}, {ref, Col2, Row2, "./"}]) ->
+    Rowidx1 = toidx(Row1),
+    Colidx1 = toidx(Col1),
+    Rowidx2 = toidx(Row2),
+    Colidx2 = toidx(Col2),
+    Cells = muin_util:expand_cellrange(Rowidx1, Rowidx2, Colidx1, Colidx2),
+    map(fun({C, R}) -> do_cell(Path1, R, C) end,
+        Cells);
 
-funcall(sscellref, [Ssref]) ->
-    {Path_, Ref} = split_ssref(Ssref),
-    Path = walk_path(?mpath, Path_),
-    do_cell(Path, Ref);
+%% TODO: Column & row ranges.
 
-funcall(ssrcref, [{RelPath, Row, Col}]) ->
-    Path = walk_path(?mpath, RelPath),
-    do_cell(Path, Row, Col);
-
-funcall(rcrelref, [{RelPath, RowOffset, ColOffset}]) ->
-    Path = walk_path(?mpath, RelPath),
-    do_cell(Path, ?my + RowOffset, ?mx + ColOffset);
-
-%% -- Range functions.
-
-%% Cellranges, e.g. A1:B10, ../page/A1:B10 etc.
-funcall(':', [{sscellref, Ref1}, {cellref, Ref2}]) ->
-    [do_cell(just_path(Ref1), X) ||
-        X <- expand_cellrange(just_ref(Ref1), Ref2)];
-
-%% Column ranges.
-funcall(':', [{sscolref, Ref1}, {col, Ref2}]) ->
-    Path = walk_path(?mpath, just_path(Ref1)),
-    Cols = seq(to_i(just_ref(Ref1), b26), to_i(Ref2, b26)),
-    %% generate a fun to read each column
-    Funs = [fun() -> db:read_column(?msite, Path, X) end || X <- Cols],
-    do_cells(Funs);
-
-%% Row ranges.
-funcall(':', [{ssrowref, Ref1}, {row, Ref2}]) ->
-    FullPath = walk_path(?mpath, just_path(Ref1)),
-    Rows = seq(to_i(just_ref(Ref1)), to_i(Ref2)),
-    Funs = [?fun0(db:read_row(?msite, FullPath, X)) || X <- Rows],
-    do_cells(Funs);
-
-%% -- Hypernumber function and its shorthand.
+%% Hypernumber function and its shorthand.
 
 funcall(hypernumber, [Url_]) ->
     %% Remove trailing ?hypernumber if needed.
     {ok, Url, _} = regexp:gsub(Url_, "\\?hypernumber$", ""),
-    
     #page{site = RSite, path = RPath,
           ref = {cell, {RX, RY}}} = hn_util:parse_url(Url),
-
-    get_value_and_link(fun() ->
-        hn_main:get_hypernumber(?msite, ?mpath, ?mx, ?my, 
-            Url, RSite, RPath, RX, RY)
-    end);
+    F = ?L(hn_main:get_hypernumber(?msite, ?mpath, ?mx, ?my,
+                                   Url, RSite, RPath, RX, RY)),
+    get_value_and_link(F);
 
 funcall(hn, [Url]) ->
     funcall(hypernumber, [Url]);
 
-%%% -- Information functions
-
-%% Unsupported info types: color, filename, parentheses (WTF?), prefix, width.
-
-funcall(cell, ["address", Ref]) ->
-    Ref;
-funcall(cell, ["col", Ref]) ->
-    {_Row, Col} = getxy(Ref),
-    Col;
-funcall(cell, ["contents", Ref]) ->
-    Val = do_cell(".", Ref),
-    ?COND(Val == blank, 0, Val);
-funcall(cell, ["format", _Ref]) ->
-    throw(tantrum); %% TODO: Implement me.
-funcall(cell, ["protect", _Ref]) ->
-    throw(tantrum); %% TODO: Implement me.
-funcall(cell, ["row", Ref]) ->
-    {Row, _Col} = getxy(Ref),
-    Row;
-funcall(cell, ["type", Ref]) ->
-    Val = do_cell(".", Ref),
-    ?COND(Val == blank, "b",
-          ?COND(is_binary(Val), "l",
-                "v"));
-
-funcall(info, _) ->
-    throw({error, unsupported}); %% TODO: What to do with this?
-
-funcall(isref, [_MaybeRef]) ->
-    throw(tantrum); %% TODO: Implement me. With cellref, range, and name support.
-
-%% Picks a module name, calls
+%% Function call, built-in or user-defined.
 funcall(Fname, Args) ->
-    %% Find module name corresponding to function; default to userdef.
-    [Modname | _] = foldl(fun({M, L}, Acc) ->
-                                  ?COND(member(Fname, L), [M], Acc)
-                          end,
-                          [userdef],
-                          ?STDFUNS),
+    case keysearch(Fname, 1, ?STDFUNS) of
+        {value, {Fname, Modname}} ->
+            Modname:Fname(Args);
+        false ->
+            userdef_call(Fname, Args)
+    end.
 
-    ?COND(Modname == userdef,
-          userdef_call(Fname, Args),
-          Modname:Fname(Args)).
+%%% Utility functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-%%% Utility functions.
-
+%% Try the userdef module first, then Ruby, Gnumeric, R, whatever.
 userdef_call(Fname, Args) ->
     case (catch userdef:Fname(Args)) of
         {'EXIT', {undef, _}} -> % Not there, try Ruby.
-            userdef_rb:start(),
-            Res = userdef_rb:test(Fname, Args),
-            userdef_rb:stop(),
-            case Res of
-                error_not_defined ->
-                    ?ERR_NAM;
-                _ ->
-                    Res
-            end;
-        Res -> % Ok.
-            Res
+                    ?ERR_NAM
     end.
-
 
 %% Returns value in the cell + get_value_and_link() is called behind the
 %% scenes.
-do_cell(RelPath, Ref) ->
-    {X, Y} = getxy(Ref),
-    
-    ?IF(X == ?mx andalso Y == ?my,
+do_cell(RelPath, Rowidx, Colidx) ->
+    ?IF(Colidx == ?mx andalso Rowidx == ?my,
         throw({error, self_reference})),
-
-    Path = walk_path(?mpath, RelPath),
-    FetchFun = ?fun0(hn_main:get_cell_info(?msite, Path, X, Y)),
+    Path = muin_util:walk_path(?mpath, RelPath),
+    FetchFun = ?L(hn_main:get_cell_info(?msite, Path, Colidx, Rowidx)),
     get_value_and_link(FetchFun).
-
-do_cell(Path, Row, Col) ->
-    do_cell(Path, to_b26(Col) ++ to_s(Row)).
-
-%% @doc Takes a list of funs that fetch a list of cell records each.
-%% Calls do_cell() on each of those cells.
-%% TODO: The records already contain all required information, don't really
-%% need to call do_cell().
-do_cells(Funs) ->
-%    DoRec = fun(CellRec) ->
-%                    do_cell((CellRec#spriki.index)#index.path,
-%                            (CellRec#spriki.index)#index.row,
-%                            (CellRec#spriki.index)#index.column)
-%            end,
-    
-    map(fun(GetCellRecFun) ->
-                map([], GetCellRecFun())
-        end,
-        Funs).
 
 %% @doc Calls supplied fun to get a cell's value and dependence information,
 %% saves the dependencies (linking it to current cell), and returns
@@ -303,3 +202,11 @@ get_value_and_link(FetchFun) ->
     {RefTree0, Errs0, Refs0} = get(retvals),
     put(retvals, {RefTree0 ++ RefTree, Errs0 ++ Errs, Refs0 ++ Refs}),
     Value.
+
+%% Row or Col information --> index.
+toidx(N) when is_number(N) ->
+    N;
+toidx({row, Offset}) ->
+    ?my + Offset;
+toidx({col, Offset}) ->
+    ?mx + Offset.
