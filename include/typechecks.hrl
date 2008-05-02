0078b5340996c2df9002828b2f72841a917472c0
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-05-02 13:44:21 +0000

    Minor updates in typchecks.hrl
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@238 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/typechecks.hrl
+++ include/typechecks.hrl
@@ -1,23 +1,24 @@
 %%% Type and error checking macros used by stdfuns modules.
 %%% Hasan Veldstra <hasan@hypernumbers.com>
 
--define(ERR_VAL, muin_util:error('#VALUE!')).
--define(ERR_DIV, muin_util:error('#DIV/0!')).
--define(ERR_NUM, muin_util:error('#NUM!')).
--define(ERR_NAM, muin_util:error('#NAME?')).
-        
+-define(ERR_VAL,  muin_util:error('#VALUE!')).
+-define(ERR_DIV,  muin_util:error('#DIV/0!')).
+-define(ERR_NUM,  muin_util:error('#NUM!')).
+-define(ERR_NAME, muin_util:error('#NAME?')).
+-define(ERR_REF,  muin_util:error('#REF!')).
+
 -define(ensure_number(Num_x),
         ?IF(not(is_number(Num_x)), ?ERR_VAL)).
 
--define(ensure_numbers(L_x),
+-define(ensure_numbers(L),
         AllNumbers_x = lists:all(fun(X_x) ->
                                        is_number(X_x)
                                end,
-                               L_x),
+                               L),
         ?IF(not(AllNumbers_x), ?ERR_VAL)).
 
--define(ensure_nonzero(Num_x),
-        ?IF(Num_x == 0, ?ERR_DIV)).
+-define(ensure_nonzero(Num),
+        ?IF(Num == 0, ?ERR_DIV)).
 
 %% Ensures number is not less than 0.
 -define(ensure_non_negative_ex(Num_x, Action_x),
