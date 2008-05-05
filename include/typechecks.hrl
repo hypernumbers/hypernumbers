ec1e06191bf6739ee7316245b30d0b2170e6302e
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-05-05 14:35:55 +0000

    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@249 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/typechecks.hrl
+++ include/typechecks.hrl
@@ -1,14 +1,16 @@
 %%% Type and error checking macros used by stdfuns modules.
 %%% Hasan Veldstra <hasan@hypernumbers.com>
 
--define(ERR_VAL,  muin_util:error('#VALUE!')).
+-define(ERR_NULL, muin_util:error('#NULL!')).
 -define(ERR_DIV,  muin_util:error('#DIV/0!')).
--define(ERR_NUM,  muin_util:error('#NUM!')).
--define(ERR_NAME, muin_util:error('#NAME?')).
+-define(ERR_VAL,  muin_util:error('#VALUE!')).
 -define(ERR_REF,  muin_util:error('#REF!')).
+-define(ERR_NAME, muin_util:error('#NAME?')).
+-define(ERR_NUM,  muin_util:error('#NUM!')).
+-define(ERR_NA,   muin_util:error('#N/A')).
 
--define(ensure_number(Num_x),
-        ?IF(not(is_number(Num_x)), ?ERR_VAL)).
+-define(ensure_number(Num),
+        ?IF(not(is_number(Num)), ?ERR_VAL)).
 
 -define(ensure_numbers(L),
         AllNumbers_x = lists:all(fun(X_x) ->
@@ -21,25 +23,23 @@
         ?IF(Num == 0, ?ERR_DIV)).
 
 %% Ensures number is not less than 0.
--define(ensure_non_negative_ex(Num_x, Action_x),
-        ?IF(not(is_number(Num_x) andalso Num_x >= 0), Action_x)).
+-define(ensure_non_negative_ex(Num, Action),
+        ?IF(not(is_number(Num) andalso Num >= 0), Action)).
 
--define(ensure_positive_ex(Num_x, Action_x),
-        ?IF(not(is_number(Num_x) andalso Num_x > 0), Action_x)).
+-define(ensure_positive_ex(Num, Action),
+        ?IF(not(is_number(Num) andalso Num > 0), Action)).
 
 %% Looks at a list of values, and if an error value is found, that error
-%% is returned.
--define(ensure_no_errvals(Values_x),
+%% value is returned.
+-define(ensure_no_errvals(Values),
         lists:foreach(fun(X_x) ->
                               ?IF(stdfuns_info:iserror([X_x]),
                                   muin_util:error(X_x))
                       end,
-                      Values_x)).
+                      Values)).
 
 -define(ensure(Test, Action),
-        case Test of
-            true ->
-                nothing;
-            false ->
-                Action
+        case (Test) of
+            true  -> nothing;
+            false -> Action
         end).
