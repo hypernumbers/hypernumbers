9f1ebd3ca8d3d624c0d0ea0b45dd24460f28c386
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-05-05 16:25:05 +0000

    * The new mass generator.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@252 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ priv/testserver/regen_tests.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby -wKU
+
+# Run as:
+#   ruby regen_tests.rb 1x -- to regenerate all 1x suites (or 2x).
+#   ruby regen_tests.rb 1x a -- to regenerate the a suite only.
+#   ruby regen_tests.rb 2x abc -- to regeneratee suites a, b, and c.
+
+PATH_TO_DATA_FILES = if File.exist?("C:") # Windows/Cygwin
+                       "C:\\\\data_files\\\\"
+                     else # Sane system
+                       "../../tests/excel_files/Win_Excel07_As_97/DATA/"
+                     end
+
+generator = (ARGV[0].downcase == "1x" ? "gen_rev_comp_test.rb" : "gen_full_test.rb")
+series = ARGV[0].downcase[0].chr
+
+rawlines = IO.readlines("readlist.txt").map { |line| line.strip }
+lines = (if ARGV[1] # Given test suite id, e.g. a?
+           rawlines.select { |line| # Select lines which start with one of those letters
+             ARGV[1].include?(line[0].chr)
+           }
+         else
+           rawlines
+         end)
+
+lines.map do |line|
+  toks = line.split(/\s+/)
+  print "-> #{toks[0]} ... "; $stdout.flush
+  filename = PATH_TO_DATA_FILES + toks[0] + ".dat" # Don't use File#join here!
+  ranges = toks.slice(1..-1).join(" ")
+
+  `ruby #{generator} #{filename} #{ranges}`
+  puts"OK"
+end
+
+mvs = if ARGV[1]
+        chars = []
+        ARGV[1].each_byte { |b| chars << b.chr }
+        chars
+      else
+        ["a", "b", "c", "d", "e"]
+      end
+
+mvs.each do |char|
+  dir = "excel_import_#{series}#{char}_test"
+  `mkdir ../../tests/#{dir}`
+  `mv #{char}*.erl ../../tests/#{dir}`
+end
+
+puts "ALL DONE."
