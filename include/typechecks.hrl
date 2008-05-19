b4d20c61fc40572720f0d9eef811382abd2ad49e
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-05-19 13:14:04 +0000

    Updated to use muin_check functions.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@297 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/typechecks.hrl
+++ include/typechecks.hrl
@@ -1,71 +1,38 @@
 %%% Type and error checking macros used by stdfuns modules.
 %%% Hasan Veldstra <hasan@hypernumbers.com>
 
--define(ERR_NULL, muin_util:error('#NULL!')).
--define(ERR_DIV,  muin_util:error('#DIV/0!')).
--define(ERR_VAL,  muin_util:error('#VALUE!')).
--define(ERR_REF,  muin_util:error('#REF!')).
--define(ERR_NAME, muin_util:error('#NAME?')).
--define(ERR_NUM,  muin_util:error('#NUM!')).
--define(ERR_NA,   muin_util:error('#N/A')).
+-include("errvals.hrl").
 
--define(ensure_number(Num),
-        ?IF(not(is_number(Num)), ?ERR_VAL)).
+-define(ensure_number(N),
+        muin_checks:number(N)).
 
--define(ensure_numbers(L),
-        Allokx = lists:all(fun(X_x) -> is_number(X_x) end, L),
-        ?IF(not(Allokx), ?ERR_VAL)).
+-define(ensure_numbers(Ns),
+        muin_checks:numbers(Ns)).
 
--define(ensure_nonzero(Num),
-        ?IF(Num == 0, ?ERR_DIV)).
+-define(ensure_nonzero(N),
+        muin_checks:nonzero(N)).
 
-%% Ensures number is >= 0.
--define(ensure_non_negative(Num),
-        ?ensure_non_negative_ex(Num, ?ERR_NUM)).
+-define(ensure_non_negative(N),
+        muin_checks:gte0(N)).
 
--define(ensure_non_negatives(L),
-        Allokx = lists:all(fun(X_x) -> is_number(X_x) andalso X_x >= 0 end,
-                           L),
-        ?IF(not(Allokx), ?ERR_NUM)).
-        
+-define(ensure_non_negatives(Ns),
+        muin_checks:gte0s(Ns)).
 
--define(ensure_non_negative_ex(Num, Action),
-        ?IF(not(is_number(Num) andalso Num >= 0), Action)).
+-define(ensure_positive(N),
+        muin_checks:gt0(N)).
 
-%% Ensures number is > 0.
--define(ensure_positive(Num),
-        ?ensure_positive_ex(Num, ?ERR_NUM)).
-
--define(ensure_positive_ex(Num, Action),
-        ?IF(not(is_number(Num) andalso Num > 0), Action)).
-
-%% Looks at a list of values, and if an error value is found, that error
-%% value is returned.
--define(ensure_no_errvals(Values),
-        lists:map(fun(X_x) ->
-                          ?COND(stdfuns_info:iserror([X_x]),
-                                muin_util:error(X_x),
-                                X_x)
-                  end,
-                  Values)).
+-define(ensure_no_errvals(Vs),
+        muin_checks:die_on_errval(Vs)).
 
 -define(ensure(Test, Action),
-        case (Test) of
-            true  -> nothing;
-            false -> Action
-        end).
+        muin_checks:ensure(Test, fun() -> Action end)).
 
--define(filter_numbers(Vals),
-        [X || X <- Vals, is_number(X)]).
-
--define(filter_numbers_with_cast(Vals),
-        [cast(X, num) || X <- Vals, is_number(muin_util:cast(X, num))]).
+-define(filter_numbers(Vs),
+        muin_checks:filter_numbers(Vs)).
         
-%% L is a list that may contain {matrix, _, [X]} tuples.
+-define(filter_numbers_with_cast(Vs),
+        muin_checks:filter_numbers_all(Vs)).
+        
 -define(flatten(L),
-        (foldl(fun({matrix, _, Xs_}, Acc) ->
-                       append([Acc, Xs_]);
-                  (X, Acc) ->
-                       append([Acc, [X]])
-               end,
-               [], L))).
+        muin_checks:deck(L)).
+        
