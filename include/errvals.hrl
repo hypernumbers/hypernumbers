5423f0935608212d1b547c2f562bf66133d0f8a1
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-05-19 13:12:30 +0000

    Moved errval macros to a separate hrl.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@295 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ include/errvals.hrl
@@ -0,0 +1,8 @@
+
+-define(ERR_NULL, muin_util:error('#NULL!')).
+-define(ERR_DIV,  muin_util:error('#DIV/0!')).
+-define(ERR_VAL,  muin_util:error('#VALUE!')).
+-define(ERR_REF,  muin_util:error('#REF!')).
+-define(ERR_NAME, muin_util:error('#NAME?')).
+-define(ERR_NUM,  muin_util:error('#NUM!')).
+-define(ERR_NA,   muin_util:error('#N/A')).
