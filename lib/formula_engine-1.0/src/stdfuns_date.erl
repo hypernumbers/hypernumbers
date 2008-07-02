c01248e2d7fddaf331596b70615c89752a9fe82b
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-07-02 14:33:42 +0000

    * Added EDATE * Added EOMONTH * Added NETWORKDAYS * Added TODAY * Added WEEKDAY * Added WEEKNUM
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@401 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_date.erl
+++ lib/formula_engine-1.0/src/stdfuns_date.erl
@@ -9,8 +9,15 @@
          datevalue/1,
          day/1,
          month/1,
-         year/1]).
-         
+         year/1,
+         edate/1,
+         eomonth/1,
+         networkdays/1,
+         today/0,
+         weekday/1,
+         weeknum/1]).
+
+
 -include("typechecks.hrl").
 -include("handy_macros.hrl").
 -include("muin_records.hrl").
@@ -24,7 +31,7 @@ date(Arg = [_, _, _]) ->
     #datetime{date = {Year, Month, Day}}.
 
 datedif([V1, V2, V3]) ->
-    [Start, End] = ?dates([V1, V2], [collect_strings]),
+    [Start, End] = ?dates([V1, V2], [cast_strings]),
     ?ensure(muin_date:gt(End, Start), ?ERR_NUM),
     ?estring(V3),
     Unit = string:to_upper(?utf8l(V3)),
@@ -78,7 +85,7 @@ datedif1(Start, End, "YD") ->
           seq(muin_date:month(Start), muin_date:month(End))).
 
 datevalue([V1]) ->
-    ?date(V1, [collect_strings]).
+    ?date(V1, [cast_strings]).
 
 day([Dt]) ->
     ?edate(Dt),
@@ -91,6 +98,110 @@ month([Dt]) ->
 year([Dt]) ->
     ?edate(Dt),
     muin_date:year(Dt).
+
+edate([V1, V2]) ->
+    Start = ?date(V1, [cast_strings]),
+    Months = ?int(V2, [cast_bools,
+                       cast_strings,
+                       zero_blanks])),
+    edate1(Start, Months).
+edate1(Start, Months) ->
+    #datetime{date = {Startyr, Startmo, Startday}} = Start,
+    Yrdelta = Months / 12,
+    Modelta = Months - (Yrdelta * 12),
+    Start#datetime{date = {Startday + Yrdelta, Startmo + Modelta, Startday}}.
+
+eomonth([V1, V2]) ->
+    Start = ?date(V1, [cast_strings]),
+    Months = ?int(V2, [cast_strings,
+                       cast_bools,
+                       zero_blanks]),
+    %% TODO: More error checks, see Excel.
+    eomonth1(Start, Months).
+eomonth1(Start, Months) ->
+    Tdate = edate1(Start, Months),
+    #datetime{date = {Tyr, Tmo, Tday}} = Tdate,
+    Lastday = calendar:last_day_of_the_month(Tyr, Tmo),
+    Tdate#datetime{date = {Tyr, Tmo, Lastday}}.
+
+%% TODO: Can provide lists of holidays here for convenience, e.g.
+%% "GB" -> British holidays, "RUS" -> Russian holidays etc.
+networkdays([V1, V2]) ->
+    networkdays([V1, V2, []]);
+networkdays([V1, V2, V3]) ->
+    [Start, End] = ?dates([V1, V2], [cast_strings]),
+    Holidays = case V3 of
+                   [] -> [];
+                   _  -> ?dates(V3, [cast_strings])
+               end,
+    networkdays1(Start, End, Holidays).
+networkdays1(Start, End, Holidays) ->
+    Dtdiff = muin_date:dtdiff(Start, End),
+    muin_date:foldl(fun(X, Acc) ->
+                            case calendar:day_of_the_week(X#datetime.date) of
+                                6 -> Acc; % Saturday
+                                7 -> Acc; % Sunday
+                                _ ->      % Check if the date falls on holiday
+                                    %% FIXME: Will break on #datetimes with the same date
+                                    %% but different time fields.
+                                    case member(X, Holidays) of
+                                        true -> Acc;
+                                        false -> Acc + 1
+                                    end
+                            end
+                    end,
+                    0, Start, End).
+
+%% TODO: Potential for confusion if user & server are in different timezones, e.g.
+%% a Japanese user on a Californian server...
+today() ->
+    {Date, _Time} = calendar:now_to_universal_time(erlang:now()),
+    #datetime{date = Date}.
+
+weekday([V1]) ->
+    weekday([V1, 1]);
+weekday([V1, V2]) ->
+    Dt = ?date(V1, [cast_strings]),
+    Rettype = ?int(V2, [cast_strings, cast_bools]),
+    ?ensure(Rettype == 1 orelse Rettype == 2 orelse Rettype == 3,
+            ?ERR_NUM),
+    weekday1(Dt, Rettype).
+weekday1(Dt, 1) ->
+    calendar:day_of_the_week(Dt#datetime.date) of
+        1 -> 2;
+        2 -> 3;
+        3 -> 4;
+        4 -> 5;
+        5 -> 6;
+        6 -> 7;
+        7 -> 1
+    end;
+weekday1(Dt, 2) ->
+    calendar:day_of_the_week(Dt#datetime.date);
+weekday1(Dt, 3) ->
+    calendar:day_of_the_week(Dt#datetime.date) - 1.
+
+weeknum([V1]) ->
+    weeknum([V1, 1]);
+weeknum([V1, V2]) ->
+    Dt = ?date(V1, [cast_strings]),
+    Rettype = ?int(V2, [cast_strings, cast_bools]),
+    ?ensure(Rettype == 1 orelse Rettype == 2,
+            ?ERR_NUM),
+    weeknum1(Dt, Rettype).
+weeknum1(Dt, Rettype) ->
+    muin_date:foldl(fun(X, Acc) ->
+                            case weekday1(X, Rettype) of
+                                1 -> Acc + 1; % Sunday for Rettype = 1, Monday for 2.
+                                _ -> Acc
+                    end,
+                    1,
+                    #datetime{date = {muin_date:year(Dt), 1, 1}},
+                    #datetime{date = {muin_date:year(Dt), 12, 31}}).
+
+    
+    
+    
 
 %%% TESTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
