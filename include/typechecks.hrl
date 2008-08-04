e6faa660117e4c89950259360324acf539ea9c43
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-08-04 18:00:28 +0000

    * ?number will now grab the first element of an array given as argument (and so will ?numbers). * Added ?ints. * ?estring will work on both Erlang strings and Ustrings (temporary hack). * Added ?bool and ?bools. * Added ?flatten_all to wrap Collector's flatten_arrays and flatten_ranges.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@441 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- include/typechecks.hrl
+++ include/typechecks.hrl
@@ -1,7 +1,5 @@
-%%%----------------------------------------------------------------------------
-%%% @doc Type and error checking macros used by stdfuns modules.
 %%% @author Hasan Veldstra <hasan@hypernumbers.com>
-%%%----------------------------------------------------------------------------
+%%% @doc Type and error checking macros used by stdfuns modules.
 
 %%% Even though a lot of macros here are simple wrappers around function calls,
 %%% it makes sense to keep them that way to make the type checking and casting
@@ -11,35 +9,36 @@
 
 -include("errvals.hrl").
 
-%% Collect numbers from Xs.
 -define(numbers(Xs, Rules),
-        muin_collect:collect_numbers(Xs, Rules)).
+        lists:map(fun(X) -> ?number(X, Rules) end, Xs)).
+        %%muin_collect:collect_numbers(Xs, Rules)).
 
-%% Collect a number.
 -define(number(X, Rules),
-        muin_collect:collect_number(X, Rules)).
+        muin_collect:collect_number(case X of
+                                        {array, _} -> muin_util:array_at(X, 1, 1);
+                                        _ -> X
+                                    end,
+                                    Rules)).
 
-%% Collect an int.
+
 -define(int(X, Rules),
         erlang:trunc(?number(X, Rules))).
 
-%% Collect a date.
+-define(ints(Xs, Rules),
+        lists:map(fun(X) -> erlang:trunc(X) end,
+                  ?numbers(Xs, Rules))).
+
 -define(date(X, Rules),
         muin_collect:collect_date(X, Rules)).
 
-%% Collect dates from Xs.
 -define(dates(Xs, Rules),
         muin_collect:collect_dates(Xs, Rules)).
 
-%% Ensure X is a date.
 -define(edate(X),
-        if is_record(X, datetime) ->
-                ok;
-           true ->
-                ?ERR_VAL
+        if is_record(X, datetime) -> ok;
+           true -> ?ERR_VAL
         end).
 
-%% Convert a Ustring to a UTF-8 list.
 -define(utf8l(Ustr),
         ustring:to_utf8(Ustr)).
 
@@ -47,17 +46,23 @@
 %% Using element() rather than pattern matching to avoid creating new names
 %% in the current scope.
 -define(estring(X),
-        if element(1, X) == ustring andalso is_binary(element(2, X)) ->
-                ok;
-           true ->
-                ?ERR_VAL
+        if element(1, X) == ustring andalso is_binary(element(2, X)) -> X; % STR
+           is_list(X) -> X; % STR
+           true -> ?ERR_VAL
         end).
 
 %% Ensure Xs are all strings.
 -define(estrings(Xs),
-        foreach(fun(X) -> ?estring(X) end,
-                Xs)).
+        foreach(fun(X) -> ?estring(X) end, Xs)).
 
+-define(bool(X, Rules),
+        muin_collect:collect_bool(X, Rules)).
+
+-define(bools(Xs, Rules),
+        muin_collect:collect_bools(Xs, Rules)).
+
+-define(flatten_all(Xs),
+        muin_collect:flatten_arrays(muin_collect:flatten_ranges(Xs))).
 
 %%%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 %%% OLD STUFF BELOW.
@@ -96,4 +101,3 @@
         
 -define(flatten(L),
         muin_checks:deck(L)).
-        
