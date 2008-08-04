b2eb75496141cb5ee2946027d38f6989fe19f244
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-08-04 18:06:16 +0000

    * Renamed eq to '=', neq to '<>' etc.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@446 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_logical.erl
+++ lib/formula_engine-1.0/src/stdfuns_logical.erl
@@ -4,54 +4,52 @@
 -module(stdfuns_logical).
 -include("handy_macros.hrl").
 -include("typechecks.hrl").
--import(xltconv, [to_b/1]).
 
 -export([
-         eq/1,
-         neq/1,
-         lt/1,
-         gt/1,
-         lte/1,
-         gte/1,
+         '='/1,
+         '<>'/1,
+         '<'/1,
+         '>'/1,
+         '<='/1,
+         '>='/1,
          'if'/1,
          'and'/1,
          %%iferror/1,
          'not'/1,
-         'or'/1
-        ]).
+         'or'/1]).
 
-eq([A, B]) ->
+'='([A, B]) ->
     A == B.
 
-neq([A, B]) ->
-    not(eq([A, B])).
+'<>'([A, B]) ->
+    not('='([A, B])).
 
-gt([A, B]) ->
+'>'([A, B]) ->
     A > B.
 
-lt([A, B]) ->
+'<'([A, B]) ->
     A < B.
 
-lte([A, B]) ->
+'<='([A, B]) ->
     A =< B.
 
-gte([A, B]) ->
+'>='([A, B]) ->
     A >= B.
 
 'if'([Test, TrueVal, FalseVal]) ->
-    ?COND(to_b(Test), TrueVal, FalseVal).
+    ?COND(Test, TrueVal, FalseVal).
 
-'and'([Vs]) ->
+'and'(Vs) ->
     Flatvs = ?flatten_all(Vs),
     Bools = ?bools(Flatvs, [cast_strings, cast_numbers, cast_blanks, cast_dates]),
     all(fun(X) -> X =/= false end, Bools).
 
 'not'([Val]) ->
     ?ensure_no_errvals([Val]),
-    not(to_b(Val)).
+    not(Val).
 
 'or'([Vals]) ->
     Flatvals = flatten(Vals),
     ?ensure_no_errvals(Flatvals),
-    any(fun(X) -> to_b(X) == true end,
+    any(fun(X) -> X == true end,
         Flatvals).
