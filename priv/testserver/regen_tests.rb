bba5f01b206beaec984a947a4acb1b61eb6a5146
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-08-15 11:23:05 +0000

    Stop using readlist.txt. Minor fixes for clarity/correctness.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@482 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/testserver/regen_tests.rb
+++ priv/testserver/regen_tests.rb
@@ -5,28 +5,19 @@
 #   ruby regen_tests.rb 1x a -- to regenerate the a suite only.
 #   ruby regen_tests.rb 2x abc -- to regeneratee suites a, b, and c.
 
+require "fileutils"
+
 PATH_TO_DATA_FILES = "../../tests/excel_files/Win_Excel07_As_97/DATA/"
+fullpath = __FILE__ + PATH_TO_DATA_FILES
 
-generator = (ARGV[0].downcase == "1x" ? "reader_test_generator.rb" : "gen_full_test.rb")
-series = ARGV[0].downcase[0].chr
+generator = (ARGV[0].downcase == "1x" ?
+             "reader_test_generator.rb" : "gen_full_test.rb")
+datfiles = Dir[fullpath + "*.dat"]
 
-rawlines = IO.readlines("readlist.txt").map { |line| line.strip }
-lines = (if ARGV[1] # Given test suite id, e.g. a?
-           rawlines.select { |line| # Select lines which start with one of those letters
-             ARGV[1].include?(line[0].chr)
-           }
-         else
-           rawlines
-         end)
-
-lines.map do |line|
-  toks = line.split(/\s+/)
-  print "-> #{toks[0]} ... "; $stdout.flush
-  filename = PATH_TO_DATA_FILES + toks[0] + ".dat" # Don't use File#join here!
-  ranges = toks.slice(1..-1).join(" ")
-
-  `ruby #{generator} #{filename} #{ranges}`
-  puts"OK"
+datfiles.select { |fn| ARGV[1].include?(File.basename(fn)[0].chr) }.each do |fn|
+  print "-> #{File.basename(fn, ".dat")}"; $stdout.flush
+  `ruby #{generator} #{fn}`
+  puts "OK"
 end
 
 mvs = if ARGV[1]
@@ -37,10 +28,11 @@ mvs = if ARGV[1]
         ["a", "b", "c", "d", "e", "x"]
       end
 
+series = ARGV[0].downcase[0].chr
 mvs.each do |char|
   dir = "excel_import_#{series}#{char}_test"
-  `mkdir ../../tests/#{dir}`
-  `mv #{char}*.erl ../../tests/#{dir}` 
+  FileUtils.mkdir("../../tests/#{dir}") rescue nil
+  FileUtils.mv("#{char}*.erl" "../../tests/#{dir}") rescue nil
 end
 
 puts "ALL DONE."
