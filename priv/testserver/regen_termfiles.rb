79d5902889b2179c60af573fdc058de97187e14c
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-08-15 11:22:56 +0000

    Generate data files for all XLS files in a directory; not using readlist.txt anymore.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@481 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/testserver/regen_termfiles.rb
+++ priv/testserver/regen_termfiles.rb
@@ -1,20 +1,19 @@
 #!/usr/bin/env ruby -wKU
 
-# Regenerates data files for Excel spreadsheets used for testing. The list is
-# read from list.txt
-# USAGE: Amend the path below, and run. Make sure to separate directories with
-# 4 slashes.
+# Generates data files for test generators from Excel spreadsheets.
+# Usage: ruby regen_termfiles.rb C:\some_folder_with_xls_files
+# (NO TRAILING BACKSLASHES!)
 
-# TODO: Get path to XLS files on the command line.
 # TODO: Load read function from readexcel.rb & invoke them in here
 #       rather than creating a new process to do it.
 
 path = ARGV[0].gsub('\\', '\\\\\\\\') + '\\\\'
 
-IO.readlines("readlist.txt").each do |s|
-  x = path + s.split(/\s+/)[0] + ".xls"
-  `ruby readexcel.rb #{x}`
-  puts "OK: #{s.strip}"
+xlsfiles = Dir[path + "*.xls"]
+
+xlsfiles.each do |fn|
+  `ruby readexcel.rb #{fn}`
+  puts "OK: #{File.basename(fn, ".xls")}"
 end
 
 puts "ALL DONE"
