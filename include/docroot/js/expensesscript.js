af135f5e0e6949d8c5fad14c9270b24896d70c55
Dale Harvey <dale@hypernumbers.com>
2008-08-25 16:14:05 +0000

    Commit demo stuff
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@541 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ include/docroot/js/expensesscript.js
@@ -0,0 +1,112 @@
+var formulae = new Array();
+var parents  = new Array();
+var children = new Array();
+var css_attrs = ["font-style","font-weight","text-align","text-decoration","color"];
+
+var loc = document.location;
+console.log(loc);
+//var url = loc.protocol+loc.host+loc.pathname;
+url="http:127.0.0.1:9000/expenses/claim/1"
+hardwiredpath="/expenses/claim/1/"
+console.log(url);
+
+var auth = false;
+
+var handle_attr = function(refxml)
+{
+    var el = $(refxml);
+    
+    var ref   = el.attr("ref");
+    var name  = ((el.children()[0]).nodeName).toLowerCase();
+    var value = el.text();
+    
+  if (name == "value")
+    {
+	 $("#"+ref).text(value);	 
+    }
+}
+    
+
+var init = function()
+{
+  console.log("in expensesscript init");
+    var so = new SWFObject("/swf/JsSocket.swf", 
+			   "mymovie", 
+			   "470", 
+			   "200", 
+			   "8", 
+			   "#FFFFFF");
+    so.addParam("allowScriptAccess", "always");
+    so.write("flashcontent");    
+};
+
+$(function()
+{
+    // reuse this script in different places
+    pagename=getPageName();
+    console.log("get the name of the page calling this script");
+    console.log(pagename);
+    if (pagename == "expenses.html")
+    {
+         build_form();
+    }
+    init();
+    load_data();
+ });
+
+build_form = function()
+{
+            loadRange("/data/");
+            $("input[type='submit']").click(function()
+            {
+                post_vars.sort(function(a,b)
+                {
+                    return a.destination.charCodeAt(0) < 
+                        b.destination.charCodeAt(0) ? -1 : 1;
+                });
+                
+                var str = "<create>";
+
+                for(i=0; i< post_vars.length; i++)
+                {
+                    var getChecked = function(arr)
+                    {
+                        for(x=0; x< arr.length; x++)
+                        {
+                            if((arr[x])[0].checked)
+                                return arr[x].attr('rawvalue');
+                        }
+                    }
+                
+                    var val = (post_vars[i].type != "radio") 
+                        ? post_vars[i].input.val()
+                        : getChecked(post_vars[i].input);
+                        
+                    str += "<formula><![CDATA["+val+"]]></formula>";
+                }
+                str += "</create>";
+                //console.log("checking post string in loadRange **starts**");
+                //console.log(str);
+                //console.log("**ends**");
+                
+                $.post("/expenses/claim/1/?attr&lastrow",str,null,"xml");                
+            });
+}
+
+load_data = function()
+{
+    
+    var get_attr = function(data) 
+    {
+	var fun = function(y) { handle_attr(this); };
+	$(data).find("ref").each(fun);
+    }    
+     
+    $.ajax({
+	type: "GET",
+	url: hardwiredpath+"?attr",
+	dataType: "xml",
+	success: get_attr
+    });
+}
+
---
+++ include/docroot/js/formbuilder.js
@@ -0,0 +1,143 @@
+        function getPath()
+	{
+            //console.log("in getPath **starts**");
+ 	    page=document.location.pathname;
+	    segments=page.split("/");
+            len=segments.length;
+            path="";
+            for (i=1; i < len-1; i++)
+            {
+              path=path+"/"+segments[i];
+            };
+            path=path+"/";
+            //console.log(path);
+            //console.log("**ends**");
+            return path;
+        }
+
+function getPageName()
+{
+  page=document.location.pathname;
+	  segements=page.split("/");
+          len=segments.length;
+	  pagename=segments[len-1];
+          return pagename;
+	    }
+
+        function parseRef(cell)
+        {
+            return [
+                ((cell.match(/[a-z]+/i)[0]).charCodeAt(0)-64),
+                 parseInt(cell.match(/[0-9]+/)[0])];
+        }
+        
+        function handleElement(root,params)
+        {
+            if(params[0] == "select")
+            {
+                if ((params[2][0] == "/") || (params[2][0] == "."))
+                {
+                    get_path=params[2];
+                }
+                else
+                {
+                    get_path=root+params[2];
+                }
+                var select = $("<select />").appendTo($(params[1]));   
+                $.get(get_path+"?attr",function(data) 
+                {
+                    $(data).find("ref").each(function()
+                    {
+                        if($(this).find("rawvalue").size() > 0)
+                        {
+                            select.append($("<option>"+
+                                $(this).find("rawvalue > string").text()+"</option>"));
+                        }
+                    });
+                });
+                post_vars.push({type:"select",input:select,destination:params[3]});
+            }
+            else if(params[0] == "radio")
+            {
+                $.get(root+params[2]+"?attr",function(data) 
+                {
+                    var inputs = new Array();
+                    $(data).find("ref").each(function()
+                    {
+                        if($(this).find("rawvalue").size() > 0)
+                        {
+                            var n = $(this).find("rawvalue > string").text();
+                            var el = $("<input type='radio' name="+params[4]+" value='"+n+"' \>");
+                            inputs.push(el);
+                            $(params[1]).append(el).append($("<span> "+n+"</span>"));
+                        }
+                    });
+                    post_vars.push({type:"radio",input:inputs,destination:params[3]}); 
+                });
+            }   
+            else if(params[0] == "textinput")
+            {
+                var el = $("<input type=\"text\" />");
+                $(params[1]).append($("<label><span>" + params[2]
+                    + "</span></label>").append(el));    
+                post_vars.push({type:"text",input:el,destination:params[3]});   
+            }
+            else if(params[0] == "textarea")
+            {
+                var el = $("<textarea></textarea>");
+                $(params[1]).append($("<label><span>" + params[2]
+                    + "</span><br /></label>").append(el));
+                post_vars.push({type:"textarea",input:el,destination:params[3]});  
+            }
+            else if(params[0] == "text")
+            {
+                $(params[1]).text(params[2]);      
+            }
+        }
+    
+        function loadForm(root,range)
+        {
+        //console.log("in loadForm **starts**");
+        //console.log(root);
+        //console.log("**ends**");
+            $.get(root+range+"?attr",function(data)   
+            {
+                var reflist = new Array();
+
+                $(data).find("ref").each(function()
+                
+                {
+                    if($(this).find("rawvalue").size() > 0)
+                    {
+                        var ref = parseRef($(this).attr("ref"));                        
+                         if(typeof reflist[ref[1]] == "undefined")
+                            reflist[ref[1]] = new Array();
+                            
+                        reflist[ref[1]][ref[0]-1] = $(this);
+                    }
+                });
+                $.each(reflist,function(i)
+                {
+                    if(this.length>0)
+                    {
+                        var params = new Array();
+                                                    
+                        for(i=0; i < this.length; i++) 
+                        {
+                            params.push(this[i].find("string").text());
+                        }
+                        handleElement(root,params);
+                    }
+                });
+            });
+        }
+        
+        function loadRange(cfg)
+        {
+            $.get(cfg+"a1?attr",function(data)
+            {
+                var range = $(data).find("rawvalue > string").text();
+    
+                loadForm(cfg,range);
+            });
+        }
