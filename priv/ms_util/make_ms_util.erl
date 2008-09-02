d84c836cd6ffd364e48c26c8e39020075bfd0bea
Gordon Guthrie <gordon@hypernumbers.com>
2008-09-02 23:40:57 +0000

    Changes to new match spec utility function generator
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@588 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/ms_util/make_ms_util.erl
+++ priv/ms_util/make_ms_util.erl
@@ -20,35 +20,44 @@ make() ->
 
 make_src(Tree) -> make_src(Tree,[]).
 
-make_src([],Acc)                              -> make_src2(Acc,[]);
+make_src([],Acc)                              -> make_src2(Acc,[],[]);
 make_src([{attribute,_,record,Record}|T],Acc) -> make_src(T,[Record|Acc]);
-make_src([_H|T],Acc)                           -> make_src(T,Acc).
+make_src([_H|T],Acc)                          -> make_src(T,Acc).
 
-make_src2([],Acc)    -> top_and_tail(Acc);
-make_src2([H|T],Acc) -> make_src2(T,[expand_rec(H)|Acc]).
+make_src2([],Acc1,Acc2)    -> top_and_tail(Acc1,Acc2);
+make_src2([H|T],Acc1,Acc2) -> {NewAcc1,NewAcc2}=expand_rec(H),
+			      make_src2(T,[NewAcc1|Acc1],[NewAcc2|Acc2]).
 
 expand_rec({Name,Def}) -> expand_fields(Name,Def,1,[]).
 
-expand_fields(Name,[],_N,Acc) -> lists:reverse([mk(Name)|Acc]);
+expand_fields(Name,[],N,Acc) -> {mk2(Name,N),lists:reverse([mk(Name)|Acc])};
 expand_fields(Name,[{record_field,_,{atom,_,F},_}|T],N,Acc) -> 
     expand_fields(Name,T,N+1,[mk(Name,F,N)|Acc]);
 expand_fields(Name,[_H|T],N,Acc) -> expand_fields(Name,T,N,Acc).
 
+%% mk2/1 builds the no of fields fns
+mk2(Name,N) -> "no_of_fields("++atom_to_list(Name)++") -> "++
+		   integer_to_list(N)++";\n".
+
 %% mk/1 builds an error line
-mk(Name) -> "get_index("++atom_to_list(Name)++",F) -> exit({error,\"Record: "++
-		atom_to_list(Name)++" has no field called \"++atom_to_list(F)});\n".
+mk(Name) -> "get_index("++atom_to_list(Name)++",F) -> "++
+		"exit({error,\"Record: "++atom_to_list(Name)++
+		" has no field called \"++atom_to_list(F)});\n".
 
 mk(Name,Field,N) -> 
     "get_index("++atom_to_list(Name)++","++
 	atom_to_list(Field)++")-> "++integer_to_list(N)++";\n".
 
-top_and_tail(Acc)->
+top_and_tail(Acc1,Acc2)->
     Top="%% This module automatically generated - do not edit\n"++
 	"\n"++
 	"-module("++?MODULENAME++").\n"++
 	"\n"++
-	"-export([get_index/2]).\n"++
+	"-export([get_index/2,no_of_fields/1]).\n"++
 	"\n",
-    Tail="get_index(Record,_Field) -> exit({error,\"Invalid Record Name: \"++Record}).\n",
-    Top++lists:flatten(Acc)++Tail.
+    Tail1="no_of_fields(Other) -> exit({error,\"Invalid Record Name: \""++
+	"++Other}).\n",
+    Tail2="get_index(Record,_Field) -> exit({error,\""++
+	"Invalid Record Name: \"++Record}).\n",
+    Top++lists:flatten(Acc1)++Tail1++lists:flatten(Acc2)++Tail2.
 
