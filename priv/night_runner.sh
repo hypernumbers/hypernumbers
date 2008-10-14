816ff6c1989cd7a36510bf2355c233a608c4c2a3
Dale Harvey <dale@hypernumbers.com>
2008-10-14 13:40:24 +0000

    added test runner script
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@744 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ priv/night_runner.sh
@@ -0,0 +1,46 @@
+#!/bin/sh                                                                                                                                                                           
+
+DATE=`date +%Y-%m-%d.%T`
+
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export HOME=/home/hypernumbers
+REPO=http://svn.arandomurl.com/hypernumbers/code/trunk
+TESTDIR=hn_test_stage
+LASTRUN=$HOME/test_results/last_run/
+
+cd $HOME
+
+svn co $REPO $HOME/$TESTDIR
+
+cd $TESTDIR
+
+./compile_code
+
+cd priv/testserver
+
+ruby regen_tests.rb 1x
+ruby regen_tests.rb 2x
+
+cd ../../
+
+./hypernumbers test 1a
+./hypernumbers test 1b
+./hypernumbers test 1c
+./hypernumbers test 1d
+./hypernumbers test 1e
+./hypernumbers test 1f
+./hypernumbers test 2a
+./hypernumbers test 2b
+./hypernumbers test 2c
+./hypernumbers test 2d
+./hypernumbers test 2e
+./hypernumbers test 2f
+./hypernumbers test 2x
+
+rm -rf $LASTRUN
+mkdir $LASTRUN
+
+cp -r logs/* $LASTRUN
+cp logs/index.html $HOME/test_results/$DATE.html
+
+rm -rf $HOME/$TESTDIR
