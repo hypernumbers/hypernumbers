fcc368af75772e8a14f912ee7dee6d0968fdcbae
Dale Harvey <dale@hypernumbers.com>
2008-10-26 01:31:37 +0000

    added dirty supervisor
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@788 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ lib/hypernumbers-1.0/src/dirty_sup.erl
@@ -0,0 +1,18 @@
+-module(dirty_sup).
+-include("spriki.hrl").
+
+-behaviour(supervisor).
+
+-export([ start_link/0, init/1 ]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+
+    Cell = {dirty_cell,{dirty_srv,start_link,[dirty_cell]},
+            permanent,2000,worker,[start]},    
+    Hypn = {dirty_hypn,{dirty_srv,start_link,[dirty_hypernumber]},
+            permanent,2000,worker,[start]},
+
+    {ok,{{one_for_one,60,1}, [ Cell,Hypn ]}}.
