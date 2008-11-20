3d0fbebf011fbaf4f9769c14fe860cfb57dafa21
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-11-20 14:20:52 +0000

    Fix logical operators.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@888 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_logical.erl
+++ lib/formula_engine-1.0/src/stdfuns_logical.erl
@@ -26,17 +26,32 @@
 '<>'([V1, V2]) ->
     not('='([V1, V2])).
 
-'>'([A, B]) ->
-    A > B.
+%% string is greater than any number
+%% two strings are compared based on length
+%% blanks are simply cast to 0s
+%% true > false
+%% true > any number
+%% true > any string
+%% same for false...
+%% array < positive numbers, but not negative ones (WTF?)
+'>'([N1, N2]) when is_number(N1) andalso is_number(N2)   -> N1 > N2;
+'>'([S, N]) when ?is_string(S) andalso is_number(N)      -> true;
+'>'([S1, S2]) when ?is_string(S1) andalso ?is_string(S2) -> stdfuns_text:len([S1]) > stdfuns_text:len([S2]);
+'>'([true, _])                                     -> true;
+'>'([false, _])                                    -> true;
+'>'([A, N]) when ?is_area(A) andalso N > 0               -> true;
+'>'([A, N]) when ?is_area(A) andalso N =< 0               -> false;
+'>'([blank, X])                                    -> '>'([0, X]);
+'>'([X, blank])                                    -> '>'([X, 0]).
 
 '<'([A, B]) ->
-    A < B.
+    not('>'([A, B])).
 
 '<='([A, B]) ->
-    A =< B.
+    (A == B) orelse '<'([A, B]).
 
 '>='([A, B]) ->
-    A >= B.
+    (A == B) orelse '>'([A, B]).
 
 'and'(Vs) ->
     Flatvs = ?flatten_all(Vs),
