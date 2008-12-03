283551904133f61c6d7517e45b2abaf672ef59ae
Dale Harvey <dale@hypernumbers.com>
2008-12-03 21:34:22 +0000

    unbalanced tree based on dict
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@994 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ lib/hypernumbers-1.0/src/dh_tree.erl
@@ -0,0 +1,59 @@
+%% @author Dale Harvey <dale@hypernumbers.com>
+%% @copyright Hypernumbers Ltd.
+-module(dh_tree).
+
+-include("hypernumbers.hrl").
+-export([create/1, add/2, erase/2, flatlist/1]).
+
+-spec create(List::list(list(string()))) -> any().
+%% doc take a list of keys and generate a tree
+create(List) ->
+    create(List,dict:new()).
+create([],Dict) ->
+    Dict;
+create([H|T],Dict) ->
+    create(T,add(H,Dict)).
+
+-spec add(Key::list(string()),Dict::any()) -> any().
+%% @doc Add a key in the form ["key1","key2"] 
+%%      to the tree
+add([],Dict) -> 
+    Dict;
+add([H],Dict) ->
+    dict:store(H,dict:new(),Dict);
+add([H|T],Dict) ->
+    NDict = case dict:is_key(H,Dict) of
+                true  -> dict:fetch(H,Dict);
+                false -> dict:new()
+            end,
+    dict:store(H,add(T,NDict),Dict).
+
+-spec erase(Key::list(string()),Dict::any()) -> any().
+%% @doc erase a node from the tree 
+%%      (including children)
+erase([H],Dict) ->
+    dict:erase(H,Dict);
+erase([H|T],Dict) ->
+    dict:store(H,erase(T,dict:fetch(H,Dict)),Dict).
+             
+-spec flatlist(Dict::any()) -> {ok,list()}.
+%% @doc Generate a flat list of all the nodes
+%%      represented by the nested dict
+flatlist(Dict) ->
+    {ok,flatlist([],[],Dict)}.
+
+-spec flatlist(List::list(), Acc::list(), Dict::any()) ->
+    list(list(string())).
+%% @doc Generate a flat list of all the nodes
+%%      represented by the nested dict
+flatlist(List,Acc,Dict) ->
+    case dict:fetch_keys(Dict) of
+        [] ->
+            [Acc|List];
+        Keys ->
+            F = fun(X,NAcc) ->
+                        flatlist(NAcc,Acc++[X],dict:fetch(X,Dict))
+                end,
+            lists:foldl(F,[Acc|List],Keys)
+    end.
+
