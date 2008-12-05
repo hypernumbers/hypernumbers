3195ff3aa6893272827d3371d51dca7757b9b6b7
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2008-12-05 15:29:52 +0000

    change to conform to conventions used elsewhere in stdfuns: - helper function for FUNC is to be called FUNC1. - helper functions take exact numbers of arguments when possible.
    also, ?ensure_no_errvals is obsolete (& it says so in typechecks.hrl), so using IFERROR instead.
    
    
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1016 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_logical.erl
+++ lib/formula_engine-1.0/src/stdfuns_logical.erl
@@ -10,18 +10,52 @@
 
 -define(default_rules_bools, [cast_numbers, cast_strings, cast_blanks, cast_dates]).
 
-'='([A,B]) ->
-    ?ensure_no_errvals([A]),
-    ?ensure_no_errvals([B]),
-    eq([A,B]).
+'='([A, B]) -> iferror([A, A, iferror([B, B, '=1'(A, B)])]).
+%% numbers & numbers
+'=1'(N1, N2) when is_number(N1) andalso is_number(N2) -> N1 == N2;
+%% numbers & blanks
+'=1'(blank, N) when N == 0                      -> true;
+'=1'(N, blank) when N == 0                      -> true;
+%% numbers & bools
+'=1'(N, B) when is_number(N) andalso is_boolean(B)    -> false;
+'=1'(B, N) when is_number(N) andalso is_boolean(B)    -> false;
+%% strings & blanks
+'=1'(blank, S) when ?is_string(S)               -> stdfuns_text:len([S]) == 0;
+'=1'(S, blank) when ?is_string(S)               -> stdfuns_text:len([S]) == 0;
+%% bools & blanks
+'=1'(false, blank)                              -> true;
+'=1'(blank, true)                               -> true;
+%% strings & numbers
+'=1'(S, N) when ?is_string(S) andalso is_number(N)    -> false;
+'=1'(N, S) when ?is_string(S) andalso is_number(N)    -> false;
+%% default: true & blank, bool & bool, blank & blank, string & string.
+'=1'(V1, V2)                                    -> V1 == V2.
 
-'<>'([V1, V2]) ->
-    not('='([V1, V2])).
 
-'>'([A,B]) ->
-    ?ensure_no_errvals([A]),
-    ?ensure_no_errvals([B]),
-    gt([A,B]).
+'<>'([V1, V2]) -> not('='([V1, V2])).
+
+'>'([A, B]) -> iferror([A, A, iferror([B, B, '>1'(A, B)])]).
+'>1'(N1, N2) when is_number(N1) andalso is_number(N2)   -> N1 > N2;
+'>1'(S, N) when ?is_string(S) andalso is_number(N)      -> true;
+'>1'(N, S) when is_number(N) andalso ?is_string(S)      -> false;
+%% FIXME: 1) Will break on non Latin-1 strings.
+%%        2) Depends on Erlang's list representation of strings.
+'>1'(S1, S2) when ?is_string(S1) andalso ?is_string(S2) ->
+    if hd(S1) > hd(S2) -> true;  % compare alphabetically first
+       hd(S1) < hd(S2) -> false;
+       true            -> stdfuns_text:len([S1]) > stdfuns_text:len([S2]) % otherwise compare on length
+    end;
+'>1'(true, _)                                     -> true;
+'>1'(false, _)                                    -> true;
+'>1'(_, true)                                     -> false;
+'>1'(_, false)                                    -> false;
+'>1'(A, N) when ?is_area(A) andalso N > 0               -> true;
+'>1'(A, N) when ?is_area(A) andalso N =< 0               -> false;
+'>1'(blank, N) when is_number(N)                  -> '>1'(0, N);
+'>1'(N, blank) when is_number(N)                  -> '>1'(N, 0);
+'>1'(blank, S) when ?is_string(S)                 -> false;
+'>1'(S, blank) when ?is_string(S)                 -> true;
+'>1'(blank, blank)                                -> false.
 
 '<'([A, B]) -> '>'([B, A]).
 
@@ -49,59 +83,3 @@
 
 iferror([Test, TrueExpr, FalseExpr]) ->
     'if'([stdfuns_info:iserror([muin:eval(Test)]), TrueExpr, FalseExpr]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%                                                                              %
-% Helper functions - not to be exported, only to be called from the functions  %
-% that they help and not directly by other funtions in this suite!             %
-%                                                                              %
-% (It's an Excel-compatible error checking thing!)                             %
-%                                                                              %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%% helper function for '=' - call '=' from other functions and
-%% don't call eq directly
-%% numbers & numbers
-eq([N1, N2]) when is_number(N1) andalso is_number(N2) -> N1 == N2;
-%% numbers & blanks
-eq([blank, N]) when N == 0                           -> true;
-eq([N, blank]) when N == 0                           -> true;
-%% numbers & bools
-eq([N, B]) when is_number(N) andalso is_boolean(B)    -> false;
-eq([B, N]) when is_number(N) andalso is_boolean(B)    -> false;
-%% strings & blanks
-eq([blank, S]) when ?is_string(S)               -> stdfuns_text:len([S]) == 0;
-eq([S, blank]) when ?is_string(S)               -> stdfuns_text:len([S]) == 0;
-%% bools & blanks
-eq([false, blank])                              -> true;
-eq([blank, true])                               -> true;
-%% strings & numbers
-eq([S, N]) when ?is_string(S) andalso is_number(N)    -> false;
-eq([N, S]) when ?is_string(S) andalso is_number(N)    -> false;
-%% default: true & blank, bool & bool, blank & blank, string & string.
-eq([V1, V2])                                    -> V1 == V2.
-
-
-%% helper function for '>' - call '>' from other functions and
-%% don't call gt directly
-gt([N1, N2]) when is_number(N1) andalso is_number(N2)   -> N1 > N2;
-gt([S, N]) when ?is_string(S) andalso is_number(N)      -> true;
-gt([N, S]) when is_number(N) andalso ?is_string(S)      -> false;
-%% FIXME: 1) Will break on non Latin-1 strings.
-%%        2) Depends on Erlang's list representation of strings.
-gt([S1, S2]) when ?is_string(S1) andalso ?is_string(S2) ->
-    if hd(S1) > hd(S2) -> true;  % compare alphabetically first
-       hd(S1) < hd(S2) -> false;
-       true            -> stdfuns_text:len([S1]) > stdfuns_text:len([S2]) % otherwise compare on length
-    end;
-gt([true, _])                                     -> true;
-gt([false, _])                                    -> true;
-gt([_, true])                                     -> false;
-gt([_, false])                                    -> false;
-gt([A, N]) when ?is_area(A) andalso N > 0         -> true;
-gt([A, N]) when ?is_area(A) andalso N =< 0        -> false;
-gt([blank, N]) when is_number(N)                  -> '>'([0, N]);
-gt([N, blank]) when is_number(N)                  -> '>'([N, 0]);
-gt([blank, S]) when ?is_string(S)                 -> false;
-gt([S, blank]) when ?is_string(S)                 -> true;
-gt([blank,blank])                                 -> false.
