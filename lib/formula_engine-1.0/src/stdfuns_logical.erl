8cef318b018724859808d17d0d6bb605b5128efd
Gordon Guthrie <gordon@hypernumbers.com>
2008-12-07 23:35:22 +0000

    Logical operators fixes...
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1046 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_logical.erl
+++ lib/formula_engine-1.0/src/stdfuns_logical.erl
@@ -10,7 +10,9 @@
 
 -define(default_rules_bools, [cast_numbers, cast_strings, cast_blanks, cast_dates]).
 
-'='([A, B]) -> iferror([A, A, iferror([B, B, '=1'(A, B)])]).
+'='([A, B]) -> muin_checks:die_on_errval([A]),
+               muin_checks:die_on_errval([B]),
+               '=1'(A, B).
 %% numbers & numbers
 '=1'(N1, N2) when is_number(N1) andalso is_number(N2) -> N1 == N2;
 %% numbers & blanks
@@ -24,17 +26,21 @@
 '=1'(S, blank) when ?is_string(S)               -> stdfuns_text:len([S]) == 0;
 %% bools & blanks
 '=1'(false, blank)                              -> true;
-'=1'(blank, true)                               -> true;
+'=1'(blank, false)                              -> true;
+'=1'(true, blank)                               -> false;
+'=1'(blank, true)                               -> false;
 %% strings & numbers
 '=1'(S, N) when ?is_string(S) andalso is_number(N)    -> false;
 '=1'(N, S) when ?is_string(S) andalso is_number(N)    -> false;
 %% default: true & blank, bool & bool, blank & blank, string & string.
 '=1'(V1, V2)                                    -> V1 == V2.
 
-
 '<>'([V1, V2]) -> not('='([V1, V2])).
 
-'>'([A, B]) -> iferror([A, A, iferror([B, B, '>1'(A, B)])]).
+'>'([A, B]) -> muin_checks:die_on_errval([A]),
+               muin_checks:die_on_errval([B]),
+               '>1'(A, B).
+
 '>1'(N1, N2) when is_number(N1) andalso is_number(N2)   -> N1 > N2;
 '>1'(S, N) when ?is_string(S) andalso is_number(N)      -> true;
 '>1'(N, S) when is_number(N) andalso ?is_string(S)      -> false;
@@ -49,8 +55,8 @@
 '>1'(false, _)                                    -> true;
 '>1'(_, true)                                     -> false;
 '>1'(_, false)                                    -> false;
-'>1'(A, N) when ?is_area(A) andalso N > 0               -> true;
-'>1'(A, N) when ?is_area(A) andalso N =< 0               -> false;
+'>1'(A, N) when ?is_area(A) andalso N > 0         -> true;
+'>1'(A, N) when ?is_area(A) andalso N =< 0        -> false;
 '>1'(blank, N) when is_number(N)                  -> '>1'(0, N);
 '>1'(N, blank) when is_number(N)                  -> '>1'(N, 0);
 '>1'(blank, S) when ?is_string(S)                 -> false;
