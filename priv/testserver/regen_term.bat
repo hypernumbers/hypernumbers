076221741a5c9a029736b46f2919d4f85dfce1a5
Gordon Guthrie <gordon@hypernumbers.com>
2009-02-13 23:29:33 +0000

    Some changes to the test suite.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1216 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/testserver/regen_term.bat
+++ priv/testserver/regen_term.bat
@@ -1,1 +1,2 @@
 ruby regen_termfiles.rb "C:\\Documents and Settings\\Gordon Guthrie\\My Documents\\SVN\\trunk\\tests\\excel_files\\Win_Excel07_As_97"
+move *.dat ..\..\tests\excel_files\Win_Excel07_As_97\DATA\
