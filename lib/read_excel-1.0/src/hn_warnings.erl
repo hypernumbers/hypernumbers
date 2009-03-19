e13da649edb23e0d0ac2475c3a8e12c0dfb4c1ff
Gordon Guthrie <gordon@hypernumbers.com>
2009-03-19 10:27:16 +0000

    Fixed a bug that caused some of the type 2 test suites to fail this morning
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1335 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/read_excel-1.0/src/hn_warnings.erl
+++ lib/read_excel-1.0/src/hn_warnings.erl
@@ -14,9 +14,13 @@ warnings(FuncVar, [H | _T] , Tbl) -> warnings1(FuncVar, H, Tbl);
 warnings(FuncVar, [], Tbl)        -> warnings1(FuncVar, [], Tbl).
 
 warnings1(FuncVar, H, Tbl) ->
+
+    % if FuncVar is 255 then this is not an Excel function but is a function
+    % whose name is the head of the arguments list...
     FuncName = if
-                  (FuncVar >  255) -> H;
-                  (FuncVar =< 255) -> excel_rev_comp:macro_to_string_WARNING(FuncVar)
+                   (FuncVar == 255) -> excel_rev_comp:macro_to_string_WARNING(FuncVar);
+                   true             -> {string, Name} = H,
+                                       Name               
               end,
     
     F1 = ["DAY",
@@ -101,7 +105,7 @@ warnings1(FuncVar, H, Tbl) ->
                      true  -> ok;
                      false -> excel_util:append(Tbl, warnings, "Function "++FuncName++
                                                 " is used in your spreadsheet but "++
-                                                "not supported in Hypernumbers"++
+                                                "not supported in Hypernumbers "++
                                                 "at the moment!")
                  end
     end.
