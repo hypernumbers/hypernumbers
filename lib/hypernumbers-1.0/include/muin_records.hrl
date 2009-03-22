3f76b2c1ab6f6f65a8dd1fe8e27180be037cb6a0
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2009-03-22 16:58:29 +0000

    Add records to represent cell, range and named expression references in Muin.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1348 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/hypernumbers-1.0/include/muin_records.hrl
+++ lib/hypernumbers-1.0/include/muin_records.hrl
@@ -1,13 +1,32 @@
 %%% @doc Various records used in Muin.
 %%% @author Hasan Veldstra <hasan@hypernumbers.com>
 
-%% Runtime information.
--record(muin_rti,
-        {site, path, col, row, array_context = false}).
+%% Runtime information: describes the context in which to run a formula.
+-record(muin_rti, {
+          site, path, col, row,   % target cell
+          array_context = false   % array formula?
+         }).
 
-%% DateTime record.
-%% Date and time fields are in the format expected by functions in the
-%% calendar module.
--record(datetime, {date = {0, 0, 0},   % year, month, day
-                   time = {0, 0, 0}}). % hour, minute, second
+%% DateTime: fields are in the format expected by functions in the
+%% calendar module in stdlib.
+-record(datetime, {
+          date = {0, 0, 0},   % year, month, day
+          time = {0, 0, 0}    % hour, minute, second
+        }).
 
+%% col & row are either integers, or {offset, integer()}
+-record(cellref, {
+          col, row,       % pos_integer()|{offset, pos_integer()}
+          path,           % string like "./" or "/foo/bar"
+          text = ""       % token text (used in the parser for function names like ATAN2)
+         }).
+
+-record(rangeref, {
+          path,         % like in #cellref{}
+          tl, br        % coords like in #cellref | {col|row, pos_integer()|{offset, pos_integer()}}
+         }).
+
+-record(namedexpr, {
+          path,
+          text
+         }).
