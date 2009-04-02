289021f3530b15ed14effab9ac33f05d5a1cb7d8
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2009-04-02 17:29:34 +0000

    Add type field to #rangeref to easily distinguish between types of ranges.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1429 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/hypernumbers-1.0/include/muin_records.hrl
+++ lib/hypernumbers-1.0/include/muin_records.hrl
@@ -22,6 +22,7 @@
          }).
 
 -record(rangeref, {
+          type,         % finite|col|row
           path,         % like in #cellref{}
           tl, br,       % coords like in #cellref | {col|row, pos_integer()|{offset, pos_integer()}}
           text = ""     % token text
