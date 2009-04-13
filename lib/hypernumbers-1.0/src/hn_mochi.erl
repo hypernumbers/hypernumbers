06e6269e39bbaa075bd06af4f88f378e0f874820
Gordon Guthrie <gordon@hypernumbers.com>
2009-04-13 21:26:29 +0000

    Bug that crashed the test suite fixed again (fix put in in r1501 and then deleted in r1510)
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1522 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/hypernumbers-1.0/src/hn_mochi.erl
+++ lib/hypernumbers-1.0/src/hn_mochi.erl
@@ -106,8 +106,11 @@ iget(Req, Ref, cell, [{"attr", []}]) ->
     ?json(Req, JS);
 iget(Req, Ref, cell, []) ->
     V = case hn_db_api:read_attributes(Ref,["value"]) of
-            [{_, {"value", Val}}] -> Val;
-            []                    -> ""
+            [{_Ref, {"value", {errval, Val}}}] -> atom_to_list(Val); 
+            [{_Ref, {"value", true}}]          -> "true"; 
+            [{_Ref, {"value", false}}]         -> "false"; 
+            [{_Ref, {"value", Val}}]           -> Val; 
+            _Else                              -> "" 
         end,
     Req:ok({"text/html",V});
 iget(Req, Ref, _Type,  Attr) ->
