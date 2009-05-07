c09ac891745adb32fa2ad1ca228c8f7055c695ac
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2009-05-07 15:24:23 +0000

    Return reference, not its value.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1671 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_lookup_ref.erl
+++ lib/formula_engine-1.0/src/stdfuns_lookup_ref.erl
@@ -21,13 +21,15 @@ column([])                      -> muin:context_setting(col);
 column([C]) when ?is_cellref(C) -> muin:col_index(muin:col(C));
 column(_)                       -> ?ERR_VAL.
 
+
 %% TODO: Needs to be recompiled every time -- how to handle that cleanly?
 %% (without writing to proc dict)
 indirect([S]) ->
     ?ensure(?is_string(S), ?ERR_REF),
     {ok, Ast} = muin:parse(
                   S, {muin:context_setting(col), muin:context_setting(row)}),
-    muin:eval_formula(Ast).
+    muin:eval(Ast). % want to return a ref here, not a value.
+
 
 row([])                      -> muin:context_setting(row);
 row([C]) when ?is_cellref(C) -> muin:row_index(muin:row(C));
