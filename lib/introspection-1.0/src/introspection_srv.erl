17dfd034073c90a2426769d7ee08fd005ae8cfe1
Gordon Guthrie <gordon@hypernumbers.com>
2009-05-22 14:51:49 +0000

    Better documented.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1794 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/introspection-1.0/src/introspection_srv.erl
+++ lib/introspection-1.0/src/introspection_srv.erl
@@ -1,5 +1,15 @@
 %%% @author Hasan Veldstra <hasan@hypernumbers.com>
 %%% @doc Introspection server.
+%%% The Introspection Application starts in a passive mode and responds to the
+%%% API function toggle_mode to switch it to active mode
+%%% In passive mode it is used to take memory snapshots. In active mode it 
+%%% takes a timeseries of snapshots based on the time specified in the macro
+%%% CLOCKTICK.
+%%% TODO: Accept messages from system_monitor().
+%%% TODO: Use disk_log.
+%%% TODO: Allow the clocktick to be set via the API
+%%% TODO: If you manually call take_snapshot on an active server
+%%%       you will get two interleaved traces in the dump file...
 
 -module(introspection_srv).
 -behaviour(gen_server).
@@ -25,9 +35,6 @@
          }).
 -define(SERVER, ?MODULE).
 
-%%% TODO:  Accept messages from system_monitor().
-%%% TODO:  Use disk_log.
-
 %%% Properties to be read for each process when a snapshot is taken:
 -define(SNAPSHOT_PROPERTIES, [registered_name, total_heap_size, current_function, 
                               initial_call]).
@@ -42,19 +49,19 @@
 %%% gen_server API
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%% @hidden
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
-%%% Init -- open logfile.
-
+%% Init -- open logfile.
+%% @hidden
 init([]) ->
     {ok, FileId} = file:open(?LOGFILE, [append]),
     {ok, #state{file_id = FileId}}.
 
 
-%%% Handle call messages.
-
+%% Handle call messages.
+%% @hidden
 handle_call({set_top, N}, _From, S) ->
     NewS = S#state{top = N},
     {reply, ok, NewS};
@@ -75,24 +82,25 @@ handle_call({take_snapshot, N}, _From, S) ->
     end,
     {reply, ok, S#state{top = N}}.
 
-%%% Handle cast messages.
-
+%% Handle cast messages.
+%% @hidden
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
 
-%%% Handle non-call/cast messages.
-
+%% Handle non-call/cast messages.
+%% @hidden
 handle_info(_Info, State) ->
   {noreply, State}.
 
 
-%%% Terminate -- close the logfile.
-
+%% Terminate -- close the logfile.
+%% @hidden
 terminate(_Reason, S) ->
     ok = file:close(S#state.file_id),
     ok.
 
+%% @hidden
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
@@ -103,15 +111,30 @@ code_change(_OldVsn, State, _Extra) ->
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%% @spec set_top(N) -> Return
+%% N = integer()
+%% @doc set_top takes an integer value and retricts the operation of the snap
+%% shot to the N process with the largest memory utilisation
 set_top(N) when is_integer(N) andalso (N > 0) ->
     gen_server:call(?SERVER, {set_top, N}).    
 
+%% @spec take_snapshot(N) -> Return
+%% N = [ integer() | all]
+%% @doc take_snapshot/1 takes a snapshot of the N most memory-intensive. If the atom
+%% all is passed it it takes a snapshot of all the processes. The value of N
+%% is stored so that (if the server is in active mode) it is the value used
+%% by the tick function
 take_snapshot(N) when (is_integer(N) andalso (N > 0)) orelse (N == all) ->
     gen_server:call(?SERVER, {take_snapshot, N}).
     
+%% @spec take_snapshot() -> Return
+%% @doc take_snapshot/0 is the same as take_snapshot(all).
 take_snapshot() ->
     gen_server:call(?SERVER, {take_snapshot, all}).
 
+%% @spec toggle_mode(Mode) -> Return
+%% Mode = [active | passive]
+%% a passive introspection server just responds to a single 
 toggle_mode(Mode) when (Mode == active) orelse (Mode == passive) ->
     gen_server:call(?SERVER, {toggle_mode, Mode}).
 
@@ -121,13 +144,13 @@ toggle_mode(Mode) when (Mode == active) orelse (Mode == passive) ->
 %%% Internal Functions
 %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+%%% @hidden
 tick(N) -> 
     timer:sleep(?CLOCKTICK),
     take_snapshot(N).
     
-%%% @doc Take snapshot of all running processes.    
-
+%% @hidden
+%% @doc Take snapshot of all running processes.    
 snapshot(N) ->
     {Day, Time} = calendar:now_to_universal_time(erlang:now()),
     Snapshot = get_system_info(),
