ecae0c92f27a8689bafc1d40dfa122a78f623428
hasanveldstra <hasanveldstra@b744bbc5-f124-0410-b3d9-bb4a5f45712a>
2009-06-01 14:49:57 +0000

    Minor edits.
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@1868 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/stdfuns_info.erl
+++ lib/formula_engine-1.0/src/stdfuns_info.erl
@@ -1,6 +1,4 @@
 %%% @doc Built-in information functions.
-%%% CELL, INFO, and ISREF are implemented in the evaluator because they need
-%%% higher-level information.
 %%% @author Hasan Veldstra <hasan@hypernumbers.com>
 %%% @private
 
@@ -90,6 +88,7 @@ errortype([?ERRVAL_NUM])  -> 6;
 errortype([?ERRVAL_NA])   -> 7;
 errortype(_)              -> ?ERR_NA.
 
+
 %% Returns the logical value TRUE if value refers to any error value except
 %% #N/A; otherwise it returns FALSE.
 iserr([?ERRVAL_NULL]) -> true;
@@ -99,6 +98,7 @@ iserr([?ERRVAL_REF])  -> true;
 iserr([?ERRVAL_NAME]) -> true;
 iserr([?ERRVAL_NUM])  -> true;
 iserr(_)              -> false.
+
 
 %% Returns true if argument is any error value.
 iserror([?ERRVAL_NULL]) -> true;
@@ -110,6 +110,7 @@ iserror([?ERRVAL_NUM])  -> true;
 iserror([?ERRVAL_NA])   -> true;
 iserror(_)              -> false.
 
+
 %% Returns TRUE if number is even, or FALSE if number is odd.
 %% The number is truncated, so ISEVEN(2.5) is true.
 %% @todo needs a test case written because it is not an Excel 97 function
@@ -117,8 +118,10 @@ iseven([V1]) ->
     Num = ?number(V1, [cast_strings, cast_bools, cast_dates]),
     (trunc(Num) div 2) * 2 == trunc(Num).
 
+
 %% @todo needs a test case written because it is not an Excel 97 function
 isodd([Num]) -> not(iseven([Num])).
+
 
 %% Returns true only for booleans or arrays where element (1,1) is a boolean.
 %% (Returns false for ranges regardless of what's in them.)
@@ -127,14 +130,17 @@ islogical([B]) when is_boolean(B) -> true;
 islogical(A) when ?is_array(A)    -> is_boolean(area_util:at(1, 1, A));
 islogical(_)                      -> false.
 
+
 isna([{errval, '#N/A'}]) -> true;
 isna(X)                  -> io:format("in stdfuns_info:isna X is ~p~n", [X]),
                             false.
+
 
 isnontext([X]) -> case istext([X]) of
                       true  -> false;
                       false -> true
                   end.
+
 
 isnumber([X]) when is_number(X) -> true;
 isnumber([_])                  -> false.
@@ -142,6 +148,7 @@ isnumber([_])                  -> false.
 
 istext([X]) when ?is_string(X) -> true;  %% complement(fun isnontext/1)
 istext([_])                    -> false.
+
 
 %% TODO: dates.
 n([Num]) when is_number(Num) -> Num;
@@ -154,7 +161,7 @@ n(X)                         -> io:format("in n X is ~p~n", [X]),
     
 na([]) -> {errval, '#N/A'}.
 
-%% ~~~~~ INCOMPATIBILITY NOTE:
+
 %% TYPE(A1:B10) in Excel = 16, in Hypernumbers it's 64.
 %% TYPE(INDIRECT("A1")) in Excel = 0 regardless of contents of A1. In Hypernumbers it's same as TYPE(A1)
 type([A]) when ?is_area(A)   -> 64;
@@ -165,19 +172,23 @@ type([{errval, _X}])         -> 16;
 type([blank])                -> 1;
 type(_)                      -> 0.
 
+
 isblank([blank]) -> true;
 isblank(Vs)      -> Flatvs = ?flatten_all(Vs),
                     all(fun muin_collect:is_blank/1, Flatvs).
 
+
 %% @todo needs a test case written because it is not an Excel 97 function
 isnonblank(Vs) -> Flatvs = ?flatten_all(Vs),
                   all(fun(X) -> not(muin_collect:is_blank(X)) end, Flatvs).
+
 
 info(["site"]) -> get(site);
 info(["path"]) -> case "/" ++ string:join(get(path), "/") ++ "/" of
                       "//" -> "/";
                       V    -> V
                   end.
+
 
 rows([A]) when ?is_area(A) -> area_util:height(A);
 rows([_])                  -> 1;
