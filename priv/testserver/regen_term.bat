cd4c3540d9d4522d96f8594ea7acaa538deb5504
Dale Harvey <dale@hypernumbers.com>
2009-07-13 21:14:23 +0000

    removed spaces from test suite
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@2049 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/testserver/regen_term.bat
+++ priv/testserver/regen_term.bat
@@ -1,2 +1,2 @@
-ruby regen_termfiles.rb "C:\\Documents and Settings\\Gordon Guthrie\\My Documents\\SVN\\trunk\\tests\\excel_files\\Win_Excel07_As_97"
+ruby regen_termfiles.rb "C:\\Documents and Settings\\dale\\Desktop\\hypernumbers\\tests\\excel_files\\Win_Excel07_As_97"
 move *.dat ..\..\tests\excel_files\Win_Excel07_As_97\DATA\
