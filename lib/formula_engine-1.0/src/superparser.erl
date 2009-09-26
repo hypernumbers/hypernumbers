a8242dc0465c3c2ce4b978977feda6e9abfdb4d9
Dale Harvey <dale@hypernumbers.com>
2009-09-26 16:33:01 +0000

    allow entering negative numbers in parethesis
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@2345 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/superparser.erl
+++ lib/formula_engine-1.0/src/superparser.erl
@@ -15,39 +15,35 @@ process(Input) ->
     % in this case {1, 1} is used because the results of this
     % are not actually going to be used here (ie {1, 1} is a dummy!)
     {ok, Toks} = xfl_lexer:lex(super_util:upcase(Input), {1, 1}),
+
     case Toks of
-        [{bool, B}]                -> [{bool, B},
-                                       {"text-align", "center"},
-                                       {"format", "null"}];
-        [{float, {F, _OrigStr}}]     -> [{float, F},
-                                       {"text-align", "right"},
-                                       {"format", "null"}];
-        [{int, I}]                 -> [{int, I},
-                                       {"text-align", "right"},
-                                       {"format", "null"}];
-        [{'-'}, {float, {F, _OrigStr}}] -> [{float, -F},
-                                          {"text-align", "right"},
-                                          {"format", "null"}];
-        [{'-'}, {int, I}]          -> [{int, -I},
-                                       {"text-align", "right"},
-                                       {"format", "null"}];
-        [{float, {F, _OrigStr}}, {'%'}] -> [{float,F/100},
-                                          {"text-align", "right"},
-                                          {"format", "0.00%"}];
-        [{int, I}, {'%'}]          -> [{float,I/100},
-                                       {"text-align", "right"},
-                                       {"format", "0%"}];
-        [{'-'}, {float, {F, _OrigStr}}, {'%'}] -> [{float,F/100},
-                                                 {"text-align", "right"},
-                                                 {"format", "0.00%"}];
-         [{'-'}, {int, I}, {'%'}]   -> [{float,I/100},
-                                       {"text-align", "right"},
-                                       {"format", "0%"}];
+        [{bool, B}] ->
+            [{bool, B}, {"text-align", "center"}, {"format", "null"}];
+        [{float, {F, _OrigStr}}] ->
+            [{float, F}, {"text-align", "right"}, {"format", "null"}];
+        [{'('},{float, {F, _OrigStr}},{')'}] ->
+            [{float, -F}, {"text-align", "right"}, {"format", "null"}];
+        [{int, I}] ->
+            [{int, I}, {"text-align", "right"}, {"format", "null"}];
+        [{'('}, {int, I}, {')'}] ->
+            [{int, -I}, {"text-align", "right"}, {"format", "null"}];
+        [{'-'}, {float, {F, _OrigStr}}] ->
+            [{float, -F}, {"text-align", "right"}, {"format", "null"}];
+        [{'-'}, {int, I}] ->
+            [{int, -I}, {"text-align", "right"}, {"format", "null"}];
+        [{float, {F, _OrigStr}}, {'%'}] -> 
+            [{float,F/100}, {"text-align", "right"}, {"format", "0.00%"}];
+        [{int, I}, {'%'}] ->
+            [{float,I/100}, {"text-align", "right"}, {"format", "0%"}];
+        [{'-'}, {float, {F, _OrigStr}}, {'%'}] ->
+            [{float,F/100}, {"text-align", "right"}, {"format", "0.00%"}];
+         [{'-'}, {int, I}, {'%'}]   ->
+            [{float,I/100}, {"text-align", "right"}, {"format", "0%"}];
         % type tag gets discarded by caller which is ok for 
         % the rest of them, but not here
-        [{errval, E}]       -> [{errval, {errval, E}}, 
-                                {"text-align", "center"},
-                                {"format", "null"}]; 
+        [{errval, E}] ->
+            [{errval, {errval, E}}, {"text-align", "center"},
+             {"format", "null"}]; 
         _Other              ->
                  case super_util:autoparse(Toks) of
                      {ok, maybe_bad_date} ->
