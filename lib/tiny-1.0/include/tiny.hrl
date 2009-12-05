d4f63d9ee2524055b012fae7bc7a738fd8d17ae9
Gordon Guthrie <gordon@hypernumbers.com>
2009-12-05 15:16:11 +0000

    (Duh!, actually commit the stuff) Start of provisioning code
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@2653 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ lib/tiny-1.0/include/tiny.hrl
@@ -0,0 +1,16 @@
+-record(tiny_sub_domains,
+        {
+          sub = [],
+          allocated = false
+         }).
+
+-record(run_details,
+        {
+          sub,
+          domain,
+          port,
+          email,
+          password
+         }).
+
+
