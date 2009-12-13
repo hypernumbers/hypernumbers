07cbf8a4ed9e96ba27c692ee70328be7962f2161
Dale Harvey <dale@hypernumbers.com>
2009-12-13 15:17:26 +0000

    cleaning up repo
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@2759 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/docbook_gen/gen_docs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd ./docbook
-xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl --output book.html book.xml
-xsltproc http://docbook.sourceforge.net/release/xsl/current/html/docbook.xsl --output gui.html gui.xml
-# Super Shonky! from GG the shonkmaster....
-sed 's/<\/head>/<\/head><link rel="stylesheet" type="text\/css" href="..\/css\/book.css" \/>/' book.html > ../html/book.html
-sed 's/<\/head>/<\/head><link rel="stylesheet" type="text\/css" href="..\/css\/book.css" \/>/' gui.html > ../html/gui.html
-cd ..
