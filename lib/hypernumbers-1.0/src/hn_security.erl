c3cdde9dcfe4513ba18d1783194854baffa18877
Gordon Guthrie <gordon@hypernumbers.com>
2009-12-16 11:32:43 +0000

    hn_security (partial commit)
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@2798 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/hypernumbers-1.0/src/hn_security.erl
+++ lib/hypernumbers-1.0/src/hn_security.erl
@@ -8,11 +8,20 @@
 %%%-------------------------------------------------------------------
 -module(hn_security).
 
+%% API
+-export([
+         make_security/1,
+         is_approved/3
+        ]).
+
+
+%% debugging API
 -export([run/0]).
 
 -compile(export_all).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("spriki.hrl").
 -include("security.hrl").
 
 %% this is how a form posts...
@@ -30,13 +39,79 @@
 %% in ipost Array is [{struct,[{"ref","/u/gordon/blah/D:D"},{"formula","Test"}]}]
 %% Ref is {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]}
 
-run() ->
-    File = "/opt/code/trunk/lib/hypernumbers-1.0/priv/docroot/"
-        ++ "views/x.tiny.hn_9000/_u/anonymous/tiny.tpl",
+is_approved(_RefX, _Json, [])    -> false;
+is_approved(RefX, Json, [H | T]) ->
+    case is_approved2(RefX, Json, H) of
+        true  -> true;            
+        false -> is_approved(RefX, Json, T)
+    end.
 
+is_approved2(RefX, Json, Transaction) ->
+    io:format("RefX is ~p~nJson is ~p~nTransaction is ~p~n", [RefX, Json, Transaction]),
+    Approved = make_approved(RefX, Transaction),
+    io:format("Approved is ~p~n", [Approved]),
+    Candidate = make_candidate(Json),
+    io:format("Candidate is ~p~n", [Candidate]),
+    is_app3(Candidate, Approved).
+
+%% both lists must be the same lentgh
+is_app3([], [])                              -> true;
+is_app3(_X, [])                              -> false;
+is_app3([], _X)                              -> false;
+is_app3([{P, _} | T1], [{P, anything} | T2]) -> is_app3(T1, T2);
+is_app3([{P, V} | T1], [{P, L} | T2])        -> case lists:member(V, L) of
+                                                    true  -> is_app3(T1, T2);
+                                                    false -> false
+                                                end;
+is_app3([{_P1, _} | _T1], [{_P2, _} | _T2])  -> false. % testing _P1 =/= _P2
+
+make_candidate(Json) -> lists:keysort(1, make_c2(Json, [])).
+
+make_c2([], Acc) -> Acc;
+make_c2([{struct, [{"ref", Path}, {"formula", Val}]} | T], Acc) ->
+    make_c2(T, [{Path, Val} | Acc]).
+
+make_approved(RefX, Transaction) -> lists:keysort(1, make_a2(RefX, Transaction, [])).
+
+make_a2(_RefX, [], Acc)      -> Acc;
+make_a2(RefX,  [H | T], Acc) -> make_a2(RefX, T, [make_a3(RefX, H) | Acc]).
+
+make_a3(#refX{path = P}, #binding{type = Type, to = To}) ->
+    io:format("P is ~p Type is ~p To is ~p~n", [P, Type, To]),
+    P2 = make_abs_path(P, To),
+    {P2, allowed_values(P2, Type)}.
+
+allowed_values(_Path, "text")     -> "lookup text";
+allowed_values(_Path, "input")    -> anything;
+allowed_values(_Path, "textares") -> anything;
+allowed_values(_Path, "select")   -> "lookup select";
+allowed_values(_Path, "radio")    -> "lookup radio";
+allowed_values(_Path, "checkbox") -> "lookup checkbox".
+
+make_abs_path(Path, [$. | _T] = Path2)  -> make_abs2(Path, Path2);
+make_abs_path(_Path, [$/ | _T] = Path2) -> Path2;
+make_abs_path(Path, Path2)              -> hn_util:list_to_path(Path) ++ Path2.
+
+make_abs2(Path, Path2) ->
+    R = lists:reverse(Path),
+    P3 = string:tokens(Path2, "/"),
+    {P4, [Ref]} = lists:split(length(P3) - 1, P3),
+    NewPath = make_abs3(R, P4),
+    NewPath ++ Ref.
+
+make_abs3(Path, [])                -> hn_util:list_to_path(lists:reverse(Path));
+make_abs3(Path, ["." | T2])        -> make_abs3(Path, T2);
+make_abs3([_H1 | T1], [".." | T2]) -> make_abs3(T1, T2);
+make_abs3(Path, [H2 | T2])         -> make_abs3([H2 | Path], T2).
+
+make_security(File) ->
+
+    % tpls aren't always well formed XML, but
+    % wrapping them in a '<div></div>' pair makes 'em so
+    File2 = "<div>" ++ File ++ "</div>",
     Opts = [{event_fun, fun process/3}],
 
-    {ok, List, _} = xmerl_sax_parser:file(File, Opts),
+    {ok, List, _} = xmerl_sax_parser:stream(File2, Opts),
     make_transactions(lists:reverse(List)).
 
 process(X, Y, undefined) -> process(X, Y, []);
@@ -71,12 +146,39 @@ make_transactions(List) -> make_t1(List, [], [], false).
 %% when the form ends we throw the contents of A1 into the main accumulator in A2
 %% if the boolean is false, then each binding is a transaction in its own right
 %% expects forms to terminate in the model or will crash
-make_t1([], [], A2, false)              -> lists:reverse(A2);
+make_t1([], [], A2, false)              -> post_only(A2);
 make_t1([formstart | T], A1, A2, false) -> make_t1(T, A1, A2, true);
 make_t1([formend | T], A1, A2, true)    -> make_t1(T, [], [A1 | A2], false);
 make_t1([H | T], A1, A2, true)          -> make_t1(T, [H | A1], A2, true);
-make_t1([H | T], A1, A2, false)         -> make_t1(T, A1, [H | A2], false).
+make_t1([H | T], A1, A2, false)         -> make_t1(T, A1, [[H | []] | A2], false).
 
+%% if the binding is 'to' and empy list then we chuck it away
+%% a transaction can be a single binding tuple or a list of bindingd tuples
+post_only(List) -> post_only2(List, []).
+
+post_only2([], Acc)      -> Acc;
+post_only2([H | T], Acc) -> case post_only3(H, []) of
+                                []     -> post_only2(T, Acc);
+                                NewAcc -> post_only2(T, [NewAcc | Acc])
+                            end.
+
+post_only3([], Acc)                      -> Acc;
+post_only3([#binding{to = []} | T], Acc) -> post_only3(T, Acc);
+post_only3([H | T], Acc)                 -> post_only3(T, [H | Acc]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%                                                                          %%%
+%%% Debugging API                                                            %%%
+%%%                                                                          %%% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+run() ->
+    File = "/opt/code/trunk/lib/hypernumbers-1.0/priv/docroot/"
+        ++ "views/x.tiny.hn_9000/_u/anonymous/tiny.tpl",
+    Opts = [{event_fun, fun process/3}],
+
+    {ok, List, _} = xmerl_sax_parser:file(File, Opts),
+    make_transactions(lists:reverse(List)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                                                                          %%%
@@ -84,26 +186,252 @@ make_t1([H | T], A1, A2, false)         -> make_t1(T, A1, [H | A2], false).
 %%%                                                                          %%% 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 test1() ->
-    _Input = [{struct,[{"ref","/u/gordon/blah/A:A"},
-                      {"formula","Field 1"}]},
-             {struct,[{"ref","/u/gordon/blah/B:B"},
-                      {"formula","Field 2"}]},
-             {struct,[{"ref","/u/gordon/blah/C:C"},
-                      {"formula","Field 3"}]},
-             {struct,[{"ref","/u/gordon/blah/D:D"},
-                      {"formula","Field 4"}]}],
-    _Ref   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
-    _Form  = "<div id='ventris'><link rel='stylesheet' type='text/css' href='/templates/ventris/default.css'></link><div id='outer'><div id='inner'><div class='clear'><div id='header'><div data-binding-from='/A1' class='hn header' data-type='text'></div><div id='menu'></div></div></div><div id='contentwrapper'><div id='primarycontent'></div><div id='secondarycontent'><form class='hn' data-type='form'><label><span><div data-binding-from='A3' class='hn' data-type='text'></div></span></label><div class='hn' data-type='input' data-binding-to='A:A'></div><label><span><div data-binding-from='B3' class='hn' data-type='text'></div></span></label><div class='hn' data-type='input' data-binding-to='B:B'></div><label><span><div data-binding-from='C3' class='hn' data-type='text'></div></span></label><div class='hn' data-type='input' data-binding-to='C:C'></div><label><span><div data-binding-from='D3' class='hn' data-type='text'></div></span></label><div class='hn' data-type='input' data-binding-to='D:D'></div><input type='submit' value='submit'></input></form></div></div><div id='footer'></div></div></div></div>",
-    false.
-
+    Json = [{struct,[{"ref","/u/gordon/blah/A:A"},
+                     {"formula","Field 1"}]},
+            {struct,[{"ref","/u/gordon/blah/B:B"},
+                     {"formula","Field 2"}]},
+            {struct,[{"ref","/u/gordon/blah/C:C"},
+                     {"formula","Field 3"}]},
+            {struct,[{"ref","/u/gordon/blah/D:D"},
+                     {"formula","Field 4"}]}],
+    RefX   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
+    Form  = "<div id='ventris'>"
+        ++ "<link rel='stylesheet' type='text/css' href='/templates/ventris/default.css'>"
+        ++ "</link>"
+        ++ "<div id='outer'>"
+        ++ "<div id='inner'>"
+        ++ "<div class='clear'>"
+        ++ "<div id='header'>"
+        ++ "<div data-binding-from='/A1' class='hn header' data-type='text'>"
+        ++ "</div>"
+        ++ "<div id='menu'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='contentwrapper'>"
+        ++ "<div id='primarycontent'>"
+        ++ "</div>"
+        ++ "<div id='secondarycontent'>"
+        ++ "<form class='hn' data-type='form'>"
+        ++ "<label>"
+        ++ "<span>"
+        ++ "<div data-binding-from='A3' class='hn' data-type='text'>"
+        ++ "</div>"
+        ++ "</span>"
+        ++ "</label>"
+        ++ "<div class='hn' data-type='input' data-binding-to='A:A'>"
+        ++ "</div>"
+        ++ "<label>"
+        ++ "<span>"
+        ++ "<div data-binding-from='B3' class='hn' data-type='text'>"
+        ++ "</div>"
+        ++ "</span>"
+        ++ "</label>"
+        ++ "<div class='hn' data-type='input' data-binding-to='B:B'>"
+        ++ "</div>"
+        ++ "<label>"
+        ++ "<span>"
+        ++ "<div data-binding-from='C3' class='hn' data-type='text'>"
+        ++ "</div>"
+        ++ "</span>"
+        ++ "</label>"
+        ++ "<div class='hn' data-type='input' data-binding-to='C:C'>"
+        ++ "</div>"
+        ++ "<label>"
+        ++ "<span>"
+        ++ "<div data-binding-from='D3' class='hn' data-type='text'>"
+        ++ "</div>"
+        ++ "</span>"
+        ++ "</label>"
+        ++ "<div class='hn' data-type='input' data-binding-to='D:D'>"
+        ++ "</div>"
+        ++ "<input type='submit' value='submit'>"
+        ++ "</input>"
+        ++ "</form>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='footer'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>",
+    Transactions = make_security(Form),
+    (true == is_approved(RefX, Json, Transactions)).
+    
 test2() ->
-    _Input = [{struct,[{"ref","/u/gordon/blah/D:D"},{"formula","Test"}]}],
-    _Ref   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
-    _Form  = "<link href='/templates/tiny/default.css' type='text/css' rel='stylesheet'></link><div id='outer'><div id='inner'><div class='clear'><div id='header'><div data-type='text' class='hn header' data-binding-from='B2'></div><div id='menu'></div></div></div><div id='plaincontent'></div><div id='contentwrapper'><div id='primarycontent'></div><div id='secondarycontent'></div><div data-type='input' class='hn' data-binding-to='D6'></div></div><div id='plaincontent'></div><div id='footer'></div></div></div></div>",
-    false.
+    Json = [{struct,[{"ref","/u/gordon/blah/D6"},{"formula","Test"}]}],
+    RefX   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
+    Form  = "<link href='/templates/tiny/default.css' type='text/css' rel='stylesheet'>"
+        ++ "</link>"
+        ++ "<div id='outer'>"
+        ++ "<div id='inner'>"
+        ++ "<div class='clear'>"
+        ++ "<div id='header'>"
+        ++ "<div data-type='text' class='hn header' data-binding-from='B2'>"
+        ++ "</div>"
+        ++ "<div id='menu'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='contentwrapper'>"
+        ++ "<div id='primarycontent'>"
+        ++ "</div>"
+        ++ "<div id='secondarycontent'>"
+        ++ "</div>"
+        ++ "<div data-type='input' class='hn' data-binding-to='D6'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='footer'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>",
+    Transactions = make_security(Form),
+    Transactions = make_security(Form),
+    is_approved(RefX, Json, Transactions).
+
+test3() ->
+    Json = [{struct,[{"ref","/u/gordon/blah/D:D"},{"formula","Test"}]}],
+    RefX   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
+    Form  = "<link href='/templates/tiny/default.css' type='text/css' rel='stylesheet'>"
+        ++ "</link>"
+        ++ "<div id='outer'>"
+        ++ "<div id='inner'>"
+        ++ "<div class='clear'>"
+        ++ "<div id='header'>"
+        ++ "<div data-type='text' class='hn header' data-binding-from='B2'>"
+        ++ "</div>"
+        ++ "<div id='menu'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='contentwrapper'>"
+        ++ "<div id='primarycontent'>"
+        ++ "</div>"
+        ++ "<div id='secondarycontent'>"
+        ++ "</div>"
+        ++ "<div data-type='input' class='hn' data-binding-to='D6'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='footer'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>",
+    Transactions = make_security(Form),
+    Transactions = make_security(Form),
+    (false == is_approved(RefX, Json, Transactions)).
+
+test4() ->
+    Json = [{struct,[{"ref","/u/gordon/blah/D6"},{"formula","Test"}]}],
+    RefX   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
+    Form  = "<link href='/templates/tiny/default.css' type='text/css' rel='stylesheet'>"
+        ++ "</link>"
+        ++ "<div id='outer'>"
+        ++ "<div id='inner'>"
+        ++ "<div class='clear'>"
+        ++ "<div id='header'>"
+        ++ "<div data-type='text' class='hn header' data-binding-from='B2'>"
+        ++ "</div>"
+        ++ "<div id='menu'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='contentwrapper'>"
+        ++ "<div id='primarycontent'>"
+        ++ "</div>"
+        ++ "<div id='secondarycontent'>"
+        ++ "</div>"
+        ++ "<div data-type='input' class='hn' data-binding-to='/D6'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='footer'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>",
+    Transactions = make_security(Form),
+    Transactions = make_security(Form),
+    (false == is_approved(RefX, Json, Transactions)).
+
+test5() ->
+    Json = [{struct,[{"ref","/u/gordon/blah/D:D"},{"formula","Test"}]}],
+    RefX   = {refX,"http://127.0.0.1:9000",["u","gordon","blah"],{page,"/"},[]},
+    Form  = "<link href='/templates/tiny/default.css' type='text/css' rel='stylesheet'>"
+        ++ "</link>"
+        ++ "<div id='outer'>"
+        ++ "<div id='inner'>"
+        ++ "<div class='clear'>"
+        ++ "<div id='header'>"
+        ++ "<div data-type='text' class='hn header' data-binding-from='B2'>"
+        ++ "</div>"
+        ++ "<div id='menu'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='contentwrapper'>"
+        ++ "<div id='primarycontent'>"
+        ++ "</div>"
+        ++ "<div id='secondarycontent'>"
+        ++ "</div>"
+        ++ "<div data-type='input' class='hn' data-binding-to='../D6'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "<div id='plaincontent'>"
+        ++ "</div>"
+        ++ "<div id='footer'>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>"
+        ++ "</div>",
+    Transactions = make_security(Form),
+    Transactions = make_security(Form),
+    (false == is_approved(RefX, Json, Transactions)).
+
+testA() ->
+    Path = ["blah", "bloh", "bleh"],
+    Path2 = "d1",
+    Ret = make_abs_path(Path, Path2),
+    io:format("Ret is ~p~n", [Ret]),
+    ("/blah/bloh/bleh/d1" == Ret).
+
+testB() ->
+    Path = ["blah", "bloh", "bleh"],
+    Path2 = "../d1",
+    Ret = make_abs_path(Path, Path2),
+    io:format("Ret is ~p~n", [Ret]),
+    ("/blah/bloh/d1" == Ret).
+
+testC() ->
+    Path = ["blah", "bloh", "bleh"],
+    Path2 = "./d1",
+    Ret = make_abs_path(Path, Path2),
+    io:format("Ret is ~p~n", [Ret]),
+    ("/blah/bloh/bleh/d1" == Ret).
+
 
 unit_test_() -> 
     [
      ?_assert(test1()),
-     ?_assert(test2())
+     ?_assert(test2()),
+     ?_assert(test3()),
+     ?_assert(test4()),
+     ?_assert(test5()),
+     ?_assert(testA()),
+     ?_assert(testB()),
+     ?_assert(testC())
     ].
