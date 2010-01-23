bbeaf8f7886ab4867c6296be43b56666ddd4c1e1
Dale Harvey <dale@hypernumbers.com>
2010-01-23 21:13:54 +0000

    fixed script bootloader
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@3023 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/run_svn_script.sh
+++ priv/run_svn_script.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-FILE=$1
+# export a shell script from svn then execture it
+# ./run_svn_script.sh http://svn.example.com/script.sh
 
-svn co $FILE ./script.sh
-./script.sh
-rm ./script.sh
+SVN_FILE=$1
+LOCAL_FILE="./script.sh"
+
+svn export $SVN_FILE $LOCAL_FILE
+$LOCAL_FILE
+rm $LOCAL_FILE
