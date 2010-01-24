d2e955f0c98e624a2ca449b13cba476743fb2373
Dale Harvey <dale@hypernumbers.com>
2010-01-24 19:08:55 +0000

    cleanup
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@3035 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- priv/tarsnap_quick.sh
+++ priv/tarsnap_quick.sh
@@ -8,8 +8,7 @@ cd $HOME
 
 echo "Starting tarsnap $DATE"
 
-echo $(env)
-
-echo $(/usr/local/bin/tarsnap)
-
-/usr/local/bin/tarsnap --cachedir tarsnap-cache --keyfile www/$SITE/priv/tarsnap/$SITE.tarsnap.key -v -c -f $SITE-var-$DATE www/$SITE/var
+/usr/local/bin/tarsnap \
+    --cachedir tarsnap-cache \
+    --keyfile www/$SITE/priv/tarsnap/$SITE.tarsnap.key \
+    -v -c -f $SITE-var-$DATE www/$SITE/var
