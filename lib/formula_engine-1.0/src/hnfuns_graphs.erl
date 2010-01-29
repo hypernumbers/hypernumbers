168b44a55c784ac2b018dc245370916a70e4d7a1
Gordon Guthrie <gordon@hypernumbers.com>
2010-01-29 23:54:00 +0000

    More graphs (proper casting and options for linegraph, histogram and piecharts)
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@3069 b744bbc5-f124-0410-b3d9-bb4a5f45712a

--- lib/formula_engine-1.0/src/hnfuns_graphs.erl
+++ lib/formula_engine-1.0/src/hnfuns_graphs.erl
@@ -10,10 +10,8 @@
 
 -export([
          linegraph/1,
-         linegraph3/1,
          piechart/1,
-         histogram/1,
-         test/1
+         histogram/1
         ]).
 
 -define(ROW, 0).
@@ -25,126 +23,124 @@
 -import(muin_util, [cast/2]).
 -import(muin_collect, [ col/2, col/3, col/4 ]).
 
-test([Data, 0]) ->
-    io:format("Data (in rows) is ~p~n", [Data]),
-    "banzai!";
-test([Data, 1]) ->
-    io:format("Data (in columns) is ~p~n", [Data]),
-    "bonzai!".
-
-linegraph([{range, Data}]) ->
-    Data2 = make_data(get_data(Data)),
-    % Max = tconv:to_s(stdfuns_stats:max(Data2)),
-    % Min = tconv:to_s(stdfuns_stats:min(Data2)),
-    Min = 0,
-    Max = 100,
-    "![graph](http://chart.apis.google.com/chart?chs=350x150&amp;chd="
-        ++ "t:" ++ make_data(Data2) ++ "&amp;cht=lc&amp;"
-        ++ "chds="++ tconv:to_s(Min) ++ "," ++ tconv:to_s(Max) ++ "&amp;"
-        ++ "chxt=y&amp;chxr=0," ++ tconv:to_s(Min) ++ "," ++ tconv:to_s(Max)
-        ++ ")";
-linegraph([1, {range, Data}, Colour]) ->
-    Data2 = make_data(get_data(Data)),
-    Colours2 = make_colours([Colour]),
-    lineg1(Data2, 0, 100, Colours2);
-linegraph([2, {range, Data1}, {range, Data2}, {range, [Colours]}]) ->
-    io:format("Data1 is ~p~nData2 is ~p~nColours is ~p~n",
-              [Data1, Data2, Colours]),
-    Data1a = make_data(get_data(Data1)),
-    Data2a = make_data(get_data(Data2)),
-    Coloursa = make_colours(Colours),
-    lineg1(Data1a ++ "|" ++ Data2a, 0, 100, Coloursa);
-linegraph([3, {range, Data1}, {range, Data2}, {range, Data3},
-           {range, [Colours]}]) ->
-    Data1a = make_data(get_data(Data1)),
-    Data2a = make_data(get_data(Data2)),
-    Data3a = make_data(get_data(Data3)),
-    Coloursa = make_colours(Colours),
-    lineg1(Data1a ++ "|" ++ Data2a ++ "|" ++ Data3a, 0, 100, Coloursa);
-linegraph([4, {range, Data1}, {range, Data2},
-           {range, Data3}, {range, Data4},
-           {range, [Colours]}]) ->
-    Data1a = make_data(get_data(Data1)),
-    Data2a = make_data(get_data(Data2)),
-    Data3a = make_data(get_data(Data3)),
-    Data4a = make_data(get_data(Data4)),
-    Coloursa = make_colours(Colours),
-    lineg1(Data1a ++ "|" ++ Data2a ++ "|" ++ Data3a ++ "|" ++ Data4a,
-           0, 100, Coloursa).
-
-lineg1(Data, Min, Max, Colours) ->
-    Min1 = tconv:to_s(Min),
-    Max1 = tconv:to_s(Max),
-    "![graph](http://chart.apis.google.com/chart?chs=350x150&amp;chd="
-        ++ "t:" ++ Data
-        ++ "&amp;cht=lc&amp;"
-        ++ "chds="++ Min1 ++ "," ++ Max1 ++ "&amp;"
-        ++ "chxt=x&amp;chxr="
-        ++ "0," ++ Min1 ++ "," ++ Max1
-        ++ "&amp;chco="
-        ++ Colours
-        ++ ")".
+%%
+%% Exported functions
+%%
 
 %% Variable 'O' is 'Orientation'
-linegraph3([Data])              -> lg3(Data, ?ROW, {{scale, auto},
-                                                    noAxes, randCol});
-linegraph3([Data, O])           -> lg3(Data, O, {{scale, auto},
-                                                 noAxes, randCol});
-linegraph3([Data, O, Min, Max]) -> lg3(Data, O, {{Min, Max}, noAxes, randCol});
-linegraph3([Data, O, Min, Max, XAxis]) ->
-                  lg3(Data, O, {{Min, Max}, XAxis, randCol});
-linegraph3([Data, O, Min, Max, XAxis, Cols]) ->
-                  lg3(Data, O, {{Min, Max}, XAxis, Cols}).
+linegraph([Data])              ->
+    lg1(Data, ?ROW, {{scale, auto}, [], []});
+linegraph([Data, O])           ->
+    lg1(Data, O, {{scale, auto}, [], []});
+linegraph([Data, O, Min, Max]) ->
+    lg1(Data, O, {{Min, Max}, [], []});
+linegraph([Data, O, Min, Max, XAxis]) ->
+    lg1(Data, O, {{Min, Max}, XAxis, []});
+linegraph([Data, O, Min, Max, XAxis, Cols]) ->
+    lg1(Data, O, {{Min, Max}, XAxis, Cols}).
+
+piechart([Data])                  -> pie1(Data, [], []);
+piechart([Data, Titles])          -> pie1(Data, Titles, []);
+piechart([Data, Titles, Colours]) -> pie1(Data, Titles, Colours).
+
+histogram([D])                   -> hist1(D, {{scale, auto}, [], []});
+histogram([D, Tt])               -> hist1(D, {{scale, auto}, Tt, []});
+histogram([D, Tt, Cols])         -> hist1(D, {{scale, auto}, Tt, Cols});
+histogram([D, Tt, Cols, Mn, Mx]) -> hist1(D, {{Mn, Mx}, Tt, Cols}).
     
-lg3(Data, Orientation, {Scale, Axes, Colours}) ->
-    {Data2, _NoOfRows, _NoOfCols} = extract(Data, Orientation),
-    Data3 = [col([X],
-                 [eval_funs,
-                  fetch, flatten,
-                  {cast, str, num, ?ERRVAL_VAL},
-                  {cast, bool, num},
-                  {cast, blank, num},
-                  {ignore, str},
-                  {ignore, blank}
-                 ],
-                 [return_errors, {all, fun is_number/1}]) || X <- Data2],
-    % check for errors and then fix the reversed lists problems
-    case has_error(Data3) of
+%%
+%% Internal Functions
+%%
+hist1(Data, {Scale, Titles, Colours}) ->
+    Data2 = cast_data(Data),
+    Titles2 = make_titles(cast_titles(Titles)),
+    Colours2 = conv_colours(get_colours(Colours)),
+    Data3 = lists:reverse(Data2),
+    {Min, Max} = get_scale(Scale, Data2),
+    "![graph](http://chart.apis.google.com/chart?cht=bvs&amp;chd=t:"
+        ++ make_data(Data3)
+        ++ "&amp;chxt=y&amp;chxl=0:|"
+        ++ tconv:to_s(Min) ++ "|" ++ tconv:to_s(Max)
+        ++ "&amp;chs=250x100&amp;chl="
+        ++ Titles2
+        ++ Colours2.
+
+pie1(Data, Titles, Colours) ->
+    Data2 = cast_data(Data),
+    Titles2 = cast_titles(Titles),
+    Colours2 = conv_colours(get_colours(Colours)),
+    case has_error([Data2, Titles2, Colours2]) of
         {true, Error} -> Error;
-        false         -> lg3a(rev(Data3), Scale, Axes, Colours)
+        false         -> pie2(Data2, Titles2, Colours2)
     end.
 
-lg3a(Data, Scale, Axes, Colours) ->
+pie2(Data, Titles, Colours) ->
+    Data1 = make_data(normalise(Data)),
+    Titles1 = make_titles(Titles),
+    Colours1 = conv_colours(get_colours(Colours)),
+    "![graph](http://chart.apis.google.com/chart?cht=p3&amp;chd=t:"
+        ++ Data1
+        ++ "&amp;chs=450x100&amp;chl="
+        ++ Titles1
+        ++ Colours1.
+
+lg1(Data, Orientation, {Scale, Axes, Colours}) ->
+    {Data2, _NoOfRows, _NoOfCols} = extract(Data, Orientation),
+    Data3 = [cast_data(X) || X <- Data2],
+    % check for errors and then fix the reversed lists problems
+    Colours2 = get_colours(Colours),
     {Min, Max} = get_scale(Scale, Data),
     XAxis2 = get_axes(Axes),
-    Colours2 = get_colours(Colours),
-    io:format("XAxis2 is ~p~nColours2 is ~p~n",
-               [XAxis2, Colours2]),
-    Ret = "![graph](http://chart.apis.google.com/chart?chs=350x150&amp;chd="
+    case has_error([Data3, Colours2, Min, Max, XAxis2]) of
+        {true, Error} -> Error;
+        false         -> lg2(rev(Data3), {Min, Max}, XAxis2, Colours)
+    end.
+
+lg2(Data, {Min, Max}, XAxis, Colours) ->
+   "![graph](http://chart.apis.google.com/chart?chs=350x150&amp;chd="
         ++ "t:" ++ conv_data(Data) ++ "&amp;cht=lc&amp;"
-        ++ "chds="++ tconv:to_s(Min) ++ "," ++ tconv:to_s(Max) ++ "&amp;"
-        ++ "chxt="
-        ++ conv_x_axis(XAxis2)
+        ++ "chds="++ tconv:to_s(Min) ++ "," ++ tconv:to_s(Max)
+        ++ "&amp;chxt="
+        ++ conv_x_axis(XAxis)
         ++ "1:|" ++ tconv:to_s(Min) ++ "|" ++ tconv:to_s(Max)
-        ++ conv_colours(Colours2)
-        ++ ")",
-    io:format("Ret is ~p~n", [Ret]),
-    Ret.
+        ++ conv_colours(Colours)
+        ++ ")".
+
+cast_data(Data) ->
+    col([Data],
+                [eval_funs,
+                 fetch, flatten,
+                 {cast, str, num, ?ERRVAL_VAL},
+                 {cast, bool, num},
+                 {cast, blank, num},
+                 {ignore, str},
+                 {ignore, blank}
+                ],
+                [return_errors, {all, fun is_number/1}]).
+
+cast_titles(Titles) ->
+    col([Titles],
+                  [eval_funs,
+                   fetch, flatten,
+                   {cast, bool, str},
+                   {cast, num, str},
+                   {cast, date, str},
+                   {cast, blank, str}],
+                  [return_errors]).
 
 conv_colours([])      -> [];
 conv_colours(Colours) -> "&amp;chco=" ++ make_colours(Colours).
 
 conv_x_axis([])    -> "y&amp;chxl=";
-conv_x_axis(XAxis) -> "x,y&amp;chxl=0:|" ++ string:join(XAxis, "|") ++ "|". 
+conv_x_axis(XAxis) -> "x,y&amp;chxl=0:|"
+                          ++ string:join(XAxis, "|") ++ "|". 
 
 conv_data(Data) -> conv_d1(Data, []).
 
 conv_d1([], Acc)      -> string:join(lists:reverse(Acc), "|");
 conv_d1([H | T], Acc) -> conv_d1(T, [make_data(H) | Acc]).
 
-get_colours(randCol) -> [];
 get_colours(Colours) ->
-    io:format("Colours is ~p~n", [Colours]),
     col([Colours],
          [eval_funs,
           {ignore, bool},
@@ -153,19 +149,22 @@ get_colours(Colours) ->
           fetch, flatten],
           [return_errors]).
 
-get_axes(noAxes) -> [];
 get_axes(XAxis)  -> col([XAxis],
                         [eval_funs,
+                         fetch, flatten,
                          {cast, num, str},
                          {cast, bool, str},
                          {cast, date, str},
-                         {cast, blank, str},
-                         fetch, flatten],
+                         {cast, blank, str}],
                         [return_errors]).
 
+get_scale({scale, auto}, [H | T]) when is_list(H) ->
+    Min = tconv:to_s(stdfuns_stats:min(lists:merge([H | T]))),
+    Max = tconv:to_s(stdfuns_stats:max(lists:merge([H | T]))),
+    {Min, Max};
 get_scale({scale, auto}, Data) ->
-    Min = tconv:to_s(stdfuns_stats:min(lists:merge(Data))),
-    Max = tconv:to_s(stdfuns_stats:max(lists:merge(Data))),
+    Min = tconv:to_s(stdfuns_stats:min(Data)),
+    Max = tconv:to_s(stdfuns_stats:max(Data)),
     {Min, Max};
 get_scale(Scale, _Data) -> Scale.
     
@@ -176,7 +175,6 @@ tartup(Data) ->
     [F | _T] = Data,
     NoOfCols = length(F), % rectangular matrix
     {chunk(Data), length(Data), NoOfCols}.
-
 
 rev(List) -> rev1(List, []).
 
@@ -196,36 +194,10 @@ has_error([H | T]) when is_list(H) -> case has_error(H) of
                                       end;
 has_error([_H | T])                -> has_error(T).
 
-piechart([{range, Data}, {range, Titles}, {range, Colours}]) ->
-    Data2 = make_data(normalise(get_data(Data))),
-    Titles2 = make_titles(get_data(Titles)),
-    Colours2 = make_colours(get_data(Colours)),
-    "![graph](http://chart.apis.google.com/chart?cht=p3&amp;chd=t:"
-        ++ Data2
-        ++ "&amp;chs=450x100&amp;chl="
-        ++ Titles2
-        ++ "&amp;chco="
-        ++ Colours2.
-
-histogram([{range, Data}, {range, Titles}]) ->
-    Data2 = make_data(get_data(Data)),
-    Titles2 = make_titles(get_data(Titles)),
-    "![graph](http://chart.apis.google.com/chart?cht=bvs&amp;chd=t:"
-        ++ Data2
-        ++ "&amp;chs=250x100&amp;chl="
-        ++ Titles2.
-
 normalise(Data) ->
     Total = lists:sum(Data),
     Fun = fun(X) -> trunc(100*X/Total) end,
     lists:map(Fun, Data).
-
-get_data(List) -> get_d1(List, []).
-
-get_d1([], Acc)                      -> lists:reverse(Acc);
-get_d1([[H] | T], Acc)               -> get_d1(T, [H | Acc]);
-get_d1([H | T], Acc) when is_list(H) ->
-    get_d1(T, lists:flatten([lists:reverse(H) | Acc])).
 
 make_data(List) -> make_d1(List, []).
 
