f40a60bc7caff804f68c09bbc0bef7ee5f09a67f
Gordon Guthrie <gordon@hypernumbers.com>
2010-02-13 13:35:14 +0000

    provisioning tester changes
    git-svn-id: http://svn.hypernumbers.com/hypernumbers/code/trunk@3190 b744bbc5-f124-0410-b3d9-bb4a5f45712a

---
+++ src/bits.erl
@@ -0,0 +1,51 @@
+%%%-------------------------------------------------------------------
+%%% @author    Gordon Guthrie
+%%% @copyright (C) 2009, Hypernumbers.com
+%%% @doc       This module really shouldn't be here - it is a scratch
+%%%            debugging module (hence the name!).
+%%% @private
+-module(bits).
+-export([ init_memory_trace/0, mem_csv/0]).                
+
+init_memory_trace() ->
+    File= case os:type() of
+	      {win32,nt} -> exit('erk, not on windows...');
+	      _          -> "../var/logs/memory.csv"
+          end,
+    io:format("File is ~p~n", [File]),
+    ok = filelib:ensure_dir(File),
+    % so shit its not true :(
+    ok = file:delete(File),
+    ok = filelib:ensure_dir(File),
+    log(stringify([time, total, processes, processes_used, system,
+                   atom, atom_used, binary, code, ets])).    
+
+mem_csv() ->
+    Mem = erlang:memory(),
+    Vals = [V || {_K, V} <- Mem],
+    io:format("Mem is ~p~nVals is ~p~n", [Mem, Vals]),
+    Secs = calendar:datetime_to_gregorian_seconds(erlang:localtime()),
+    log(stringify([Secs | Vals])).
+
+log(String) ->
+    File= case os:type() of
+	      {win32,nt} -> exit('erk, not on windows...');
+	      _          -> "../../logs/memory.log"
+	  end,
+    _Return=filelib:ensure_dir(File),
+    
+    case file:open(File, [append]) of
+	{ok, Id} ->
+	    io:fwrite(Id, "~s~n", [String]),
+	    file:close(Id);
+	_ ->
+	    error
+    end.
+
+stringify(List) -> str1(List, []).
+
+str1([], Acc) -> string:join(lists:reverse(Acc), ",");
+str1([H | T], Acc) when is_atom(H) ->
+    str1(T, ["\"" ++ atom_to_list(H) ++ "\"" | Acc]);
+str1([H | T], Acc) when is_integer(H) ->
+    str1(T, ["\"" ++ integer_to_list(H) ++ "\"" | Acc]).
--- src/deploy.erl
+++ src/deploy.erl
@@ -2,12 +2,16 @@
 
 -export([run/0]).
 
-run() -> run1(1).
+run() -> bits:init_memory_trace(),
+         run1(1).
 
 
 run1(1000) -> io:format("one thousand is enough, thank you...~n");
-run1(N)    -> Sub = "sub" ++ integer_to_list(N),
-              hn_setup:site("http://" ++ Sub ++ ".localhost:9000", pirate, []),
+run1(N)    -> Dom = "http://" ++ "sub" ++ integer_to_list(N) ++
+                  ".localhost:9000",
+              hn_setup:site(Dom, pirate, []),
               io:format("Site no ~p deployed~n", [N]),
+              bits:mem_csv(),
+              ok = hn_db_admin:outof_mem(Dom),
               run1(N + 1).
-             
+    
