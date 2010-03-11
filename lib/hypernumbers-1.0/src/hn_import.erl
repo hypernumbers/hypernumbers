0b6fdbac06832d43ade00c55a8cfa0da3e4e782b
Gordon Guthrie <gordon@hypernumbers.com>
2010-03-11 14:25:28 +0000

    csv import code added

--- lib/hypernumbers-1.0/src/hn_import.erl
+++ lib/hypernumbers-1.0/src/hn_import.erl
@@ -1,16 +1,54 @@
 %% @author Dale Harvey
 %% @copyright 2008 Hypernumbers Ltd
 %% @doc Import external spreadsheets into hypernumbers
+
 -module(hn_import).
+
 -include("hypernumbers.hrl").
 -include("spriki.hrl").
--define(pget(Key, List), proplists:get_value(Key, List, undefined)).
--export([ json_file/2 ]).
 
-json_file(Url, FileName) -> 
+-define(pget(Key, List), proplists:get_value(Key, List, undefined)).
+
+-export([
+         json_file/2,
+         csv_file/2,
+         csv_append/2
+        ]).
+
+csv_file(Url, FileName) ->
 
     Ref = hn_util:parse_url(Url),
+
+    % first read the file
+    Recs = parse_csv:parse_file(FileName),
+    Refs  = make_refs(Recs, Ref),
+
+    %% first clear the page
+    ok = hn_db_api:clear(Ref, all, nil),
+
+    % now write it 
+    ok = hn_db_api:write_attributes(Refs).
+
+csv_append(Url, FileName) ->
+
+    Ref = hn_util:parse_url(Url),
+
+    % first read the file
+    Recs = parse_csv:parse_file(FileName),
+    Refs  = make_append_refs(Recs, Ref),
+    
+    % now write it 
+    [ok = hn_db_api:write_last(X, nil, nil) || X <- Refs],
+    ok.
+    
+json_file(Url, FileName) -> 
+
+    % first read the file
     {ok, JsonTxt}    = file:read_file(FileName),
+
+    Ref = hn_util:parse_url(Url),
+
+    % second unpack the json
     {struct, Json}   = hn_util:js_to_utf8(mochijson:decode(JsonTxt)),
     {struct, Styles} = ?pget("styles", Json),
     {struct, Cells}  = ?pget("cell", Json),
@@ -19,6 +57,10 @@ json_file(Url, FileName) ->
 
     StyleRecs = [make_style_rec(X) || X <- Styles],
 
+    % now clear the page
+    ok = hn_db_api:clear(Ref, all, nil),
+
+    % finally write out the data
     [ rows(Ref, X, StyleRecs, row,    fun write_col_row/3) || X <- Rows],
     [ rows(Ref, X, StyleRecs, column, fun write_col_row/3) || X <- Cols],
         
@@ -88,3 +130,27 @@ make_style_rec({Idx, Style}) ->
     Rec2 = [ F2(X, Rec) || X <- lists:seq(1, NoOfFields)],
     
     {Idx, list_to_tuple([magic_style| Rec2])}.
+
+make_append_refs(List, Ref) -> make_a1(List, Ref, []).
+
+make_a1([], _Ref, Acc)     -> lists:reverse(Acc);
+make_a1([H | T], Ref, Acc) ->
+    NewRefs = make_a2(tuple_to_list(H), Ref, 1, []),
+    make_a1(T, Ref, [NewRefs | Acc]).
+
+make_a2([], _Ref, _C, Acc)    -> Acc;
+make_a2([H | T], Ref, C, Acc) ->
+    NewRef = Ref#refX{obj={column, {C, C}}},
+    make_a2(T, Ref, C + 1, [{NewRef, H} | Acc]).
+    
+make_refs(List, Ref) -> make_r1(List, Ref, 1, []).
+
+make_r1([], _Ref, _R,  Acc)   -> lists:merge(Acc);
+make_r1([H | T], Ref, R, Acc) ->
+    NewAcc = make_r2(tuple_to_list(H), Ref, R, 1, []),
+    make_r1(T, Ref, R + 1, [NewAcc | Acc]).
+
+make_r2([], _Ref, _R, _C, Acc)   -> Acc;
+make_r2([H | T], Ref, R, C, Acc) ->
+    NewRef = Ref#refX{obj={cell, {C, R}}},
+    make_r2(T, Ref, R, C + 1, [{NewRef, [{"formula", H}]} | Acc]).
