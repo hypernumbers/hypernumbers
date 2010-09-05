6e2baab801454ccf37ec7d0e2deb3322c83e9cd8
Gordon Guthrie <dale@arandomurl.com>
2010-09-05 17:34:15 +0100

    test runner for the mac mini

---
+++ priv/mini_runner.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+DATE=`date +%Y-%m-%d.%T`
+
+REPO=git@github.com:hypernumbers/hypernumbers.git
+HNTOP=/Users/daleharvey/hypernumbers
+WEBROOT=/Users/daleharvey/hn_tests/dev-www
+TESTDIR=/Users/daleharvey/hn_tests/test-hypernumbers
+LASTRUN=$WEBROOT/last_run
+
+cd $HNTOP
+git clone $REPO $TESTDIR
+
+## Compile, and run Hypernumbers
+cd $TESTDIR
+./hn build
+sed -i '/{127,0,0,1}, 9090}/d' var/sys.config
+sed -i '/{127,0,0,1}, 9091}/d' var/sys.config
+./hn start
+
+## Generate Excel Tests
+cd $TESTDIR/priv/testserver
+ruby regen_tests.rb 1x
+ruby regen_tests.rb 2x
+cd $TESTDIR
+
+## Generate System Tests
+echo $(./hn call 'testsys:generate().')
+echo $(./hn call 'test:sys().')
+
+## Run tests
+echo $(./hn call 'test:excel("1a").')
+echo $(./hn call 'test:excel("1b").')
+echo $(./hn call 'test:excel("1c").')
+echo $(./hn call 'test:excel("1d").')
+echo $(./hn call 'test:excel("1e").')
+
+echo $(./hn call 'test:excel("2a").')
+echo $(./hn call 'test:excel("2b").')
+echo $(./hn call 'test:excel("2c").')
+echo $(./hn call 'test:excel("2d").')
+echo $(./hn call 'test:excel("2e").')
+echo $(./hn call 'test:excel("2f").')
+echo $(./hn call 'test:excel("2g").')
+echo $(./hn call 'test:excel("2x").')
+echo $(./hn call 'test:excel("2y").')
+echo $(./hn call 'test:excel("2z").')
+
+## Cleanup.
+./hn stop
+
+rm -rf $LASTRUN
+mkdir -p $LASTRUN
+
+cp -r $TESTDIR/var/tests/* $LASTRUN
+cp $TESTDIR/var/tests/index.html $WEBROOT/$DATE.html
+
+rm -Rf $TESTDIR
