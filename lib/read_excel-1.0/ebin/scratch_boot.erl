57736acc7ccd7226f46adecedbb73aae5700aa75
Gordon Guthrie <gordon@hypernumbers.com>
2010-09-20 00:05:02 +0100

    small tidy up

--- lib/read_excel-1.0/ebin/scratch_boot.erl
+++
@@ -1,8 +0,0 @@
--module(scratch_boot).
-
--export([boot/0]).
-
-boot()->
-    code:add_patha("/opt/SVN/spriki/trunk/lib/utilities-1.0/ebin"),
-    Pid=spawn(filefilters, test_DEBUG, []),
-    io:format("filefilters spawned with Pid ~p~n",[Pid]).
