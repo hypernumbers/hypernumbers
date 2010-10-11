b480632827350c322ff4a20d6b75c83584ef2b1c
Gordon Guthrie <gordon@hypernumbers.com>
2010-10-11 17:19:28 +0100

    frontpage js tweak (clear on focus)

--- priv/frontpage/frontpage5.js
+++ priv/frontpage/frontpage5.js
@@ -1,29 +1,12 @@
 var Frontpage = function() {
 
-    var api = {},
-    currentStory = 1;
-    
-    api.showStory = function(story) {
-        $("#fp5_story_"+currentStory).hide();
-        $("#fp5_story_"+story).show();
-        $("#fp5_menu_"+currentStory).css('background', 'url(http://files.hypernumbers.com/redesign/menu.png) no-repeat');
-        $("#fp5_menu_"+story).css('background', 'url(http://files.hypernumbers.com/redesign/activemenu.png) no-repeat');
-        currentStory = story;
-    };
-    
-    api.init = function() {
-        var click = function(e) {
-            var menu = e.target.id.split("_")[2];
-            api.showStory(menu);
-            };
-        
-        $("#fp5_menu").bind("click", click);            
+    var onFocusFun = function(e) {
+        $("#signupemail").val("");
+        $("#siognupemail").unbind(onFocusFun);
     };
 
-    api.init();
-    api.showStory(1);
+    $("#signupemail").focus(onFocusFun);
 
-    return api;
 };
 
 var HN_Frontpage = new Frontpage();
