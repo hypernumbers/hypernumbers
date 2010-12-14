6ad5f35665e9fcbd058126f457583c4d20ab71d1
Gordon Guthrie <gordon@hypernumbers.com>
2010-12-14 13:35:41 +0000

    grep out eunit warnings

--- priv/dialyzer/dialyse_hn.sh
+++ priv/dialyzer/dialyse_hn.sh
@@ -6,4 +6,5 @@ dialyzer -r \
     $HN_LIB/hypernumbers-1.0/ebin/ \
     $HN_LIB/formula_engine-1.0/ebin/ \
     $HN_LIB/read_excel-1.0/ \
+    | grep -v "The variable _ can never match since previous clauses completely covered the type 'false'" \
     > ~/hypernumbers/priv/dialyzer/output.dialyzer.txt
