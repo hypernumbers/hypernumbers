bca4d6979aae5515f983b557193f62656f163567
Gordon Guthrie <gordon@hypernumbers.com>
2011-01-13 01:33:23 +0000

    New graph types
    Added xy lines and sparklines.
    
    xy lines expect 2d ranges (horizontal or vertical)
    sparklines expect 1d ranges

--- lib/formula_engine-1.0/src/typechecks.erl
+++ lib/formula_engine-1.0/src/typechecks.erl
@@ -10,6 +10,7 @@
 -export([
          std_strs/1,
          std_ints/1,
+         std_nums/1,
          flat_strs/1
         ]).
 
@@ -18,12 +19,18 @@ std_strs(Vals) ->
     Passes = [return_errors, {all, fun muin_collect:is_string/1}],
     muin_collect:col(Vals, Rules, Passes).
 
+std_nums(Vals) ->
+    Rules = [eval_funs, fetch, area_first, {cast, num}],
+    Passes = [return_errors],
+    muin_collect:col(Vals, Rules, Passes).
+
 std_ints(Vals) ->
     Rules = [eval_funs, fetch, area_first, {cast, int}],
     Passes = [return_errors],
     muin_collect:col(Vals, Rules, Passes).
 
 flat_strs(Vals) ->
+    io:format("Vals is ~p~n",[Vals]),
     Rules = [eval_funs, fetch, flatten, {cast, str}],
     Passes = [return_errors, {all, fun muin_collect:is_string/1}],
     muin_collect:col(Vals, Rules, Passes).
