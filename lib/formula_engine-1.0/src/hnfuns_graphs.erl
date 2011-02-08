3d0c9f7afb9452aa3ec02f02e2105dd313b8db75
Gordon Guthrie <gordon@hypernumbers.com>
2011-02-08 17:00:20 +0000

    Interim commit of speedo's
    Need to auto size on scale and also allow the thresholds to be set

--- lib/formula_engine-1.0/src/hnfuns_graphs.erl
+++ lib/formula_engine-1.0/src/hnfuns_graphs.erl
@@ -371,14 +371,11 @@ make_c([{K, V} | T], Acc) -> NewAcc = "&amp;" ++ K ++ "=" ++ V,
 'speedo.2x4'([_V, _Tt, _S] =  L)     -> speedo(?SIZE2x4, L);
 'speedo.2x4'([_V, _Tt, _S, _Th] = L) -> speedo(?SIZE2x4, L).
 
-'speedo.3x6'(List)  -> io:format("In speedo.3x6 for ~p~n", [List]),
-                       speedo(?SIZE3x6,  List).
-'speedo.4x8'(List)  -> speedo(?SIZE4x8,  List).
-'speedo.6x12'(List) -> speedo(?SIZE6x12, List).
+'speedo.3x6'(List)  -> {resize, 3, 6,  speedo(?SIZE3x6,  List)}.
+'speedo.4x8'(List)  -> {resize, 4, 8,  speedo(?SIZE4x8,  List)}.
+'speedo.6x12'(List) -> {resize, 6, 12, speedo(?SIZE6x12, List)}.
 
-speedo(Size, [V])                     -> io:format("Val is ~p~n", [V]),
-                                         V2 = cast_val(V),
-                                         io:format("V2 is ~p~n", [V2]),
+speedo(Size, [V])                     -> V2 = cast_val(V),
                                          speedo1(Size, V2, "", "", "", "", 1);
 speedo(Size, [V, Tt])                 -> V2 = cast_val(V),
                                          speedo1(Size, V2, Tt, "", "", "", 1);
@@ -393,8 +390,6 @@ speedo(Size, [V, Tt, SubT, Th, Labs]) -> Scale = speedo_scale(Th),
 cast_val(Val) ->  cast_v2(Val, 0, 100, 1).
 
 cast_v2(Val, Min, Max, Scale) ->
-    io:format("Val is ~p~n", [Val]),
-    io:format("Min is ~p Max is ~p~n", [Min, Max]),
     if
         Val < Min                     -> ?ERRVAL_VAL;
         Val > Max                     -> ?ERRVAL_VAL;
@@ -402,12 +397,10 @@ cast_v2(Val, Min, Max, Scale) ->
     end.
 
 speedo1(Size, Val, Title, Subtitle, Threshold, Lables, Scale) ->
-    io:format("Size is ~p Val is ~p~n", [Size, Val]),
     [Tt2]  = cast_titles(Title),
     [Sb2]  = cast_titles(Subtitle),
     %[Th2]  = cast_data(Threshold),
     %[Lab2] = cast_titles(Lables),
-    io:format("Scale is ~p~n", [Scale]),
     if
         Val < 0                             -> ?ERRVAL_VAL;
         Val > 100 * Scale                   -> ?ERRVAL_VAL;
@@ -419,7 +412,7 @@ speedo1(Size, Val, Title, Subtitle, Threshold, Lables, Scale) ->
                        {?size, Size},
                        {?type, "gm"},
                        {?colours, "000000,008000|FFCC33|FF0000"},
-                       {?data, tconv:to_s(Val)},
+                       {?data, "t:" ++ tconv:to_s(Val)},
                        {?speedolab, Sb2},
                        {?title, Tt2}
                        ],
