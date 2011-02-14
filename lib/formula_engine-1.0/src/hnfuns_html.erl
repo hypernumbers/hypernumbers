69c0c56902c7585ffa8efaf8726772db925a39e2
Gordon Guthrie <gordon@hypernumbers.com>
2011-02-14 08:57:18 +0000

    Boxes and other layout items no longer have a preview in the spreadsheet

--- lib/formula_engine-1.0/src/hnfuns_html.erl
+++ lib/formula_engine-1.0/src/hnfuns_html.erl
@@ -23,7 +23,7 @@
     [H2] = typechecks:throw_std_ints([H]),
     [T2] = typechecks:throw_std_strs([Text]),
     check_size(W2, H2),
-    {preview, {T2, W2, H2}, "<span class='hn-wc-wd-" ++ integer_to_list(W2) ++
+    {resize, W2, H2, "<span class='hn-wc-wd-" ++ integer_to_list(W2) ++
      " hn-wc-ht-" ++ integer_to_list(H2) ++ "' " ++
      "style='position:absolute;top:25%;left:0%;'>"
      ++ T2 ++ "</span>"}.
@@ -44,7 +44,7 @@
     check_size(W, H),
     BodyStyle = "hn-wc-ht-body2-" ++ Height, 
     [H1] = typechecks:throw_html_box_contents([Headline]),
-    {preview, {H1, W, H}, box(Width, Height, Background, Border, Style, Lines,
+    {resize, W, H, box(Width, Height, Background, Border, Style, Lines,
                               BodyStyle, [H1, Content, Footer])};
 'html.box.1'(Background, Border, Style, Lines, [Width, Height, Headline, Content]) ->
     [W] = typechecks:throw_std_ints([Width]),
@@ -52,14 +52,14 @@
     check_size(W, H),
     BodyStyle = "hn-wc-ht-body1-" ++ Height, 
     [H1] = typechecks:throw_html_box_contents([Headline]),
-    {preview, {H1, W, H}, box(Width, Height, Background, Border, Style, Lines,
+    {resize, W, H, box(Width, Height, Background, Border, Style, Lines,
                               BodyStyle, [H1, Content])};
 'html.box.1'(Background, Border, Style, Lines, [Width, Height, Content]) ->
     [W] = typechecks:throw_std_ints([Width]),
     [H] = typechecks:throw_std_ints([Height]),
     check_size(W, H),
     BodyStyle = "hn-wc-ht-" ++ Height,
-    {preview, {"Box", W, H}, box(Width, Height, Background, Border, Style, Lines,
+    {resize, W, H, box(Width, Height, Background, Border, Style, Lines,
                                  BodyStyle, [Content])}.
 
 check_size(W, H) when W > 0 andalso W < 13 andalso H > 1 andalso H < 21 -> ok;
