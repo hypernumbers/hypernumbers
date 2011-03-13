550c1a51255f7add41899569a7834b2d456cbdeb
Sam Elliott <sam@lenary.co.uk>
2011-03-13 16:08:10 +0000

    Moved status_srv to be per-site under site_sup

--- lib/hypernumbers-1.0/src/hypernumbers_sup.erl
+++ lib/hypernumbers-1.0/src/hypernumbers_sup.erl
@@ -22,17 +22,13 @@ init([]) ->
     Service = {service_sup, {service_sup, start_link, []},
                 permanent, infinity, supervisor, [service_sup]},
 
-    Status = {status_srv, {status_srv, start_link, []},
-              permanent, 2000, worker, [status_srv]},
-
     SiteMaster = {sitemaster_sup, {sitemaster_sup, start_link, []},
                   permanent, infinity, supervisor, [sitemaster_sup]},
 
     Mochi = {mochi_srv, {hn_mochi, start, []},
              permanent, 3000, worker, dynamic},
 
-    {ok,{{one_for_one, 60, 1}, [ Service, 
-                                 Status, 
+    {ok,{{one_for_one, 60, 1}, [ Service,
                                  SiteMaster,
                                  Mochi
                                ]}}.
