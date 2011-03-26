4eab7decfdfd169ab737c2ea2239939bf0f44512
Gordon Guthrie <gordon@hypernumbers.com>
2011-03-26 15:36:23 +0000

    restyle of z-order debugger

--- lib/formula_engine-1.0/src/hnfuns_z.erl
+++ lib/formula_engine-1.0/src/hnfuns_z.erl
@@ -17,30 +17,40 @@ debugz([Z]) ->
                      fun debug1/1).
 
 debug1([{zeds, Matches, NoMatches, Errs}]) ->
-    Html = "<div class='hn_debug_top'>Debugging Z Specifications</div>"
-        ++ make_matches(Matches) ++ make_no_matches(NoMatches) ++ make_errs(Errs),
-    {resize, 5, 10, Html}.
+    Html = "<div class='hn_debug_z'>"
+        ++ "<div class='hn_debug_top'>Debugging Z Specifications</div>"
+        ++ make_matches(Matches) ++ make_no_matches(NoMatches) ++ make_errs(Errs)
+        ++ "</div>",
+    {resize, {5, 10, [], []}, Html}.
 
-make_matches([]) -> "<div>no pages match</div>";
+make_matches([]) -> "<div class='hn_debug_hd'>no pages match</div>";
 make_matches(L)  ->
-    "<div class='hn_debug_hd'>The following pages match:"
-        ++ make_m2(L, []) ++ "</div>".
+    "<div class='hn_debug_hd'>The following pages match:</div>"
+        ++ "<table>"
+        ++ make_m2(L, [])
+        ++ "</table>".
 
 make_no_matches([]) -> [];
 make_no_matches(L)  ->
-    "<div class='hn_debug_hd'>The following pages (or page stems) do not match:"
-        ++ make_no2(L, []) ++ "</div>".
+    "<div class='hn_debug_hd'>The following pages (or page stems) "
+        ++ "do not match:</div>"
+        ++ make_no2(L, []).
 
 make_errs([]) -> [];
 make_errs(L)  ->
-    "<div class='hn_debug_hd'>The following pages throw errors:"
-        ++ make_e2(L, []) ++ "</div>".
+    "<div class='hn_debug_hd'>The following pages throw errors:</div>"
+        ++ make_e2(L, []).
 
 make_m2([], Acc) -> lists:flatten(lists:reverse(Acc));
 make_m2([{{Path, Ref}, Val} | T], Acc) ->
-    NewA = "<div><a href='" ++ hn_util:list_to_path(Path)
-        ++ "'>" ++ hn_util:list_to_path(Path) ++ "</a> " ++ Ref ++ ": "
-        ++ tconv:to_s(Val) ++ "</div>",
+    V = case Val of
+            {errval, Err} -> atom_to_list(Err);
+            _             -> tconv:to_s(Val)
+        end,
+    NewA = "<tr><td><a href='" ++ hn_util:list_to_path(Path)
+        ++ "'>" ++ hn_util:list_to_path(Path)
+        ++ "</a></td><td>" ++ Ref ++ "</td><td>"
+        ++ V ++ "</td></tr>",
     make_m2(T, [NewA | Acc]).
 
 make_no2([], Acc) -> lists:flatten(lists:reverse(Acc));
