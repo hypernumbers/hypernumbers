9f789dfdc79c3a839dc77c95a8a8bbfc443797f5
Gordon Guthrie <gordon@hypernumbers.com>
2011-05-23 10:59:02 +0100

    added new fn map.rows.button

--- lib/formula_engine-1.0/src/hnfuns_controls.erl
+++ lib/formula_engine-1.0/src/hnfuns_controls.erl
@@ -8,17 +8,39 @@
 -module(hnfuns_controls).
 
 -export([
-         'create.button'/1
+         'create.button'/1,
+         'map.rows.button'/1
         ]).
 
 -include("spriki.hrl").
 -include("errvals.hrl").
 
+'map.rows.button'(List) ->
+    [Title, Map] = typechecks:std_strs(List),
+    Maps = hn_util:get_maps(get(site)),
+    Id = "id_" ++ muin_util:create_name(),
+    case lists:member(Map, Maps) of
+        false -> ?ERRVAL_VAL;
+        true  -> Js = ["/hypernumbers/ajaxfileupload.js",
+                       "/webcomponents/hn.maprows.js"],
+                 Reload = ["HN.MapRows.reload();"],
+                 Incs = #incs{js = Js, js_reload = Reload},
+                 Payload = {struct, [{"map", Map}]},
+                 Form = #form{id = {'map-rows-button', Title},
+                              kind = "map-rows-button",
+                              attrs = Payload},
+                 Html = "<input id='" ++ Id ++ "' type='submit' "
+                     ++ "class='hn-maprows' value='"
+                     ++ Title ++ "' data-map-type='row' data-map='"
+                     ++ Map ++ "' />",
+                 {webcontrol, {Form, {Title, 1, 1, Incs}}, Html}
+    end.
+
 'create.button'(List) when is_list(List) ->
     Id = "id_" ++ muin_util:create_name(),
     [Title | Commands] = typechecks:std_strs(List),
     case Commands of
-        [] -> ?ERR_VAL;
+        [] -> ?ERRVAL_VAL;
         _  ->
             Origin = hn_util:list_to_path(muin:context_setting(path)),
             % first we create the list of commands records
@@ -43,11 +65,12 @@
             Html = lists:flatten("<input id='" ++ Id ++ "' type='submit' "
                                  ++ "class='hn-webcontrol' value='"
                                  ++ Title ++ "' data-payload='" ++ Json ++ "' "
-                                 ++ "' data-action='postcreatepages'"
+                                 ++ "data-action='postcreatepages'"
                                  ++ "data-origin='" ++ Origin ++ "' />"),
             % we stash the record set as the form definition and when the POST
-            % comes in from the button we will translate it back into a record format
-            % and then compare the records to see if the action is approved
+            % comes in from the button we will translate it back into a
+            % record format and then compare the records to see if the
+            % action is approved
             Form = #form{id = {'create-button', Title}, kind = "create-button",
                          attrs = Commands3},
             {webcontrol, {Form, {Title, 1, 1, #incs{}}}, Html}
