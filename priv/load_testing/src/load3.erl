eab9cbb1d7650e8111589586b9ab66deb05fc69f
Gordon Guthrie <gordon@hypernumbers.com>
2011-06-02 08:50:04 +0100

    bits and bobs for load testing

--- priv/load_testing/src/load3.erl
+++ priv/load_testing/src/load3.erl
@@ -1,7 +1,8 @@
 -module(load3).
 
 -export([
-         load/0
+         load/0,
+         fprof/0
         ]).
 
 -record(refX,
@@ -12,30 +13,54 @@
           obj         = null
          }).
 
--define(size, 10).
+-define(size, 100).
 -define(site, "http://hypernumbers.dev:9000").
+-define(doublequote, 34). % use insted of $"
 
-load() -> Templates = ["calcs", "data"],
+fprof() ->
+    Dir = "/media/logging/",
+    Now = dh_date:format("y_M_d_h_i_s"),
+    Log = Dir ++ "fprof_" ++ Now ++ ".log",
+    Analysis = Dir ++ "fprof_analysis" ++ Now ++ ".log",
+    fprof:trace([start, {file, Log}, {procs, all}]),
+    Seg1 = "fprof_page",
+    Seg2 = "data",
+    RefX1 = #refX{site = ?site, path = [Seg1], obj = {page, "/"}},
+    RefX2 = #refX{site = ?site, path = [Seg1, Seg2], obj = {page, "/"}},
+    io:format("loading ~p~n", [RefX1]),
+    ok = hn_templates:load_template(RefX1, "meter_page"),
+    io:format("loading ~p~n", [RefX2]),
+    ok = hn_templates:load_template(RefX2, "data"),
+    new_db_api:wait_for_dirty(?site),
+    fprof:trace(stop),
+    fprof:profile([{file, Log}]),
+    fprof:analyse([{dest, Analysis}]),
+    ok.
+
+load() -> Templates = ["meter_page", "data"],
           copy(Templates),
           l2(?size).
 
 l2(0) -> ok;
-l2(N) -> Seg1 = "calcs" ++ integer_to_list(N),
+l2(N) -> Seg1 = "meter_page" ++ integer_to_list(N),
          Seg2 = "data",
          RefX1 = #refX{site = ?site, path = [Seg1], obj = {page, "/"}},
          RefX2 = #refX{site = ?site, path = [Seg1, Seg2], obj = {page, "/"}},
          io:format("loading ~p~n", [RefX1]),
-         ok = hn_templates:load_template(RefX1, "calcs"),
+         ok = hn_templates:load_template(RefX1, "meter_page"),
          io:format("loading ~p~n", [RefX2]),
          ok = hn_templates:load_template(RefX2, "data"),
          syslib:limiter(?site),
+         syslib:log("loading " ++ integer_to_list(N) ++ ","
+                    ++ integer_to_list(util2:get_timestamp()), "load.log"),
          l2(N - 1).
 
 copy([])      -> ok;
 copy([H | T]) ->
     Site = "hypernumbers.dev\\&9000",
     Root = code:lib_dir(hypernumbers) ++ "/../../",
-    Cmd = "cp " ++ [$"] ++ Root ++ "priv/load_testing/priv/" ++ H ++ ".json" ++
-                [$"] ++ " " ++ Root ++ "var/sites/" ++ Site ++ "/templates/",
+    Cmd = "cp " ++ [?doublequote] ++ Root ++ "priv/load_testing/priv/"
+        ++ H ++ ".json" ++ [?doublequote] ++ " " ++ Root ++ "var/sites/"
+        ++ Site ++ "/templates/",
     _Ret = os:cmd(Cmd),
     copy(T).
