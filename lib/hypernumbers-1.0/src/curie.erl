c5759aa4a33370b16186213afc46ac232fb80400
jchlanda <j.chlanda@gmail.com>
2011-06-07 11:32:53 -0700

    fixed recursive call in walk. abstracted its functionality to a new function, walk_helper

--- lib/hypernumbers-1.0/src/curie.erl
+++ lib/hypernumbers-1.0/src/curie.erl
@@ -44,9 +44,33 @@ build_fun2(RetRef, AST, ParamList, Site, Path) ->
 %	Do you have to check off page reference for each cell that is in the AST?
 %	If so, find the place where to place call to check_off_page,
 %	to find the answer see how muin_util:walk_path works.
-walk([], _, _, Acc,_ ,_) -> lists:reverse(Acc);
+walk([], _, _, Acc,_ ,_) -> lists:reverse(Acc),
+	io:format("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!ACC is ~p~n", [Acc]);
 walk([{cellref, {offset, X}, {offset, Y}, _, _} = H | T], Ref, Params, Acc, Site, Path) ->
 	io:format("---------walk cellref---------~n"),
+	walk([walk_helper(H, Ref, Params, Site, Path) | T], Ref, Params, Acc, Site, Path);
+	
+	
+%TODO
+%	-translate range to list of cell names ("b1:b3" = ["b1","b2","b3"])
+%	-update function contains, so it can take two lists as argument and 
+%	 return true if list1 is a sublist of list2
+%	-then walk through the list of cells
+walk([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, RangeString}], Ref, Params, Acc, Site, Path)	->
+	io:format("TODO: Rangeref is ~p~n", [RangeString]);
+walk([H | T], Ref, Params, Acc, Site, Path)	when is_list(H)	->
+	io:format("walk, head is a list: ~p~n", [H]),
+	walk(T, Ref, Params, [walk(H, Ref, Params, [], Site, Path) | Acc], Site, Path);
+walk([H | T], Ref, Params, Acc, Site, Path) ->
+	io:format("skipped: ~p~n",[H]),
+    walk(T, Ref, Params, [H | Acc], Site, Path).
+
+
+%if in a AST there is a reference to a cell from outside the range this function is called,
+%it takes the cell's reference and retreives a AST which the cell holds
+walk_helper(NewReference, Ref, Params, Site, Path)	->
+	io:format("---------walk helper---------~n"),
+	{cellref, {offset, X}, {offset, Y}, _, _} = NewReference,
 	#refX{obj = {cell, {OldX, OldY}}} = Ref,
 	NewX = OldX + X,
 	NewY = OldY + Y,
@@ -57,34 +81,17 @@ walk([{cellref, {offset, X}, {offset, Y}, _, _} = H | T], Ref, Params, Acc, Site
     io:format("NewRetRef is ~p~n", [NewRetRef]),
     case contains(Params, NewObj) of
 		true	-> 
-			io:format("Cell is in input range: ~p~n",[H]),
-			walk(T, Ref, Params, [H | Acc], Site, Path);
-		%TODO
-		%	!!!!!!!!!!!!!
-		%	If called recursively, the function generates error, probably because it tries to assign a new value to already created variable,
-		%	Probably better to export it to a new function which processes the input, gives the result and terminates,
-		%	so each call will have its own "fresh" variables.
-		false	-> io:format("--------------walk recursively--------------~n"),
+			io:format("Cell is in input range: ~p~n",[NewReference]),
+			NewReference;
+		
+		
+		false	-> 
+			io:format("--------------walk_helper false--------------~n"),
 			[{_, AST2}] = new_db_api:read_attribute(NewRetRef, "__ast"),
 			io:format("AST2 is ~p~n", [AST2]),
-			List = walk(AST2, NewObj, Params, [], Site, Path),
-			io:format("List is ~p~n", [List]),
-			walk(T, Ref, Params, [List, Acc], Site, Path)
-	end;
-%TODO
-%	-translate range to list of cell names ("b1:b3" = ["b1","b2","b3"])
-%	-update function contains, so it can take two lists as argument and 
-%	 return true if list1 is a sublist of list2
-%	-then walk through the list of cells
-walk([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, RangeString}], Ref, Params, Acc, Site, Path)	->
-	io:format("TODO: Rangeref is ~p~n", [RangeString]);
-walk([H | T], Ref, Params, Acc, Site, Path)	when is_list(H)	->
-	List = walk(H, Ref, Params, [], Site, Path),
-	walk(T, Ref, Params, [List | Acc], Site, Path);
-walk([H | T], Ref, Params, Acc, Site, Path) ->
-	io:format("skipped: ~p~n",[H]),
-    walk(T, Ref, Params, [H | Acc], Site, Path).
-
+			AST2
+	end,
+			io:format("++++++++++++++++++++++++walk helper end~n").
 
 check_off_page([], _Path) -> valid;
 check_off_page([{cellref, _, _, P, _} | T], Path) ->
