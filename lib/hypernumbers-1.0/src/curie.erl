8cd45c7b8cb36147ff980e0298daf3e4d2199626
Jakub Chlanda <j.chlanda@gmail.com>
2011-06-10 13:17:42 +0100

    rangeref case now in place. build_fun returns an AST based on the content of result cell, but only with references to input cells in it.

--- lib/hypernumbers-1.0/src/curie.erl
+++ lib/hypernumbers-1.0/src/curie.erl
@@ -48,63 +48,38 @@ walk([{cellref, _OffsetX, _OffsetY, _Path, Cell} = H | T], Ref, Params, Acc, Sit
 		false	->
 			walk([walk_helper_cellref(H, Ref, Params, Site, Path) | T], walk_new_ret_ref(H, Site), Params, Acc, Site, Path, _FinalRetRef)
 	end;
-%TODO
-%	-translate range to list of cell names ("b1:b3" = ["b1","b2","b3"])
-%	-update function contains, so it can take two lists as argument and 
-%	 return true if list1 is a sublist of list2
-%	-then walk through the list of cells
-%	-!!!!!!remember to remove underscores from arguments
-walk([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, RangeString} = H | T], Ref, Params, Acc, Site, Path, FinalRetRef)	->
-	io:format("Hello rangeref~n"),
-	%(#refX{obj = {range, {X1, Y1, X2, Y2}}} = RefX)
-	
-	%get return cell coordinates:
-	%Ref is {refX,"http://hypernumbers.dev:9000",undefined,"./",{cell,{2,6}}}
-
-	
-	
-	io:format("Ref is ~p~nFinalRetRef is ~p~n", [Ref, FinalRetRef]),
-	
-
-	io:format("Ref is ~p~n", [Ref]),
-	walk_helper_rangeref(Ref, FinalRetRef,hn_util:range_to_list(#refX{site = Site, path = Path,obj = {range, {StartX, StartY, StopX, StopY}}})),
-	
-	
-	walk(T, Ref, Params, [H | Acc], Site, Path, FinalRetRef);
-	
+%Translates range to list of cell names ("b1:b3" = ["b1","b2","b3"])
+walk([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, _RangeString} | T], Ref, Params, Acc, Site, Path, FinalRetRef)	->
+	walk([walk_helper_rangeref(Ref, FinalRetRef,hn_util:range_to_list(#refX{site = Site, path = Path,obj = {range, {StartX, StartY, StopX, StopY}}})) |T], Ref, Params, Acc, Site, Path, FinalRetRef);	
 walk([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef)	when is_list(H)	->
 	walk(T, Ref, Params, [walk(H, Ref, Params, [], Site, Path, _FinalRetRef) | Acc], Site, Path, _FinalRetRef);
 walk([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef) ->
-	io:format("-skipped ~p~n", [H]),
 	walk(T, Ref, Params, [H | Acc], Site, Path, _FinalRetRef).
 
 
 %used when AST contains a reference to a cell not in the input range,
-walk_new_ret_ref(NewReference, Site)	->
-	{cellref, _OffsetX, _OffsetY, Path, Cell} = NewReference,
+%walk_new_ret_ref(NewReference, Site)
+walk_new_ret_ref({cellref, _OffsetX, _OffsetY, Path, Cell}, Site)	->
 	%calculate new refX
-	NewRetRef = #refX{site = Site, path = Path,
+	#refX{site = Site, path = Path,
                    obj = hn_util:parse_ref(Cell)}.
                    
 walk_helper_rangeref(CurrentRef, FinalRetRef, List)	->
-	io:format("walk_helper_rangeref:~nCurrentRef is ~p~nFinalRetRef is ~p~nList is ~p~n", [CurrentRef, FinalRetRef, List]),
 	{_, _, _, _, {cell, {ReturnX, ReturnY}}} = FinalRetRef,
 	{_, _, _, _, {cell, {CurrentX, CurrentY}}} = CurrentRef,
 	DeltaX = CurrentX - ReturnX, 
 	DeltaY = CurrentY - ReturnY,
-	io:format("DeltaX is ~p~nDeltaY is ~p~n", [DeltaX, DeltaY]),
-	io:format("walk_helper_rangeref END:~n"),
-	update_rangeref_offset(DeltaX, DeltaY, List, ReturnX, ReturnY, []).
+	update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, List, ReturnX, ReturnY, []).
 
-update_rangeref_offset(DeltaX, DeltaY, [{refX, _, _, _, {cell,{X,Y}}} = H | T], ReturnX, ReturnY, Acc)	->a. 
-
-
-
+update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, [{refX, _A, _B, _C, {cell,{X,Y}}} | T], ReturnX, ReturnY, Acc)	->
+	update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, T, ReturnX, ReturnY, [refX_to_cellref(FinalRetRef, {refX, _A, _B, _C, {cell,{X + DeltaX,Y + DeltaY}}}) |Acc]);
+update_rangeref_offset(_FinalRetRef, _DeltaX, _DeltaY, [], _ReturnX, _ReturnY, Acc)	->	
+	Acc.
 
 
 %if in an AST there is a reference to a cell from outside of the input range this function is called,
 %it takes the cell's reference and retreives an AST which the cell holds
-walk_helper_cellref(NewReference, Ref, Params, Site, Path)	->
+walk_helper_cellref(NewReference, _Ref, Params, Site, Path)	->
 	%get the cell name and store it in Cell
 	{cellref, {offset, OffsetX}, {offset, OffsetY}, _Path, Cell} = NewReference,
 	%calculate new refX
@@ -121,11 +96,19 @@ walk_helper_cellref(NewReference, Ref, Params, Site, Path)	->
 					update_cellref_offset(AST, OffsetX, OffsetY, [])
 			end
 	end.
-	
+
+
+
+%{refX,"http://hypernumbers.dev:9000", undefined, ["function"], {cell,{0,-5}}}
+%{cellref,{offset,0},{offset,-7},"./","B1"},	
+%get cell's name from its coordinates
+%refX_to_cellref(FinalRetRef, CurrentCell)
+refX_to_cellref({refX, _, _, _, {cell, {FinalX, FinalY}}}, {refX, _, _, _, {cell, {X, Y}}})	->
+		{cellref, {offset, X}, {offset, Y}, "./", hn_util:obj_to_ref({cell, {X + FinalX, Y + FinalY}})}.
+
 
 update_cellref_offset([], _, _, Acc)	-> 
-	NewList = lists:reverse(Acc),
-	NewList;
+	lists:reverse(Acc);
 update_cellref_offset([H | T], X, Y, Acc) when is_list(H)	->
 	update_cellref_offset(T, X, Y, [ update_cellref_offset(H , X, Y, []) | Acc]);
 update_cellref_offset([{cellref, {offset, OldX}, {offset, OldY}, _A, _B} | T], X, Y, Acc)	->
