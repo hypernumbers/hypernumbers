4a5aaa66329a9aee8f3b3649dc12358e08ee8695
Jakub Chlanda <j.chlanda@gmail.com>
2011-06-15 13:13:34 +0100

    code tidied up and comments added. build_fun now starts with checking if final return cell contains ast. asts are obtained by calling new_db_api:read_ref/1, so it doesn't crash if ther is no ast. some simple test cases added to http://iu22.tiny.hn/#tour=true,path=/function/

--- lib/hypernumbers-1.0/src/curie.erl
+++ lib/hypernumbers-1.0/src/curie.erl
@@ -1,73 +1,97 @@
 %%% @author    Gordon Guthrie
+%%% @author    Jakub Chlanda
 %%% @copyright (C) 2011, Hypernumbers Ltd
 %%% @doc
-%%%
+%%%		Implements famoust paper "A User-Centred Approach to Functions in Excel"
+%%%		by Simon Peyton Jones, Alan Blackwell and Margaret Burnett
 %%% @end
-%%% Created :  3 Jun 2011 by gordon@hypernumbers.com
+%%% Created :  15 Jun 2011
 
 -module(curie).
 
 -include("spriki.hrl").
 
--compile(export_all).
-%-export([
-%         build_fun/4
-%        ]).
+%-compile(export_all).
+-export([
+         build_fun/4,
+         get_cells_ast/3
+        ]).
 
 
 
 %%TODO
 
-%-check if result cell is empty, and if yes react accordingly, rather than just crush :)
+%in muin:run_code(Pcode, #muin_rti{site=Site, path=Path,
+                          %~ col=Col,   row=Row,
+                          %~ array_context=AryCtx,
+                          %~ auth_req=AuthReq})
+%~ Pcode is ['/',
+          %~ {cellref,{offset,0},{offset,-3},"./","B7"},
+          %~ {cellref,{offset,0},{offset,-2},"./","B8"}]
+%~ #muin_rti is {muin_rti,"http://hypernumbers.dev:9000", ["function"], 2,11,false,nil}
+
 
 
 
 %%curie:build_fun("http://hypernumbers.dev:9000", ["function"], "b8", ["b1", "b2", "b3"]).
 build_fun(Site, Path, ReturnRef, ParamList) ->
-	RetRef = #refX{site = Site, path = Path,
-                   obj = hn_util:parse_ref(ReturnRef)},
-    [{_, AST}] = new_db_api:read_attribute(RetRef, "__ast"),
-    io:format("~n---------Initial AST is~n~p~n~n", [AST]),
-    %params in upper case, easier to compare
-    ParamListUpper = lists:map(fun string:to_upper/1, ParamList),
-	%no cells from different workbooks are allowed
-	%check return cell
-    case check_off_page(AST, Path) of
-        invalid -> {error, off_page_reference};
-        valid   ->
-			case check_input_off_reference(ParamList, Site, Path) of
-					invalid -> {error, off_page_reference};
-					valid   -> build_fun2(RetRef, AST, ParamListUpper, Site, Path)
-			end
-    end.
-   
+
+	My_AST = get_cells_ast(ReturnRef, Site, Path),
+	case is_list(My_AST) of
+		false	-> 
+				io:format("~n---------Initial AST is~n~p~n~n", [My_AST]),
+			    io:format("~n---------Final AST is~n"),
+			    My_AST;
+		true	->
+				case My_AST of
+					[]		-> {error, no_ast_in_final_result};
+					_AST	->
+						RetRef = #refX{site = Site, path = Path,
+									   obj = hn_util:parse_ref(ReturnRef)},
+						io:format("~n---------Initial AST is~n~p~n~n", [_AST]),
+						%params in upper case, easier to compare
+						ParamListUpper = lists:map(fun string:to_upper/1, ParamList),
+						%no cells from different workbooks are allowed
+						%check return cell
+						case check_off_page(_AST, Path) of
+							invalid -> {error, off_page_reference};
+							valid   ->
+								case check_input_off_reference(ParamList, Site, Path) of
+										invalid -> {error, off_page_reference};
+										valid   -> build_fun2(RetRef, _AST, ParamListUpper, Site, Path)
+								end
+						end
+			   end
+	end.
+	
 
 build_fun2(RetRef, AST, ParamList, Site, Path) ->
-	NewAST = walk_zero(AST, RetRef, ParamList, [], Site, Path),
+	io:format("~n---------Final AST is~n"),
+	walk(AST, RetRef, ParamList, [], Site, Path).
 	
-    io:format("~n---------Final AST is~n~p~n~n", [NewAST]).
     
+%main function of the program, takes AST and walks down it. 
+%returns new AST which only contains references to input cells.
+walk(AST, RetRef, ParamList, [], Site, Path)	-> walk2(AST, RetRef, ParamList, [], Site, Path, RetRef).
 
 
-walk_zero(AST, RetRef, ParamList, [], Site, Path)	-> walk(AST, RetRef, ParamList, [], Site, Path, RetRef).
-
-
-walk([], _, _, Acc,_ ,_, _) ->
+%added final result cell to make global offset calculation possible
+walk2([], _, _, Acc,_ ,_, _) ->
 	lists:reverse(Acc);
-walk([{cellref, _OffsetX, _OffsetY, _Path, Cell} = H | T], Ref, Params, Acc, Site, Path, _FinalRetRef) ->
+walk2([{cellref, _OffsetX, _OffsetY, _Path, Cell} = H | T], Ref, Params, Acc, Site, Path, _FinalRetRef) ->
 	case contains(Params, Cell) of
 		true	->
-			walk(T, Ref, Params, [H | Acc], Site, Path, _FinalRetRef);
+			walk2(T, Ref, Params, [H | Acc], Site, Path, _FinalRetRef);
 		false	->
-			walk([walk_helper_cellref(H, Ref, Params, Site, Path) | T], walk_new_ret_ref(H, Site), Params, Acc, Site, Path, _FinalRetRef)
+			walk2([walk_helper_cellref(H, Ref, Params, Site, Path) | T], walk_new_ret_ref(H, Site), Params, Acc, Site, Path, _FinalRetRef)
 	end;
 %Translates range to list of cell names ("b1:b3" = ["b1","b2","b3"])
-walk([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, _RangeString} | T], Ref, Params, Acc, Site, Path, FinalRetRef)	->
-	walk([walk_helper_rangeref(Ref, FinalRetRef,hn_util:range_to_list(#refX{site = Site, path = Path,obj = {range, {StartX, StartY, StopX, StopY}}})) |T], Ref, Params, Acc, Site, Path, FinalRetRef);	
-walk([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef)	when is_list(H)	->
-	walk(T, Ref, Params, [walk(H, Ref, Params, [], Site, Path, _FinalRetRef) | Acc], Site, Path, _FinalRetRef);
-walk([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef) ->
-	walk(T, Ref, Params, [H | Acc], Site, Path, _FinalRetRef).
+walk2([{rangeref, finite, _, {{offset, StartX}, {offset, StartY}}, {{offset, StopX}, {offset, StopY}}, _, _, _RangeString} | T], Ref, Params, Acc, Site, Path, FinalRetRef)	->
+	walk2([walk_helper_rangeref(Ref, FinalRetRef,hn_util:range_to_list(#refX{site = Site, path = Path,obj = {range, {StartX, StartY, StopX, StopY}}})) |T], Ref, Params, Acc, Site, Path, FinalRetRef);	
+walk2([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef)	when is_list(H)	->
+	walk2(T, Ref, Params, [walk2(H, Ref, Params, [], Site, Path, _FinalRetRef) | Acc], Site, Path, _FinalRetRef);
+walk2([H | T], Ref, Params, Acc, Site, Path, _FinalRetRef) ->
+	walk2(T, Ref, Params, [H | Acc], Site, Path, _FinalRetRef).
 
 
 %used when AST contains a reference to a cell not in the input range,
@@ -76,7 +100,8 @@ walk_new_ret_ref({cellref, _OffsetX, _OffsetY, Path, Cell}, Site)	->
 	%calculate new refX
 	#refX{site = Site, path = Path,
                    obj = hn_util:parse_ref(Cell)}.
-                   
+   
+                
 walk_helper_rangeref(CurrentRef, FinalRetRef, List)	->
 	{_, _, _, _, {cell, {ReturnX, ReturnY}}} = FinalRetRef,
 	{_, _, _, _, {cell, {CurrentX, CurrentY}}} = CurrentRef,
@@ -84,6 +109,8 @@ walk_helper_rangeref(CurrentRef, FinalRetRef, List)	->
 	DeltaY = CurrentY - ReturnY,
 	update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, List, ReturnX, ReturnY, []).
 
+
+%keep track of the offset
 update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, [{refX, _A, _B, _C, {cell,{X,Y}}} | T], ReturnX, ReturnY, Acc)	->
 	update_rangeref_offset(FinalRetRef, DeltaX, DeltaY, T, ReturnX, ReturnY, [refX_to_cellref(FinalRetRef, {refX, _A, _B, _C, {cell,{X + DeltaX,Y + DeltaY}}}) |Acc]);
 update_rangeref_offset(_FinalRetRef, _DeltaX, _DeltaY, [], _ReturnX, _ReturnY, Acc)	->	
@@ -111,15 +138,16 @@ walk_helper_cellref(NewReference, _Ref, Params, Site, Path)	->
 	end.
 
 
-
+%get cell's name from its coordinates
 %{refX,"http://hypernumbers.dev:9000", undefined, ["function"], {cell,{0,-5}}}
 %{cellref,{offset,0},{offset,-7},"./","B1"},	
-%get cell's name from its coordinates
 %refX_to_cellref(FinalRetRef, CurrentCell)
 refX_to_cellref({refX, _, _, _, {cell, {FinalX, FinalY}}}, {refX, _, _, _, {cell, {X, Y}}})	->
 		{cellref, {offset, X}, {offset, Y}, "./", hn_util:obj_to_ref({cell, {X + FinalX, Y + FinalY}})}.
 
 
+%when hopping from cell to cell (going down the AST) offset has to be updated,
+%to make sure it always points to the final result cell
 update_cellref_offset([], _, _, Acc)	-> 
 	lists:reverse(Acc);
 update_cellref_offset([H | T], X, Y, Acc) when is_list(H)	->
@@ -129,16 +157,14 @@ update_cellref_offset([{cellref, {offset, OldX}, {offset, OldY}, _A, _B} | T], X
 update_cellref_offset([H | T], X, Y , Acc)	-> 	update_cellref_offset(T, X, Y , [H | Acc]).
 
 
-
+%check if a cell points away from a specified worksheet
 %check_off_page({cellref, {offset,-1}, {offset,-57}, "/page1/","/page1/a1"}, ["function"])
-
 check_off_page([], _Path) -> valid;
 check_off_page([{cellref, _, _, P, _} | T], Path) ->
 	case muin_util:walk_path(Path, P) of
         Path -> 
 			check_off_page(T, Path);
         _    -> 
-			io:format("    INVALID~n"),
 			invalid
     end;
 check_off_page([_H | T], Path) ->
@@ -154,24 +180,28 @@ contains([_H | T], Element)	->
 	contains(T, Element).
 	
 
-check_input_off_reference([], _Site, _Path)	-> valid;
+%check if specified input list contains off page references
+check_input_off_reference([], _Site, _Path) -> valid;
 check_input_off_reference([H | T], Site, Path) ->
-	
-    case check_off_page([get_ast(get_cell_info(H, Site, Path))], Path) of
+    case check_off_page([get_cells_ast(H, Site, Path)], Path) of
         invalid -> invalid;
-        valid   -> check_input_off_reference(T, Site, Path)
+        valid -> check_input_off_reference(T, Site, Path)
     end.
 
 
-get_cell_info(H, Site, Path)	->
+%return cell's abstract syntax tree (not a tuple but just a list)
+get_cells_ast(H, Site, Path)	->
 	Cell = #refX{site = Site, path = Path,
                    obj = hn_util:parse_ref(H)},
-    try new_db_api:read_attribute(Cell, "__ast") of
-		Val -> Val
-	catch
-		error	-> []
-    end.
-    
-
-get_ast([])	-> [];
-get_ast([{_z, AST} | _T])	->	AST.
+    List = new_db_api:read_ref(Cell),
+    case List of 
+		[]	-> [];
+		_List	->
+			[{_address, Properties}] = _List,
+			Tuple = lists:keyfind("__ast", 1, Properties),
+			case Tuple of 
+				false			-> [];
+				{"__ast", AST}	-> AST;
+				[]				-> []
+			end
+	end.
