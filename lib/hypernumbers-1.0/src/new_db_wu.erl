af036a70fd11b8ee3ddde93a243d885056ddd892
Gordon Guthrie <gordon@hypernumbers.com>
2011-07-07 09:00:54 +0100

    clear contents now does for wiki stuff too

--- lib/hypernumbers-1.0/src/new_db_wu.erl
+++ lib/hypernumbers-1.0/src/new_db_wu.erl
@@ -223,9 +223,9 @@ clear_cells(RefX, Uid) -> clear_cells(RefX, contents, Uid).
 
 -spec clear_cells(#refX{}, all | style | contents | tuple(), auth_srv:uid()) -> ok.
 clear_cells(Ref, contents, Uid) ->
-    do_clear_cells(Ref, content_attrs(), clear, Uid);
+    do_clear_cells(Ref, ["input", content_attrs()], clear, Uid);
 clear_cells(Ref, all, Uid) ->
-    do_clear_cells(Ref, ["style", "merge" | content_attrs()], clear, Uid);
+    do_clear_cells(Ref, ["style", "merge", "input" | content_attrs()], clear, Uid);
 clear_cells(Ref, style, Uid) ->
     do_clear_cells(Ref, ["style", "merge"], ignore, Uid);
 clear_cells(Ref, {attributes, DelAttrs}, Uid) ->
