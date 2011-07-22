761142f28f99478643aa46a6492c68296b83736e
Jakub Chlanda <j.chlanda@gmail.com>
2011-07-22 12:39:54 +0100

    added new functions to test the functionality of the module: test_write/8, test_delete/2, and test_read/2, see the comments above each function for sample function calls..

--- lib/hypernumbers-1.0/src/curie_test.erl
+++ lib/hypernumbers-1.0/src/curie_test.erl
@@ -9,18 +9,20 @@
 -module(curie_test).
 
 -export([
-         test_me/8
+         test_write/8,
+         test_delete/2,
+         test_read/2
         ]).
 
 %%TODO
 	%~ Make sure that cookie is valid, it lives only 7 days, find the way of getting new cookie every time the function is called!
 %%TODO
 
-%~ curie_test:test_me("http://hypernumbers.dev:9000", ["page1"],"b1", "b2", "b8", ["b5", "b6"], ["c5", "c6"], ["f5", "f6"]).
-test_me(Site, Page, Name, Description, OutputValue, ListOfParameterNames, ListOfParameterDescriptions, ListOfParameterValues)	->
+%~ curie_test:test_write("http://hypernumbers.dev:9000", ["page1"],"b1", "b2", "b8", ["b5", "b6"], ["c5", "c6"], ["f5", "f6"]).
+test_write(Site, Page, Name, Description, OutputValue, ListOfParameterNames, ListOfParameterDescriptions, ListOfParameterValues)	->
     
     String_Page = get_page(Page, ""),
-    Entry = 	{struct,[	{"type","user_defined"},
+    Entry = 	{struct,[	{"type","user_defined_write"},
 							{"properties",
 				{struct,[	{"site", Site},
 							{"page", String_Page},
@@ -41,8 +43,48 @@ test_me(Site, Page, Name, Description, OutputValue, ListOfParameterNames, ListOf
                          "", Json_Entry }, [], []).
 	
 	
+%~ curie_test:test_delete("http://hypernumbers.dev:9000", "user.normalise").	
+test_delete(Site, Function_Name)	->
+		Entry = 	{struct,[	{"type","user_defined_delete"},
+							{"properties",
+								{struct,[	{"site", Site},
+											{"function_name", Function_Name}
+										]
+								}
+							}
+						]},
+	
+	Json_Entry = lists:flatten(mochijson:encode(Entry)),
+	%~ Cookie lives only 7 days!
+    Auth = "test!hypernumbers.com|90a9b1042a97f45c008d1949b3cf25a2|63478978880|e50fa7683a5852dbd57fc4b62406c3b4",
+    httpc:request(post, {"http://hypernumbers.dev:9000/page1/",
+                         [{"host","hypernumbers.dev:9000"},
+                          {"accept", "application/json"},
+                          {"cookie", Auth}],
+                         "", Json_Entry }, [], []).
+	
+	
+%~ curie_test:test_read("http://hypernumbers.dev:9000", "user.normalise").	
+test_read(Site, Function_Name)	->
+	Entry = 	{struct,[	{"type","user_defined_read"},
+							{"properties",
+								{struct,[	{"site", Site},
+											{"function_name", Function_Name}
+										]
+								}
+							}
+						]},
+	
+	Json_Entry = lists:flatten(mochijson:encode(Entry)),
+	%~ Cookie lives only 7 days!
+    Auth = "test!hypernumbers.com|90a9b1042a97f45c008d1949b3cf25a2|63478978880|e50fa7683a5852dbd57fc4b62406c3b4",
+    httpc:request(post, {"http://hypernumbers.dev:9000/page1/",
+                         [{"host","hypernumbers.dev:9000"},
+                          {"accept", "application/json"},
+                          {"cookie", Auth}],
+                         "", Json_Entry }, [], []).
+	
 get_parameters(_Site, _Page, [], [], [], Parameters)	->
-	io:format("Parameters are: ~p~n", [Parameters]),
 	lists:reverse(Parameters);
 
 get_parameters(Site, Page, [H | T], [H2 | T2], [H3 | T3], Parameters)	->
