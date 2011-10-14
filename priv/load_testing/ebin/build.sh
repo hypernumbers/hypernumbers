4567f32e8c94932b8a9656cf414804a12313e148
Gordon Guthrie <gordon@hypernumbers.com>
2011-10-14 17:21:33 +0100

    fixed build.sh

--- priv/load_testing/ebin/build.sh
+++ priv/load_testing/ebin/build.sh
@@ -1,3 +1,3 @@
 erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../include ../src/load_testing.erl
 erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../include ../src/load_util.erl
-erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../../../lib/hypernumbers-1.0/include ../src/table_testing.erl
+x
