1aebd7ff5c390c46a78f6eb7b574776154b9747b
Gordon Guthrie <gordon@hypernumbers.com>
2011-10-24 14:51:37 +0100

    batch job to count up datastore errors...

---
+++ priv/verification/count.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Invalid tables (type 1)"
+grep "Invalid tables (type 1)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid tables (type 2)"
+grep "Invalid tables (type 2)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid relations (type 1)"
+grep "Invalid relations (type 1)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid relations (type 2)"
+grep "Invalid relations (type 2)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid relations (type 3)"
+grep "Invalid relations (type 3)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid include (type 1)"
+grep "Invalid include (type 1)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid include (type 2)"
+grep "Invalid include (type 2)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid timer"
+grep "Invalid timer" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid form"
+grep "Invalid form" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid formula"
+grep "Invalid formula" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (cell) (type 1)"
+grep "Invalid Object (cell) (type 1)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (cell) (type 2)"
+grep "Invalid Object (cell) (type 2)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (row) (type 3)"
+grep "Invalid Object (row) (type 3)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (column) (type 4)"
+grep "Invalid Object (column) (type 4)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (page) (type 5)"
+grep "Invalid Object (page) (type 5)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid Object (type 6)"
+grep "Invalid Object (type 6)" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid types"
+grep "Invalid types" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid grid"
+grep "Invalid grid" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid reverse index"
+grep "Invalid reverse index" errors.20_Oct_11_13_27_19.terms | wc -l
+echo "Invalid zinf"
+grep "Invalid zinf" errors.20_Oct_11_13_27_19.terms | wc -l
