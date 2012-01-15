8ea8e6b524484349314654e66143dd8c76a7fd61
Gordon Guthrie <gordon@hypernumbers.com>
2012-01-15 15:08:52 +0000

    remove line space

--- lib/hypernumbers-1.0/src/hn_mochi.erl
+++ lib/hypernumbers-1.0/src/hn_mochi.erl
@@ -601,7 +601,6 @@ iget(Ref, Obj, #qry{view=?WEBPAGE}, Env=#env{accept=html,uid=Uid})
     Page = hn_render:wrap_page(Html, Width, Height, Addons, "webpage"),
     text_html_nocache(Env, Page);
 
-
 iget(Ref=#refX{site=S}, page, #qry{view=FName},
      Env=#env{accept=html, uid=Uid})
   when FName /= undefined ->
