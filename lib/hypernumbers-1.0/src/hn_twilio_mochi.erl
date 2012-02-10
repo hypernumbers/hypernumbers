4bf73c17dfa9bc869fcdce7b4ea61ec5ab6f134f
Gordon Guthrie <gordon@hypernumbers.com>
2012-02-10 13:46:28 +0000

    added a blank twilio handler...

---
+++ lib/hypernumbers-1.0/src/hn_twilio_mochi.erl
@@ -0,0 +1,16 @@
+%%% @author    Gordon Guthrie
+%%% @copyright (C) 2012, Hypernumbers Ltd
+%%% @doc       This module handles Twilio input for hn_mochi
+%%%
+%%% @end
+%%% Created : 10 Feb 2012 by gordon@hypernumbers.com
+
+-module(hn_twilio_mochi).
+
+-export([
+         handle/3
+         ]).
+
+handle(Ref, Qry, Env) ->
+    ok.
+
