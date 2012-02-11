e505057c788bec50be346c04b7681a4742121b47
gordon <gordon@hypernumbers.com>
2012-02-11 11:04:09 +0000

    some changes to the phone functions

--- lib/formula_engine-1.0/src/hnfuns_controls.erl
+++ lib/formula_engine-1.0/src/hnfuns_controls.erl
@@ -13,29 +13,39 @@
          'map.sheet.button'/1,
          'map.custom.button'/1,
          'load.templates.button'/1,
-         'twilio.phone.out'/1
+         'phone.out'/1,
+         'phone.in'/1
         ]).
 
 -include("spriki.hrl").
 -include("errvals.hrl").
 
-'twilio.phone.out'(List) ->
-    Title = "Twilio Phone Outbound",
-    %% we call them hn.twiliolib.* because of a name collision bug
-    %% with the twilio libraries
-    Js = ["/webcomponents/hn.twiliolib.js",
+'phone.in'([]) ->
+    Title = "Phone In",
+    Site = get(site),
+    Path = get(path),
+    X = get(mx),
+    Y = get(my),
+    URL = hn_util:refX_to_url(#refX{site = Site, path = Path,
+                                    obj = {cell, {X, Y}}}),
+    Link = "<a href='" ++ URL ++ "?view=phone' target='hnsoftphone'>Soft Phone</a>",
+    {preview, {"Phone In", 1, 1, #incs{}}, Link}.
+
+'phone.out'(List) ->
+    Title = "Phone Out",
+    Js = ["/webcomponents/hn.phone.js",
           "http://static.twilio.com/libs/twiliojs/1.0/twilio.js"],
-    Reload = ["HN.Twilio.phone_outbound_reload();"],
-    CSS = ["/webcomponents/hn.twiliolib.css"],
+    Reload = ["HN.Phone.phone_outbound_reload();"],
+    CSS = ["/webcomponents/hn.phone.css"],
     Incs = #incs{js = Js, js_reload = Reload, css = CSS},
     PhoneNo = "07776 251669",
     Payload = {struct, [{'twilio_outboundphone', PhoneNo}]},
-    HTML = "<button id='hn-twilio-outboundphone' value='Twilio Phone' " ++
+    HTML = "<button id='hn-outboundphone' value='Phone' " ++
         "data-phoneno='" ++ PhoneNo ++"' disabled>" ++
-        "<div class='hn-twilio-outbound_img'>" ++ "Twilio Phone" ++ "</div>"
+        "<div class='hn-outboundphone_img'>" ++ "Phone" ++ "</div>"
         "</button>",
-    Form = #form{id = {'twilio-outboundphone', Title},
-                 kind = "twilio-outboundphone",
+    Form = #form{id = {'phone', Title},
+                 kind = "phone",
                  attrs = Payload},
     {webcontrol, {Form, {Title, 2, 2, Incs}}, HTML}.
 
