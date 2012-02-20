7596b271da4a874bf646e21b0ec99fe706b2b446
Gordon Guthrie <gordon@hypernumbers.com>
2012-02-20 14:04:33 +0000

    More phone functionality
    We now have:
    * an auto sms fn
    * a manual sms fn
    * logging of all calls/sms's to a common contact page

--- lib/formula_engine-1.0/src/hnfuns_phone.erl
+++ lib/formula_engine-1.0/src/hnfuns_phone.erl
@@ -25,20 +25,23 @@
 'auto.robocall'([Condition, PhoneNo, Msg]) ->
     check_if_paid(fun 'auto.robocall2'/2, [Condition, PhoneNo, Msg, ""]).
 
-'manual.sms.out'([PhoneNo, Prefix]) ->
-    check_if_paid(fun 'manual.sms.out2'/2, [PhoneNo, normalise(Prefix)]);
-'manual.sms.out'([PhoneNo]) ->
-    check_if_paid(fun 'manual.sms.out2'/2, [PhoneNo, ""]).
+'manual.sms.out'([PhoneNo, Msg, Prefix]) ->
+    check_if_paid(fun 'manual.sms.out2'/2, [PhoneNo, Msg, normalise(Prefix)]);
+'manual.sms.out'([PhoneNo, Msg]) ->
+    check_if_paid(fun 'manual.sms.out2'/2, [PhoneNo, Msg, ""]).
 
-'manual.sms.out2'([PhoneNo, Prefix], _AC) ->
+'manual.sms.out2'([PhoneNo, Msg, Prefix], _AC) ->
     [PhoneNo2] = typechecks:std_strs([PhoneNo]),
+    Msg2 = rightsize(Msg),
     PhoneNo3 = compress(PhoneNo2),
-    Log = #contact_log{to = PhoneNo3},
+    Log = #contact_log{idx = get(idx), type = "manual sms",
+                       to = "+" ++ Prefix ++ PhoneNo3, contents = Msg2},
     TwiML = "",
-    Capability = [{manual_sms, "+" ++ Prefix ++ PhoneNo3}],
+    Capability = [{manual_sms, "+" ++ Prefix ++ PhoneNo3, Msg2}],
     Type ={"softphone_type", "manual sms"},
     Config = [{"button_txt", "(+" ++ Prefix ++ ") " ++ PhoneNo3},
-              {"headline_txt", "Send SMS"}],
+              {"headline_txt", "Send SMS"},
+              {"sms_msg", Msg2}],
     Phone = #phone{twiml = TwiML, capability = Capability, log = Log,
                    softphone_type = Type, softphone_config = Config},
     Headline = "Send SMS",
@@ -78,22 +81,25 @@ check_if_paid(Fun, Args) ->
 'auto.sms.out2'([Condition, PhoneNo, Msg, Prefix], AC) ->
     [Condition] = typechecks:std_bools([Condition]),
     [PhoneNo2] = typechecks:std_strs([PhoneNo]),
-    PhoneNo3 = compress(PhoneNo2),
-    [Msg2] = typechecks:std_strs([Msg]),
-    Length = length(Msg2),
-    Msg3 = if
-               Length > 160  -> {Msg2, _} = lists:split(160, Msg2),
-                                Msg2;
-               Length =< 160 -> Msg2
-           end,
+    PhoneNo3 = "+" ++ Prefix ++ compress(PhoneNo2),
+    Msg2 = rightsize(Msg),
+    Log = #contact_log{idx = get(idx), type = "automatic sms", to = PhoneNo3,
+                      contents = Msg2},
+    S = get(site),
     case Condition of
         true ->
-            case contact_utils:post_sms(AC, "+" ++ Prefix ++ PhoneNo3, Msg3) of
-                {ok, ok} -> "SMS: " ++ Msg3 ++ " sent to phone " ++ PhoneNo3;
-                _        -> ?ERRVAL_ERR
+            case contact_utils:post_sms(AC, PhoneNo3, Msg2) of
+                {ok, ok} ->
+                    spawn(hn_twilio_mochi, log,
+                          [S, Log#contact_log{status = "ok"}]),
+                    "SMS: " ++ Msg2 ++ " sent to phone " ++ PhoneNo3;
+                _        ->
+                    spawn(hn_twilio_mochi, log,
+                          [S, Log#contact_log{status = "failed"}]),
+                    ?ERRVAL_ERR
             end;
         false ->
-            "SMS: " ++ Msg3 ++ " to be sent to phone " ++ PhoneNo3
+            "SMS: " ++ Msg2 ++ " to be sent to phone " ++ PhoneNo3
     end.
 
 'phone.out'([PhoneNo, Prefix]) ->
@@ -111,7 +117,8 @@ check_if_paid(Fun, Args) ->
                             site_phone_no = Site_Phone} = AC,
             [PhoneNo2] = typechecks:std_strs([PhoneNo]),
             PhoneNo3 = compress(PhoneNo2),
-            Log = #contact_log{to = PhoneNo3},
+            Log = #contact_log{idx = get(idx), type = "outbound call",
+                               to = "+" ++ Prefix ++ PhoneNo3},
             TwiML = "<Response>" ++
                 "<Dial callerId='" ++ Site_Phone ++ "' record='true'>" ++
                 "<Number>" ++ Prefix ++ PhoneNo3 ++ "</Number>"
@@ -165,3 +172,12 @@ normalise(Prefix) ->
              Num          -> Num
          end,
     integer_to_list(P2).
+
+rightsize(Msg) ->
+    [Msg2] = typechecks:std_strs([Msg]),
+    Length = length(Msg2),
+    if
+        Length > 160  -> {Msg3, _} = lists:split(160, Msg2),
+                         Msg3;
+        Length =< 160 -> Msg2
+    end.
--- lib/hypernumbers-1.0/src/hn_twilio_mochi.erl
+++ lib/hypernumbers-1.0/src/hn_twilio_mochi.erl
@@ -13,13 +13,16 @@
 -include("twilio_web.hrl").
 
 -export([
+         log/2,
          get_phone/3,
          handle_call/2,
          handle_phone_post/3
         ]).
 
-get_phone(#refX{site = _S}, #phone{capability = [{manual_sms, PhoneNo}]} = P, _Uid) ->
+get_phone(#refX{site = _S}, #phone{capability = [{manual_sms, PhoneNo, Msg}]} = P,
+          _Uid) ->
     {struct, lists:flatten([{"phoneno", PhoneNo},
+                            {"msg", Msg},
                             P#phone.softphone_config,
                             P#phone.softphone_type])};
 get_phone(#refX{site = S}, #phone{capability = [{client_outgoing, _, _}] = C} = P,
@@ -34,34 +37,42 @@ get_phone(#refX{site = S}, #phone{capability = [{client_outgoing, _, _}] = C} = 
     % by twilio, twilio needs to get the originating URL from the data
     % so we sign the hypertab with the URL that the request will come in on
     IncomingPath = ["_services", "phone"],
-    HyperTag = passport:create_hypertag(S, IncomingPath, Uid, Email, P, Age),
+    % now add the current user into the phone log
+    {ok, Email} = passport:uid_to_email(Uid),
+    #phone{log = Log} = P,
+    Log2 = Log#contact_log{from = Email},
+    P2 = P#phone{log = Log2},
+    HyperTag = passport:create_hypertag(S, IncomingPath, Uid, Email, P2, Age),
     {struct, lists:flatten([{"phonetoken", binary_to_list(Token)},
                             {hypertag, HyperTag},
                             P#phone.softphone_config,
                             P#phone.softphone_type])}.
 
-handle_phone_post(#refX{} = Ref, Phone, #env{body = Body}) ->
+handle_phone_post(#refX{site = S} = Ref, Phone, #env{body = Body, uid = Uid}) ->
     case Body of
         [{"manual_sms", {struct, Args}}] ->
+            Log = Phone#phone.log,
+            {ok, Email} = passport:uid_to_email(Uid),
             case handle_sms(Ref, Phone, Args) of
-                {ok, ok}   -> {ok, 200};
-                {error, N} -> {error, N}
+                {ok, ok}   ->
+                    log(S, Log#contact_log{from = Email, status = "ok"}),
+                    {ok, 200};
+                {error, N} ->
+                    log(S, Log#contact_log{from = Email, status = "failed"}),
+                    {error, N}
             end;
         _  -> {error, 401}
     end.
 
 handle_sms(Ref, Phone, Args) ->
-    #phone{capability = [{manual_sms, P}]} = Phone,
-    % check that the phone no in the request matches that
+    #phone{capability = [{manual_sms, P, Msg}]} = Phone,
+    % check that the phone no and msg in the request matches those
     % in the capability
-    case proplists:lookup("phoneno", Args) of
-        none             -> {error, 401};
-        {"phoneno", P}   -> {"msg", Msg} = proplists:lookup("msg", Args),
-                           #refX{site = S} = Ref,
-                           AC = contact_utils:get_twilio_account(S),
-                           contact_utils:post_sms(AC, P, Msg);
-        % somebody is spoofing the phone no, erk!
-        {"phoneno", _P2} -> {error, 401}
+    case validate_sms([{"msg", Msg}, {"phoneno", P}], Args) of
+        false -> {error, 401};
+        true  -> #refX{site = S} = Ref,
+                 AC = contact_utils:get_twilio_account(S),
+                 contact_utils:post_sms(AC, P, Msg)
     end.
 
 handle_call(#refX{} = Ref, #env{} = Env) ->
@@ -80,14 +91,57 @@ handle_c2(#refX{site = S, path = P},
     {"hypertag", HyperTag} = proplists:lookup("hypertag", CP),
     HT = passport:open_hypertag(S, P, HyperTag),
     {ok, _Uid, _EMail, Phone, _, _} = HT,
+    Log = Phone#phone.log,
+    Log2 = Log#contact_log{reference = Body#twilio.call_sid},
     case AppSID of
-        LocalAppSID -> Phone#phone.twiml;
+        LocalAppSID -> log(S, Log2),
+                       Phone#phone.twiml;
         _Other      -> error
     end;
 handle_c2(_Ref, #twilio{called = null,
                         caller = null,
                         from = null,
                         to = null,
-                  call_duration = #twilio_duration{} = CD}) ->
-    io:format("Call Duration is ~p~n", [CD]),
+                  call_duration = #twilio_duration{}} = Tw) ->
+    io:format("Tw is ~p~n", [Tw]),
     {ok, 200}.
+
+validate_sms([], _Args) ->
+    true;
+validate_sms([{K, V} | T], Args) ->
+    case proplists:lookup(K, Args) of
+        none     -> false;
+        {K, V}   -> validate_sms(T, Args);
+        % somebody is spoofing client-side, erk!
+        {K, _V2} -> false
+    end.
+
+log(Site, #contact_log{} = Log) ->
+    #contact_log{idx = Idx} = Log,
+    XRefX = new_db_api:idx_to_xrefX(Site, Idx),
+    #xrefX{path = P} = XRefX,
+    P2 = lists:merge(P, ["contacts"]),
+    RefX = hn_util:xrefX_to_refX(XRefX),
+    RefX2 = RefX#refX{type = gurl, path = P2, obj = {row, {1, 1}}},
+    Array = [
+             {struct, [{"label", "type"},
+                       {"formula", Log#contact_log.type}]},
+             {struct, [{"label", "from"},
+                       {"formula", Log#contact_log.from}]},
+             {struct, [{"label", "to"},
+                       {"formula", Log#contact_log.to}]},
+             {struct, [{"label", "cc"},
+                       {"formula", Log#contact_log.cc}]},
+             {struct, [{"label", "bcc"},
+                       {"formula", Log#contact_log.bcc}]},
+             {struct, [{"label", "subject"},
+                       {"formula", Log#contact_log.subject}]},
+              {struct, [{"label", "contents"},
+                       {"formula", Log#contact_log.contents}]},
+             {struct, [{"label", "reference"},
+                       {"formula", Log#contact_log.reference}]},
+             {struct, [{"label", "status"},
+                       {"formula", Log#contact_log.status}]}
+             ],
+    new_db_api:handle_form_post(RefX2, Array, nil).
+
