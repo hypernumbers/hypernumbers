931ee29b210cf89545cdafef8b14acbaedefdf96
Gordon Guthrie <gordon@hypernumbers.com>
2012-03-26 14:40:46 +0100

    added new suite of ztests

---
+++ tests/ztest/ztest_test2_SUITE.erl
@@ -0,0 +1,216 @@
+%%%-------------------------------------------------------------------
+%%% @author    Gordon Guthrie
+%%% @copyright (C) 2012, Hypernumbers Ltd
+%%% @doc       Test suite for z-tests
+%%%
+%%% @end
+%%% Created :  24 Mar 2012 by gordon@hypernumbers.com
+%%%-------------------------------------------------------------------
+-module(ztest_test2_SUITE).
+
+-suite_defaults([{timetrap, {minutes, 10}}]).
+
+%% Note: This directive should only be used in test suites.
+-compile(export_all).
+
+-define(SITE, "http://tests.hypernumbers.dev:9000").
+-define(SLEEP, 1000).
+
+-include_lib("hypernumbers/include/spriki.hrl").
+-include("ct.hrl").
+
+%% Test server callback functions
+%%--------------------------------------------------------------------
+%% @doc
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Initiation before the whole suite
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%%
+%% @spec init_per_suite(Config) -> Config
+%% @end
+%%--------------------------------------------------------------------
+init_per_suite(Config) ->
+    case hn_setup:site_exists(?SITE) of
+        true  -> ok = hn_setup:delete_site(?SITE);
+        false -> ok
+    end,
+    hn_setup:site(?SITE, blank, []),
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Cleanup after the whole suite
+%%
+%% @spec end_per_suite(Config) -> _
+%% @end
+%%--------------------------------------------------------------------
+end_per_suite(_Config) ->
+    ok = hn_setup:delete_site(?SITE).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Case - atom()
+%%   Name of the test case that is about to be run.
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Initiation before each test case
+%%
+%% Note: This function is free to add any key/value pairs to the Config
+%% variable, but should NOT alter/remove any existing entries.
+%% Initiation before each test case
+%%
+%% @spec init_per_testcase(TestCase, Config) -> Config
+%% @end
+%%--------------------------------------------------------------------
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Case - atom()
+%%   Name of the test case that is about to be run.
+%% Config - [tuple()]
+%%   A list of key/value pairs, holding the test case configuration.
+%%
+%% Cleanup after each test case
+%%
+%% @spec end_per_testcase(TestCase, Config) -> _
+%% @end
+%%--------------------------------------------------------------------
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% TestCases - [Case]
+%% Case - atom()
+%%   Name of a test case.
+%%
+%% Returns a list of all test cases in this test suite
+%%
+%% @spec all() -> TestCases
+%% @end
+%%--------------------------------------------------------------------
+all() ->
+    [
+     test1
+    ].
+
+%% Test cases starts here.
+%%--------------------------------------------------------------------
+test1() ->
+    [{doc, "Add a z-fn"}].
+
+test1(Config) when is_list(Config) ->
+    ok = load_vanilla_zformula("test1"),
+    Got = read_raw("test1"),
+    pass(0, Got).
+
+%%% Internal fns
+pass(Expected, Expected) -> io:format("Pass: Expected ~p Got ~p~n",
+                                      [Expected, Expected]),
+                            'test passed';
+pass(Expected, Got)      -> io:format("Fail: Expected ~p Got ~p~n",
+                                      [Expected, Got]),
+                            exit("test failed").
+
+load_vanilla_zformula(PageRoot) ->
+    io:format("loading vanilla zformula for ~p~n", [PageRoot]),
+    Formula = "=sum(/" ++ PageRoot ++ "/[true]/a1)",
+    ZCell = #refX{site = ?SITE, path = [PageRoot], type = url,
+                  obj = {cell, {1, 1}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", Formula}]}]).
+
+load_complex_zformula(PageRoot) ->
+    io:format("loading complex zformula for ~p~n", [PageRoot]),
+    Formula = "=sum(/" ++ PageRoot ++ "/[a2>1]/a1)",
+    ZCell = #refX{site = ?SITE, path = [PageRoot], type = url,
+                  obj = {cell, {1, 1}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", Formula}]}]).
+
+dump(PageRoot) ->
+    ZCell = #refX{site = ?SITE, path = [PageRoot], type = url,
+                  obj = {cell, {1, 1}}},
+    [{_, Cell}] = new_db_api:read_ref(ZCell),
+    io:format("Cell on ~p is ~p~n", [PageRoot, Cell]).
+
+read_raw(PageRoot) ->
+    read_raw([PageRoot], 1 ,1).
+
+read_raw(PageRoot, X, Y) ->
+    timer:sleep(?SLEEP),
+    ZCell = #refX{site = ?SITE, path = PageRoot, type = url,
+                  obj = {cell, {X, Y}}},
+    io:format("Reading ~p~n", [ZCell]),
+    case new_db_api:read_attribute(ZCell, "__rawvalue") of
+        [{_, ZCellVal}] -> ZCellVal;
+        Other           -> Other
+    end.
+
+add_page(PageRoot) ->
+    io:format("adding page on ~p~n", [PageRoot]),
+    ZCell = #refX{site = ?SITE, path = [PageRoot, "added page"], type = url,
+                  obj = {cell, {1, 1}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", "1"}]}]).
+
+add_complex_page(PageRoot) ->
+    io:format("adding complex page on ~p~n", [PageRoot]),
+    ZCell1 = #refX{site = ?SITE, path = [PageRoot, "added page"], type = url,
+                   obj = {cell, {1, 1}}},
+    ZCell2 = #refX{site = ?SITE, path = [PageRoot, "added page"], type = url,
+                   obj = {cell, {1, 2}}},
+   ok = new_db_api:write_attributes([{ZCell1, [{"formula", "1"}]}]),
+   ok = new_db_api:write_attributes([{ZCell2, [{"formula", "999"}]}]).
+
+
+load_values(_PageRoot, 0) -> ok;
+load_values(PageRoot, N) when is_integer(N) andalso N > 0 ->
+    io:format("loading value for page ~p on ~p~n", [N, PageRoot]),
+    ZCell = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, 1}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", "1"}]}]),
+    load_values(PageRoot, N - 1).
+
+load_complex_values(_PageRoot, 0) -> ok;
+load_complex_values(PageRoot, N) when is_integer(N) andalso N > 0 ->
+    io:format("loading complex value for page ~p on ~p~n", [N, PageRoot]),
+    ZCell1 = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, 1}}},
+    ZCell2 = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, 2}}},
+    ok = new_db_api:write_attributes([{ZCell1, [{"formula", "1"}]}]),
+    ok = new_db_api:write_attributes([{ZCell2, [{"formula", "999"}]}]),
+    load_complex_values(PageRoot, N - 1).
+
+delete(PageRoot, N, Y) when is_integer(N) andalso N > 0 ->
+    io:format("Deleting the cell for ~p on ~p~n", [N, PageRoot]),
+    ZCell = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, Y}}},
+    io:format("deleting ~p~n", [ZCell]),
+    ok = new_db_api:delete(ZCell, nil).
+
+poke10(PageRoot, N) when is_integer(N) andalso N > 0 ->
+    io:format("Poking the value 10 into ~p on ~p~n", [N, PageRoot]),
+    ZCell = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, 1}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", "10"}]}]).
+
+dropcomplex(PageRoot, N) when is_integer(N) andalso N > 0 ->
+    io:format("Dropping the complex z-query into ~p on ~p~n", [N, PageRoot]),
+    ZCell = #refX{site = ?SITE, path = [PageRoot, integer_to_list(N)], type = url,
+                  obj = {cell, {1, 2}}},
+    ok = new_db_api:write_attributes([{ZCell, [{"formula", "-1"}]}]).
+
+debug() ->
+    init_per_suite([]),
+    {ok, U} = passport:email_to_uid("test@hypernumbers.com"),
+    ok = hn_groups:add_user(?SITE, "admin", U),
+    test1([]).
