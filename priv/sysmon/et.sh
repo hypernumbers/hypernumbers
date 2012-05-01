5b4d49c6dcf3478b6d1ceba8802f06915b8fbc7e
main_node <gordon@hypernumbers.com>
2012-05-01 16:44:42 +0000

    some changes to make et work

--- priv/sysmon/et.sh
+++ priv/sysmon/et.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#echo "et.sh running `date`" >> /tmp/cron.log
+echo "et.sh running `date`" >> /tmp/cron.log
 RETURN=`/hn/hypernumbers/priv/sysmon/et`
-#echo $RETURN >> /tmp/cron.log
+echo $RETURN >> /tmp/cron.log
