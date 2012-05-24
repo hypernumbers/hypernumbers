c09bb03aa0d6a08dbb67383e8fd8e4bf93369ce8
Gordon Guthrie <gordon@hypernumbers.com>
2012-05-24 11:35:59 +0100

    minor tidy up

--- lib/hypernumbers-1.0/src/hn_mochi.erl
+++ lib/hypernumbers-1.0/src/hn_mochi.erl
@@ -815,8 +815,8 @@ iget(Ref, _Type, Qry, Env) ->
 -spec ipost(#refX{}, #qry{}, #env{}) -> any().
 
 % this is the path that the twilio phone redirect is wired to
-ipost(#refX{path = ["_services", "phoneredirect" | []], obj = {page, "/"}}, _Qry,
-      Env = #env{accept = html}) ->
+ipost(#refX{path = ["_services", "phoneredirect" | []], obj = {page, "/"}},
+      _Qry, Env = #env{accept = html}) ->
     Redir = case application:get_env(hypernumbers, environment) of
                 {ok, development} ->
                     true;
