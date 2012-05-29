129af644b67b349e27f5ebcd2efe6f7f6c458d32
Gordon Guthrie <gordon@hypernumbers.com>
2012-05-29 20:47:18 +0100

    some load tests...

--- priv/load_testing/ebin/build.sh
+++ priv/load_testing/ebin/build.sh
@@ -1,2 +1,3 @@
 erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../include ../src/load_testing.erl
 erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../include ../src/load_util.erl
+erlc -I ../../../lib/hypernumbers-1.0/include/ -I ../include ../src/zload.erl
---
+++ priv/load_testing/src/zload.erl
@@ -0,0 +1,19 @@
+-module(zload).
+
+-define(ZSITE, "http://zload.hypernumbers.dev:9000").
+
+-export([
+        load/0
+       ]).
+
+load() ->
+    case hn_setup:site_exists(?ZSITE) of
+        false -> ok;
+        true  -> hn_setup:delete_site(?ZSITE)
+    end,
+    {initial_view, []} = hn_setup:site(?ZSITE, blank, []),
+    io:format("Created blank site - about to load data~n"),
+    % now load up zload pages into blank
+    Dir = "/home/gordon/hypernumbers/lib/hypernumbers-1.0/priv/"
+        ++ "site_types/z_load",
+    ok = hn_setup:import_json_DEBUG("http://zload.hypernumbers.dev:9000", Dir).
