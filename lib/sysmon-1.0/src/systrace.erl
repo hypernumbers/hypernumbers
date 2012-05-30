ae1655de9b79c6b17c35094b47187ba88f4469db
Gordon Guthrie <gordon@hypernumbers.com>
2012-05-30 16:51:20 +0100

    exhancements to systrace

--- lib/sysmon-1.0/src/systrace.erl
+++ lib/sysmon-1.0/src/systrace.erl
@@ -8,28 +8,77 @@
 -module(systrace).
 
 -export([
-         profile_dbsrv/1
+         profile_dbsrv/1,
+         profile_db_wu/0
         ]).
 
 -define(OneMinute, 60000). % in microseconds
+-define(D, 68).
+
+-record(timings, {
+          pid = "",
+          function = "",
+          calls = "",
+          time = ""
+         }).
+
+profile_db_wu() ->
+    PIDs = [{all, new_db_wu}],
+    TraceFlags = [all],
+    Fun1 = fun(X) ->
+                  Fns = X:module_info(functions),
+                  Fun2 = fun({X1, Arity}, Acc) ->
+                                 Hd = hd(lists:reverse(atom_to_list(X1))),
+                                 case Hd of
+                                     % ascii matching!
+                                     ?D -> [{X1, Arity} | Acc];
+                                     _  -> Acc
+                                 end
+                         end,
+                   lists:foldl(Fun2, [], Fns)
+          end,
+    profile(PIDs, TraceFlags, Fun1).
 
 profile_dbsrv(Site) ->
     Id = hn_util:site_to_atom(Site, "_dbsrv"),
     PID = whereis(Id),
-    erlang:trace(PID, true, [all]),
-    erlang:trace_pattern({dbsrv, '_', '_'}, true, [local, call_time]),
-    Fns = dbsrv:module_info(functions),
-    timer:sleep(?OneMinute),
-    Fun = fun({Fn, Arity}) ->
-                  Trace = erlang:trace_info({dbsrv, Fn, Arity}, call_time),
-                   case Trace of
-                       {call_time, []} ->
-                           ok;
-                       {call_time, [{_Pid, C, S, Ms}]} ->
-                           io:format("~p/~p has been called ~p times av ~p secs~n",
-                                     [Fn, Arity, C, (((S * 1000 + Ms)/1000)/C)])
-                   end
+    TraceFlags = [all],
+    Fun = fun(X) ->
+                  X:module_info(functions)
           end,
-    [Fun(X) || X <- Fns],
-    erlang:trace(PID, false, [all]).
+    profile([{PID, dbsrv}], TraceFlags, Fun).
+
+profile(PIDs, TraceFlags, SelFun) ->
+    % first set up the tracing
+    Fun1 = fun({PID, Module}) ->
+                   erlang:trace(PID, true, TraceFlags),
+                   erlang:trace_pattern({Module, '_', '_'}, true,
+                                        [local, call_time])
+           end,
+    [Fun1(X) || X <- PIDs],
+    % now sample
+    Fun2 = fun({_PID, Mod}) ->
+                   Fns = SelFun(Mod),
+                   timer:sleep(?OneMinute),
+                   Fun3 = fun({Fn, Arity}) ->
+                                  Trace = erlang:trace_info({Mod, Fn, Arity},
+                                                            call_time),
+                                  case Trace of
+                                      {call_time, []} ->
+                                          [];
+                                      {call_time, [{P, C, S, Ms}]} ->
+                                          Total = ((S * 1000 + Ms)/1000),
+                                          #timings{pid = P,
+                                                   function = {Mod, Fn, Arity},
+                                                   calls = C,
+                                                   time = Total}
+                                  end
+                          end,
+                   lists:flatten([Fun3(X) || X <- Fns])
+           end,
+    Timings = [Fun2(X) || X <- PIDs],
+    % now tidy up
+    [erlang:trace(PID, false, [all]) || {PID, _M} <- PIDs],
+    % return the timings
+    Timings.
 
