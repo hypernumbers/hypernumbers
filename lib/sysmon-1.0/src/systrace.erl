b7126d8a525ff2c8e5765516031b80e026c1ab2e
Gordon Guthrie <gordon@hypernumbers.com>
2012-05-31 15:43:18 +0100

    can now site profile with systrace

--- lib/sysmon-1.0/src/systrace.erl
+++ lib/sysmon-1.0/src/systrace.erl
@@ -8,37 +8,56 @@
 -module(systrace).
 
 -export([
+         profile_dbsrvP/1,
+         profile_db_wuP/0,
+         profile_siteP/1,
          profile_dbsrv/1,
          profile_db_wu/0,
-         profile_site/1
+         profile_site/1,
+         profile_site/2
         ]).
 
 -define(OneMinute, 60000). % in microseconds
 -define(D, 68).
 
--record(timings, {
+-record(ts, {
           pid = "",
-          function = "",
+          tracee = "",
           calls = "",
           time = ""
          }).
+
+profile_site(Site, N) when is_integer(N) andalso N > 0 ->
+    Dir = code:lib_dir(hypernumbers) ++ "/../../var/logs/",
+    Stamp = "." ++ dh_date:format("Y_M_d_H_i_s"),
+    File = "systrace." ++ Site ++ Stamp ++ ".log",
+    Logs = prof(N, Site),
+    hn_util:log_terms(Logs, Dir ++ File).
+
+prof(N, Site) -> prof2(1, N, Site, []).
+
+prof2(N, N, _Site, Acc) -> lists:reverse(Acc);
+prof2(N, M, Site, Acc)  -> NewAcc = profile_site(Site),
+                           prof2(N + 1, M, Site, [NewAcc | Acc]).
+
+profile_siteP(Site) -> print(profile_site(Site)).
 
 profile_site(Site) ->
     {_SiteName, RegProcesses} = syslib:get_registered(Site),
     RP2 = rectify(RegProcesses),
     TraceFlags = [all],
-    Fun = fun(X) ->
-                  X:module_info(functions)
+    Fun = fun(_PID, X) ->
+                  Fns = X:module_info(functions),
+                  zip(Fns, X, [])
           end,
-    Prof = profile(RP2, TraceFlags, Fun),
-    print(Prof),
-    ok.
+    profile(RP2, TraceFlags, Fun).
 
+profile_db_wuP() -> print(profile_db_wu()).
 
 profile_db_wu() ->
     PIDs = [{all, new_db_wu}],
     TraceFlags = [all],
-    Fun1 = fun(X) ->
+    Fun1 = fun(_PID, X) ->
                   Fns = X:module_info(functions),
                   Fun2 = fun({X1, Arity}, Acc) ->
                                  Hd = hd(lists:reverse(atom_to_list(X1))),
@@ -50,48 +69,51 @@ profile_db_wu() ->
                          end,
                    lists:foldl(Fun2, [], Fns)
           end,
-    Prof = profile(PIDs, TraceFlags, Fun1),
-    print(Prof),
-    ok.
+    profile(PIDs, TraceFlags, Fun1).
+
+profile_dbsrvP(Site) -> print(profile_dbsrv(Site)).
 
 profile_dbsrv(Site) ->
     Id = hn_util:site_to_atom(Site, "_dbsrv"),
     PID = whereis(Id),
     TraceFlags = [all],
-    Fun = fun(X) ->
-                  X:module_info(functions)
+    Fun = fun(_PID, X) ->
+                  Fns = X:module_info(functions),
+                  zip(Fns, X, [])
           end,
-    Prof = profile([{PID, dbsrv}], TraceFlags, Fun),
-    print(Prof),
-    ok.
+    profile([{PID, dbsrv}], TraceFlags, Fun).
 
-profile(PIDs, TraceFlags, SelFun) ->
+profile(PIDs, TraceFlags, TraceeFun) ->
     % first set up the tracing
-    Fun1 = fun({PID, Module}) ->
+    Fun1 = fun({PID, Mod}) ->
                    erlang:trace(PID, true, TraceFlags),
-                   erlang:trace_pattern({Module, '_', '_'}, true,
+                   erlang:trace_pattern({Mod, '_', '_'}, true,
                                         [local, call_time])
            end,
     [Fun1(X) || X <- PIDs],
     % now sample
-    Fun2 = fun({_PID, Mod}) ->
-                   Fns = SelFun(Mod),
+    Fun2 = fun({PID, Mod}) ->
+                   Tracees = TraceeFun(PID, Mod),
                    timer:sleep(?OneMinute),
-                   Fun3 = fun({Fn, Arity}) ->
-                                  Trace = erlang:trace_info({Mod, Fn, Arity},
-                                                            call_time),
+                   Fun3 = fun(Term) ->
+                                  Trace = erlang:trace_info(Term, call_time),
                                   case Trace of
                                       {call_time, []} ->
                                           [];
-                                      {call_time, [{P, C, S, Ms}]} ->
-                                          Total = ((S * 1000 + Ms)/1000),
-                                          #timings{pid = P,
-                                                   function = {Mod, Fn, Arity},
-                                                   calls = C,
-                                                   time = Total}
+                                      {call_time, Time} ->
+                                          case pid_time(Time, PID) of
+                                              no_match ->
+                                                  [];
+                                              {P, C, S, Ms} ->
+                                                  Total = (S * 1000 + Ms)/1000,
+                                                  #ts{pid = P,
+                                                      tracee = Term,
+                                                      calls = C,
+                                                      time = Total}
+                                          end
                                   end
                           end,
-                   {Mod, lists:flatten([Fun3(X) || X <- Fns])}
+                   {PID, Mod, lists:flatten([Fun3(X) || X <- Tracees])}
            end,
     Timings = [Fun2(X) || X <- PIDs],
     % now tidy up
@@ -101,41 +123,61 @@ profile(PIDs, TraceFlags, SelFun) ->
 
 print([]) ->
     ok;
-print([{Mod, []} | T]) ->
-    io:format("No activity for ~p~n", [Mod]),
+print([{PID, Mod, []} | T]) ->
+    io:format("No activity for ~p on ~p~n", [Mod, PID]),
     print(T);
-print([{Mod, H} | T]) ->
-    Fun = fun(#timings{time = Tm}, Acc) ->
+print([{PID, Mod, H} | T]) ->
+    Fun = fun(#ts{time = Tm}, Acc) ->
                   Acc + Tm
           end,
     Total = lists:foldl(Fun, 0, H),
-    io:format("Total is ~p for ~p~n", [Total, Mod]),
+    io:format("Total is ~p for ~p on ~p~n", [Total, Mod, PID]),
     p2(H, Total),
     print(T).
 
 p2([], _Total) -> ok;
 p2([H | T], Total) ->
-    #timings{function = {Mod, Fn, Arity}, calls = C, time = Tm} = H,
-    case {Total, C} of
-        {Zero, C} when (Zero == 0 orelse Zero == 0.0)
-        andalso (C == 0 orelse C == 0.0) ->
-            io:format("~p,~p,~p,~p,~p,-,-~n",
-                      [Mod, Fn, Arity, C, Tm]);
-        {Zero, _} when Zero == 0 orelse Zero == 0.0 ->
-            io:format("~p,~p,~p,~p,~p,-,~p~n",
-                      [Mod, Fn, Arity, C, Tm, Tm/C]);
-        {_, C} when C == 0 orelse C == 0.0 ->
-            io:format("~p,~p,~p,~p,~p,~p%,-~n",
-                      [Mod, Fn, Arity, C, Tm, (Tm/Total)*100]);
-        _Other ->
-            io:format("~p,~p,~p,~p,~p,~p%,~p~n",
-                      [Mod, Fn, Arity, C, Tm, (Tm/Total)*100, Tm/C])
+    io:format("H is ~p~n", [H]),
+    #ts{tracee = Tracee, calls = C, time = Tm} = H,
+    case Tracee of
+        {Mod, Fn, Arity} ->
+            case {Total, C} of
+                {Zero, C} when (Zero == 0 orelse Zero == 0.0)
+                andalso (C == 0 orelse C == 0.0) ->
+                    io:format("~p,~p,~p,~p,~p,-,-~n",
+                              [Mod, Fn, Arity, C, Tm]);
+                {Zero, _} when Zero == 0 orelse Zero == 0.0 ->
+                    io:format("~p,~p,~p,~p,~p,-,~p~n",
+                              [Mod, Fn, Arity, C, Tm, Tm/C]);
+                {_, C} when C == 0 orelse C == 0.0 ->
+                    % don't print nothing
+                    ok;
+                _Other ->
+                    io:format("~p,~p,~p,~p,~p,~p%,~p~n",
+                              [Mod, Fn, Arity, C, Tm, (Tm/Total)*100, Tm/C])
+            end;
+        Term ->
+            case {Total, C} of
+                {Zero, C} when (Zero == 0 orelse Zero == 0.0)
+                andalso (C == 0 orelse C == 0.0) ->
+                    io:format("~p,~p,~p,-,-~n",
+                              [Term, C, Tm]);
+                {Zero, _} when Zero == 0 orelse Zero == 0.0 ->
+                    io:format("~p,~p,~p,-,~p~n",
+                              [Term, C, Tm, Tm/C]);
+                {_, C} when C == 0 orelse C == 0.0 ->
+                    % don't print nothing
+                    ok;
+                _Other ->
+                    io:format("~p,~p,~p,~p%,~p~n",
+                              [Term, C, Tm, (Tm/Total)*100, Tm/C])
+            end
     end,
     p2(T, Total).
 
 rectify(List) -> r2(List, []).
 
-r2([], Acc)          -> Acc;
+r2([], Acc)           -> Acc;
 r2([{N, P}| T], Acc) -> r2(T, [{P, rect(N)} | Acc]).
 
 rect("dbsrv")     -> dbsrv;
@@ -149,3 +191,11 @@ rect("sup")       -> sitemaster_sup;
 rect("phonecall") -> phonecall_sup;
 rect("calc_sup")  -> calc_sup;
 rect("zinf")      -> zinf_srv.
+
+pid_time([], _PID)                       -> no_match;
+pid_time([{PID, _, _, _} = H | _T], PID) -> H;
+pid_time([_H | T], PID)                  -> pid_time(T, PID).
+
+zip([], _A, Acc)          -> lists:reverse(Acc);
+zip([{B, C} | T], A, Acc) -> zip(T, A, [{A, B, C} | Acc]).
+
