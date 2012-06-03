63c5d1a0e361dc093b63ea5d862d6529553834bc
Gordon Guthrie <gordon@hypernumbers.com>
2012-06-03 10:25:03 +0100

    tweaks to load testing

--- lib/load_testing/src/zload.erl
+++ lib/load_testing/src/zload.erl
@@ -16,6 +16,15 @@ load() ->
     % now load up zload pages into blank
     Dir = "/home/gordon/hypernumbers/lib/hypernumbers-1.0/priv/"
         ++ "site_types/z_load",
-    io:format("setting up site~n"),
+    io:format("setting up site ~p~n", [erlang:time()]),
     ok = hn_setup:import_json_DEBUG("http://zload.hypernumbers.dev:9000", Dir),
-    io:format("finished...~n").
+    check().
+
+check() ->
+    case dbsrv:is_busy("http://zload.hypernumbers.dev:9000") of
+        true ->
+            timer:sleep(5000),
+            check();
+        false ->
+            io:format("Finished ~p~n", [erlang:time()])
+    end.
