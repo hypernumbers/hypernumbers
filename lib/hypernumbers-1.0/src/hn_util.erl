ed31fa660ad72ce46210359203b3d96f66b96a58
Gordon <gordon@hypernumbers.com>
2013-02-28 08:06:12 +0000

    some binary manipulation fns

--- lib/hypernumbers-1.0/src/hn_util.erl
+++ lib/hypernumbers-1.0/src/hn_util.erl
@@ -12,6 +12,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([
+         bin_to_hexstr/1,
+         hexstr_to_bin/1,
          is_site_valid/1,
          site_type_exists/1,
          split_emails/1,
@@ -96,6 +98,18 @@
          % Formula utilities
          make_formula/2
         ]).
+
+bin_to_hexstr(Bin) ->
+  lists:flatten([io_lib:format("~2.16.0B", [X]) ||
+    X <- binary_to_list(Bin)]).
+
+hexstr_to_bin(S) ->
+  hexstr_to_bin(S, []).
+hexstr_to_bin([], Acc) ->
+  list_to_binary(lists:reverse(Acc));
+hexstr_to_bin([X,Y|T], Acc) ->
+  {ok, [V], []} = io_lib:fread("~16u", [X,Y]),
+  hexstr_to_bin(T, [V | Acc]).
 
 read_lines(File) ->
     case file:open(File, read) of
