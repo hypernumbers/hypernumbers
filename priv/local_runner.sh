2f0a81a2c8b2e2e62da19deeaab975308c1d2c1a
Gordon Guthrie <gordon@vixo.com>
2013-12-12 07:33:12 +0000

    make work on local machine

--- priv/local_runner.sh
+++ priv/local_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-HNTOP=/home/gordon/hypernumbers
-WEBROOT=/home/gordon/hn_tests/dev-www
-TESTDIR=/home/gordon/hn_tests/test-hypernumbers
+HNTOP=/home/vagrant/hypernumbers
+WEBROOT=/home/vagrant/hn_tests/dev-www
+TESTDIR=/home/vagrant/hn_tests/test-hypernumbers
 LASTRUN=$WEBROOT/last_run
 
 cd $HNTOP
@@ -15,41 +15,48 @@ ruby regen_tests.rb 2x
 cd $HNTOP
 
 ## Generate System Tests
+printf "Generate Tests\n"
 printf $(./hn call 'testsys:generate().')
-printf "\n\n"
+printf "\n\nGenerate Fuzz Tests\n"
+printf $(./hn call 'test:generate_fuzz_tests().')
+printf "\n\nRun Systems Tests\n"
 printf $(./hn call 'test:sys().')
-printf "\n\n"
+printf "\n\nRun Security Tests\n"
 printf $(./hn call 'test:security().')
-printf "\n\n"
 
-## Run tests
+printf "\n\nRun Excel 1a Tests\n"
 printf $(./hn call 'test:excel("1a").')
-printf "\n\n"
+printf "\n\nRun Excel 1b Tests\n"
 printf $(./hn call 'test:excel("1b").')
-printf "\n\n"
+printf "\n\nRun Excel 1c Tests\n"
 printf $(./hn call 'test:excel("1c").')
-printf "\n\n"
+printf "\n\nRun Excel 1d Tests\n"
 printf $(./hn call 'test:excel("1d").')
-printf "\n\n"
+printf "\n\nRun Excel 1e Tests\n"
 printf $(./hn call 'test:excel("1e").')
-printf "\n\n"
-
+printf "\n\nRun Excel 2a Tests\n"
 printf $(./hn call 'test:excel("2a").')
-printf "\n\n"
+printf "\n\nRun Excel 2b Tests\n"
 printf $(./hn call 'test:excel("2b").')
-printf "\n\n"
+printf "\n\nRun Excel 2c Tests\n"
 printf $(./hn call 'test:excel("2c").')
-printf "\n\n"
+printf "\n\nRun Excel 2d Tests\n"
 printf $(./hn call 'test:excel("2d").')
-printf "\n\n"
+printf "\n\nRun Excel 2e Tests\n"
 printf $(./hn call 'test:excel("2e").')
-printf "\n\n"
 
+printf "\n\nRun Auth Tests\n"
 printf $(./hn call 'test:auth().')
-printf "\n\n"
+printf "\n\nRun Authorizaton Tests"
+printf $(./hn call 'test:authorization().')
+printf "\n\nRun ZTests"
 printf $(./hn call 'test:ztest().')
-printf "\n\n"
+printf "\n\nRun Fuzz Tests"
+printf $(./hn call 'test:fuzz().')
+printf "\n\nFinally Cleanup\n"
 
 ## Cleanup.
 ./hn stop
+
+printf "Over and out...\n"
 
