347ae906fd6ff3fb378580922c759ce89d8dbb67
Gordon Guthrie <gordon@vixo.com>
2014-03-09 09:55:44 +0000

    more load testing shenanigans

--- lib/load_testing/src/load_util.erl
+++ lib/load_testing/src/load_util.erl
@@ -1,9 +1,26 @@
 %%% @author        gordon <gordon@hypernumbers.com>
-%%% @copyright     (C) 2011, gordon
-%%% @doc           Utilities for load teting
+%%% @copyright     (C) 2011-2014, gordon
+%%% @doc           Utilities for load testing
 %%%
 %%% @end
 %%% Created : 20 Aug 2011 by gordon <gordon@gordon.dev>
+
+%%%-------------------------------------------------------------------
+%%%
+%%% LICENSE
+%%%
+%%% This program is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU Affero General Public License as
+%%% published by the Free Software Foundation version 3
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU Affero General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU Affero General Public License
+%%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%%-------------------------------------------------------------------
 
 -module(load_util).
 
