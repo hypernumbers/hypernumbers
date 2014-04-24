2c01d94004900632970c56b16ff70d70a15d30cd
Gordon Guthrie <gordon@vixo.com>
2014-04-24 11:52:10 +0000

    add per minute tick fn

--- lib/formula_engine-1.0/src/hnfuns_special.erl
+++ lib/formula_engine-1.0/src/hnfuns_special.erl
@@ -98,18 +98,19 @@ tick([]) -> tick([1]);
 tick(Options) ->
     Opts = typechecks:std_ints(Options),
     Spec = case Opts of
-               [0]    -> hourly;
-               [1]    -> daily;
-               [2]    -> {weekly, 1}; % monday
-               [2, 1] -> {weekly, 1}; % monday
-               [2, 2] -> {weekly, 2}; % tuesday
-               [2, 3] -> {weekly, 3}; % wednesday
-               [2, 4] -> {weekly, 4}; % thursday
-               [2, 5] -> {weekly, 5}; % friday
-               [2, 6] -> {weekly, 6}; % saturday
-               [2, 7] -> {weekly, 7}; % sunday
-               [3]    -> {monthly, 1};
-               [3, N] when N > 0 andalso N < 31 -> {monthly, N};
+               [0]    -> everyminute;
+               [1]    -> hourly;
+               [2]    -> daily;
+               [3]    -> {weekly, 1}; % monday
+               [3, 1] -> {weekly, 1}; % monday
+               [3, 2] -> {weekly, 2}; % tuesday
+               [3, 3] -> {weekly, 3}; % wednesday
+               [3, 4] -> {weekly, 4}; % thursday
+               [3, 5] -> {weekly, 5}; % friday
+               [3, 6] -> {weekly, 6}; % saturday
+               [3, 7] -> {weekly, 7}; % sunday
+               [4]    -> {monthly, 1};
+               [4, N] when N > 0 andalso N < 31 -> {monthly, N};
                _      -> ?ERR_VAL
            end,
     #spec_val{val = stdfuns_date:now([]), sp_timer = #sp_timer{spec = Spec}}.
